5329157b30dbb1758dec3e6584d6c4d5f0c1ee03
peatroot <garethpeat@gmail.com>
2018-10-22 16:55:24 +0100

    Colour nodes based on nodeType

--- src/pages/DiseasePage.js
+++ src/pages/DiseasePage.js
@@ -23,10 +23,17 @@ const diseaseDAGQuery = gql`
         id
         name
         parentIds
+        nodeType
       }
     }
   }
 `;
+
+const colourMap = {
+  base: "chartreuse",
+  parent: "lightblue",
+  child: "coral",
+};
 
 const DiseasePage = ({ match }) => {
   const { efoId } = match.params;
@@ -61,8 +68,8 @@ const DiseasePage = ({ match }) => {
             return node;
           });
 
-          const width = 1600;
-          const height = 800;
+          const width = 1400;
+          const height = 700;
           const margin = 100;
 
           const dag = d3.dratify()(nodes);
@@ -80,25 +87,6 @@ const DiseasePage = ({ match }) => {
 
           return (
             <svg width={width} height={height}>
-              {descendents.map(({ x, y, id, data }) => (
-                <g
-                  key={id}
-                  transform={`translate(${margin +
-                    x * (width - 2 * margin)},${margin +
-                    y * (height - 2 * margin)})`}
-                >
-                  <circle r={3} cx={0} cy={0} fill="none" stroke="black" />
-                  <text
-                    x={0}
-                    y={10}
-                    textAnchor="middle"
-                    alignmentBaseline="middle"
-                    fontSize={10}
-                  >
-                    {data.name}
-                  </text>
-                </g>
-              ))}
               {links.map(({ source, target, data }) => (
                 <path
                   key={`${source.id}-${target.id}`}
@@ -116,6 +104,47 @@ const DiseasePage = ({ match }) => {
                     },
                   ])}
                 />
+              ))}
+              {descendents.map(({ x, y, id, data }) => (
+                <g
+                  key={id}
+                  transform={`translate(${margin +
+                    x * (width - 2 * margin)},${margin +
+                    y * (height - 2 * margin)})`}
+                >
+                  {/* <text
+                    x={0}
+                    y={10}
+                    textAnchor="middle"
+                    alignmentBaseline="middle"
+                    fontSize={10}
+                  >
+                    {data.name}
+                  </text> */}
+                  <switch>
+                    <foreignObject x={-50} y={0} width={100} height={100}>
+                      <p
+                        xmlns="http://www.w3.org/1999/xhtml"
+                        style={{
+                          fontSize: "10px",
+                          textAlign: "center",
+                          margin: 0,
+                          padding: "2px",
+                          background: colourMap[data.nodeType],
+                          border: "1px solid black",
+                          wordBreak: "break-word",
+                        }}
+                      >
+                        {data.name}
+                      </p>
+                    </foreignObject>
+
+                    <text x="20" y="20">
+                      Your SVG viewer cannot display html.
+                    </text>
+                  </switch>
+                  <circle r={3} cx={0} cy={0} fill="white" stroke="black" />
+                </g>
               ))}
             </svg>
           );
