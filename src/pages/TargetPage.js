d17e82f7db8330af38c00dc0f4957af2b97c9efe
peatroot <garethpeat@gmail.com>
2018-10-23 15:43:49 +0100

    Use target query in TargetPage

--- src/pages/TargetPage.js
+++ src/pages/TargetPage.js
@@ -1,5 +1,40 @@
 import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-const TargetPage = () => <div>TARGET PAGE</div>;
+const targetQuery = gql`
+  query TargetQuery($ensgId: String!) {
+    target(ensgId: $ensgId) {
+      id
+      name
+      symbol
+      synonyms
+    }
+  }
+`;
+
+const TargetPage = ({ match }) => {
+  const { ensgId } = match.params;
+  return (
+    <div>
+      <h1>{ensgId}</h1>
+      <Query query={targetQuery} variables={{ ensgId }}>
+        {({ loading, error, data }) => {
+          if (loading || error) {
+            return null;
+          }
+          const { symbol, name, synonyms } = data.target;
+          return (
+            <div>
+              <h2>{symbol}</h2>
+              <h3>{name}</h3>
+              <h4>{synonyms.join(", ")}</h4>
+            </div>
+          );
+        }}
+      </Query>
+    </div>
+  );
+};
 
 export default TargetPage;
