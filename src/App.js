b5d0341cb37a8f608ce0e7605265569dc07d23d2
Alfredo Miranda <alfredo@miranda.io>
2018-11-09 17:58:58 +0000

    Wrap App component in OtUiProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import { OtUiThemeProvider } from "ot-ui";
+
 import HomePage from "./pages/HomePage.js";
 import TargetPage from "./pages/TargetPage";
 import DiseasePage from "./pages/DiseasePage";
@@ -8,13 +10,15 @@ import DiseasePage from "./pages/DiseasePage";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/target/:ensgId" component={TargetPage} />
-          <Route path="/disease/:efoId" component={DiseasePage} />
-        </React.Fragment>
-      </Router>
+      <OtUiThemeProvider>
+        <Router>
+          <React.Fragment>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/target/:ensgId" component={TargetPage} />
+            <Route path="/disease/:efoId" component={DiseasePage} />
+          </React.Fragment>
+        </Router>
+      </OtUiThemeProvider>
     );
   }
 }
