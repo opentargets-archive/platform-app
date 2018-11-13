573b89e798438d2c36776000b8c303f910c470fb
Alfredo Miranda <alfredo@miranda.io>
2018-11-13 10:31:59 +0000

    Add links to the target and disease pages from the home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,19 @@
 import React from "react";
 import BasePage from "./BasePage";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <BasePage>Home</BasePage>;
+  return (
+    <BasePage>
+      <div>Home</div>
+      <div>
+        <Link to="/target/ENSG00000145777">Target page</Link>
+      </div>
+      <div>
+        <Link to="/disease/EFO_0000384">Disease page</Link>
+      </div>
+    </BasePage>
+  );
 };
 
 export default HomePage;
