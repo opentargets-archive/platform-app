8474832d1b66fd266d75096f72c6ed4455f471ea
Alfredo Miranda <alfredo@miranda.io>
2018-11-14 11:28:10 +0000

    Center page content

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Grid from "@material-ui/core/Grid";
 import { NavBar, Footer } from "ot-ui";
 
 import { externalLinks } from "../constants";
 
 const BasePage = ({ children }) => {
   return (
-    <div>
+    <Fragment>
       <NavBar name="Platform" />
-      {children}
+      <Grid container justify="space-around">
+        <Grid item md={10}>
+          {children}
+        </Grid>
+      </Grid>
       <Footer externalLinks={externalLinks} />
-    </div>
+    </Fragment>
   );
 };
 
