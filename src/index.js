bb355b50bee5f6ea8367ff4167fbc9590a380fbd
peatroot <garethpeat@gmail.com>
2018-11-21 09:36:55 +0000

    Import dc css before that of app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'dc/dc.min.css';
+import './index.css';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
