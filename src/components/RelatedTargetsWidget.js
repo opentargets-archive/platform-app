d381fcd43534ec00b4fc7b8bfdeeeffe2249ee92
Alfredo Miranda <alfredo@miranda.io>
2018-12-07 13:55:04 +0000

    Implement the realted targets widget

--- src/components/RelatedTargetsWidget.js
+++ src/components/RelatedTargetsWidget.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
@@ -12,6 +13,18 @@ import RelatedTargetsWidgetIcon from '../icons/RelatedTargetsWidgetIcon';
 const styles = theme => ({
   widget: {
     height: theme.widgetHeight,
+    border: `2px solid ${theme.palette.text.primary}`,
+  },
+  widgetNoData: {
+    border: '2px solid #E2DFDF',
+  },
+  widgetIcon: {
+    height: '50px',
+    width: '50px',
+    fill: '#5a5f5f',
+  },
+  widgetIconNoData: {
+    fill: '#e2dfdf',
   },
   bold: {
     fontWeight: 'bold',
@@ -19,7 +32,7 @@ const styles = theme => ({
 });
 
 class RelatedTargetsWidget extends Component {
-  static widgetName = 'similar targets';
+  static widgetName = 'related targets';
 
   handleClick = () => {
     const { history, match } = this.props;
@@ -41,19 +54,44 @@ class RelatedTargetsWidget extends Component {
 
     return (
       <Grid item md={3}>
-        <Card onClick={this.handleClick} className={classes.widget}>
+        <Card
+          onClick={this.handleClick}
+          className={classNames(classes.widget, {
+            [classes.widgetNoData]: relatedTargetsCount === 0,
+          })}
+        >
           <CardContent>
-            <Typography variant="h5" align="center">
-              Related targets
-            </Typography>
-            <RelatedTargetsWidgetIcon />
-            <Typography variant="caption" align="center">
-              View the top {relatedTargetsCount} targets related to {symbol}{' '}
-              based on diseases in common
-            </Typography>
-            <Typography variant="caption" align="center">
-              Source: Open Targets
-            </Typography>
+            <Grid container direction="column">
+              <Typography
+                variant="h5"
+                align="center"
+                color={relatedTargetsCount > 0 ? 'default' : 'secondary'}
+              >
+                Related Targets
+              </Typography>
+              <Grid container justify="center">
+                <RelatedTargetsWidgetIcon
+                  className={classNames(classes.widgetIcon, {
+                    [classes.widgetIconNoData]: relatedTargetsCount === 0,
+                  })}
+                />
+              </Grid>
+              <Typography
+                variant="h5"
+                align="center"
+                color={relatedTargetsCount > 0 ? 'default' : 'secondary'}
+              >
+                View the top {relatedTargetsCount} targets related to {symbol}{' '}
+                based on diseases in common
+              </Typography>
+              <Typography
+                variant="caption"
+                align="center"
+                color={relatedTargetsCount > 0 ? 'default' : 'secondary'}
+              >
+                Source: Open Targets
+              </Typography>
+            </Grid>
           </CardContent>
         </Card>
         <Route
