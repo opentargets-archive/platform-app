71888e1a6181b22dacffc0c63e216f9cde36b80c
peatroot <garethpeat@gmail.com>
2018-12-07 09:30:37 +0000

    Improve styling of CheckboxList table

--- src/components/CheckboxList.js
+++ src/components/CheckboxList.js
@@ -6,61 +6,108 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import classNames from 'classnames';
 
 const styles = theme => ({
-  checkboxListCellNoData: {
-    border: '1px solid #E2DFDF',
+  table: {
+    borderSpacing: 0,
+    borderCollapse: 'collapse',
+    width: '100%',
+  },
+
+  cellAny: {
+    borderStyle: 'solid',
+    borderWidth: '1px',
+  },
+  cellNoData: {
+    borderColor: '#E2DFDF',
     color: '#E2DFDF',
   },
-  checkboxListCellLabel: {
-    padding: '0 4px',
-  },
-  checkboxListCell: {
-    border: '1px solid #5A5F5F',
-    color: '#5A5F5F',
-  },
-  checkboxListCellValue: {
+  cellValue: {
     width: '1.5rem',
     height: '1.5rem',
     textAlign: 'center',
     verticalAlign: 'middle',
   },
-  checkboxListCellValueActive: {
+  cellValueUnchecked: {
+    borderColor: '#5A5F5F',
+    color: '#E2DFDF',
+  },
+  cellValueChecked: {
+    borderColor: '#5A5F5F',
     backgroundColor: '#7B196A',
     color: 'white',
   },
-  checkboxListIcon: {
-    fontSize: '1rem',
+  cellLabel: {
+    padding: '0 4px',
+    borderLeftWidth: 0,
   },
-  checkboxListTable: {
-    borderSpacing: 0,
-    width: '100%',
+  cellLabelUnchecked: {
+    borderColor: '#5A5F5F',
+    color: '#E2DFDF',
   },
+  cellLabelChecked: {
+    borderColor: '#5A5F5F',
+    color: '#5A5F5F',
+  },
+  icon: {
+    fontSize: '0.8rem',
+  },
+
+  // checkboxListCellNoData: {
+  //   border: '1px solid #E2DFDF',
+  //   color: '#E2DFDF',
+  // },
+  // checkboxListCellLabel: {
+  //   padding: '0 4px',
+  // },
+  // checkboxListCell: {
+  //   border: '1px solid #5A5F5F',
+  //   color: '#5A5F5F',
+  // },
+  // checkboxListCellValue: {
+  //   width: '1.5rem',
+  //   height: '1.5rem',
+  //   textAlign: 'center',
+  //   verticalAlign: 'middle',
+  // },
+  // checkboxListCellValueActive: {
+  //   backgroundColor: '#7B196A',
+  //   color: 'white',
+  // },
+  // checkboxListIcon: {
+  //   fontSize: '1rem',
+  // },
+  // checkboxListTable: {
+  //   borderSpacing: 0,
+  //   width: '100%',
+  // },
 });
 
 const CheckboxList = ({ classes, items }) => {
   const noData = !items.some(d => d.value);
   return (
-    <table className={classes.checkboxListTable}>
+    <table className={classes.table}>
       <tbody>
         {items.map(d => (
           <tr key={d.label}>
             <td
-              className={classNames(classes.checkboxListCellValue, {
-                [classes.checkboxListCellNoData]: noData,
-                [classes.checkboxListCellValueActive]: !noData && d.value,
+              className={classNames(classes.cellAny, classes.cellValue, {
+                [classes.cellNoData]: noData,
+                [classes.cellValueChecked]: !noData && d.value,
+                [classes.cellValueUnchecked]: !noData && !d.value,
               })}
             >
               <Icon
                 color="inherit"
-                className={classNames('fas', classes.checkboxListIcon, {
+                className={classNames('fas', classes.icon, {
                   'fa-times': !d.value,
                   'fa-check': d.value,
                 })}
               />
             </td>
             <td
-              className={classNames(classes.checkboxListCellLabel, {
-                [classes.checkboxListCellNoData]: noData,
-                [classes.checkboxListCell]: !noData,
+              className={classNames(classes.cellAny, classes.cellLabel, {
+                [classes.cellNoData]: noData,
+                [classes.cellLabelChecked]: !noData && d.value,
+                [classes.cellLabelUnchecked]: !noData && !d.value,
               })}
             >
               <Typography color="inherit">{d.label}</Typography>
