4b4b9b064d31e5fc63199ef85b4d54dafe214ad8
peatroot <garethpeat@gmail.com>
2018-12-10 14:37:14 +0000

    Update protein information to use widget and modal

--- src/components/ProteinInformation/index.js
+++ src/components/ProteinInformation/index.js
@@ -1,105 +1,72 @@
-import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import withStyles from '@material-ui/core/styles/withStyles';
 
-import ProteinInformationDetail from './Detail';
+import Widget from '../Widget';
 import CheckboxList from '../CheckboxList';
+import ProteinInformationDetail from './Detail';
 
-const styles = theme => ({
-  widget: {
-    height: theme.widgetHeight,
+const ProteinInformationWidget = ({
+  ensgId,
+  symbol,
+  protein: {
+    hasSequenceAnnotationVisualisation,
+    hasProteinStructure,
+    hasSubCellularLocation,
+    hasSubUnitData,
+    hasUniprotKeywords,
+    sources,
   },
-  checkboxListContainer: {
-    width: '100%',
-  },
-});
-
-class ProteinInformationWidget extends Component {
-  static widgetName = 'protein information';
-
-  handleClick = () => {
-    const { history, match } = this.props;
-    history.push(`${match.url}/protein-information`);
-  };
-
-  handleClose = () => {
-    const { history, match } = this.props;
-    history.push(match.url);
-  };
-
-  render() {
-    const { classes, ensgId, symbol, protein, match } = this.props;
-    const {
-      hasSequenceAnnotationVisualisation,
-      hasProteinStructure,
-      hasSubCellularLocation,
-      hasSubUnitData,
-      hasUniprotKeywords,
-    } = protein;
-
-    const items = [
-      {
-        value: hasSequenceAnnotationVisualisation,
-        label: 'Sequence annotation',
-      },
-      {
-        value: hasProteinStructure,
-        label: '3D structure',
-      },
-      {
-        value: hasSubCellularLocation,
-        label: 'Subcellular location',
-      },
-      {
-        value: hasSubUnitData,
-        label: 'Sub-unit data',
-      },
-      {
-        value: hasUniprotKeywords,
-        label: 'UniProt keywords',
-      },
-    ];
-    return (
-      <Grid item md={3}>
-        <Card onClick={this.handleClick} className={classes.widget}>
-          <CardContent>
-            <Typography variant="h5" align="center">
-              Protein information
-            </Typography>
-
-            <Grid
-              container
-              direction="column"
-              alignItems="stretch"
-              justify="space-evenly"
-              className={classes.checkboxListContainer}
-            >
-              <Grid item xs={12}>
-                <CheckboxList items={items} />
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-        <Route
-          path={`${match.path}/protein-information`}
-          render={() => {
-            return (
-              <ProteinInformationDetail
-                open
-                onClose={this.handleClose}
-                ensgId={ensgId}
-                symbol={symbol}
-              />
-            );
-          }}
-        />
+}) => {
+  const hasData =
+    hasSequenceAnnotationVisualisation ||
+    hasProteinStructure ||
+    hasSubCellularLocation ||
+    hasSubUnitData ||
+    hasUniprotKeywords;
+  const items = [
+    {
+      value: hasSequenceAnnotationVisualisation,
+      label: 'Sequence annotation',
+    },
+    {
+      value: hasProteinStructure,
+      label: '3D structure',
+    },
+    {
+      value: hasSubCellularLocation,
+      label: 'Subcellular location',
+    },
+    {
+      value: hasSubUnitData,
+      label: 'Sub-unit data',
+    },
+    {
+      value: hasUniprotKeywords,
+      label: 'UniProt keywords',
+    },
+  ];
+  return (
+    <Widget
+      title="Protein information"
+      detailUrlStem="protein-information"
+      detail={<ProteinInformationDetail ensgId={ensgId} symbol={symbol} />}
+      hasData={hasData}
+      sources={sources}
+    >
+      <Grid
+        container
+        direction="column"
+        alignItems="stretch"
+        justify="space-evenly"
+      >
+        <Grid item xs={12}>
+          <CheckboxList items={items} />
+        </Grid>
       </Grid>
-    );
-  }
-}
+    </Widget>
+  );
+};
 
-export default withStyles(styles)(withRouter(ProteinInformationWidget));
+ProteinInformationWidget.widgetName = 'protein information';
+
+export default ProteinInformationWidget;
