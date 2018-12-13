2b3149ce8da455ca572f2521e2c2a2ee2d4d186c
peatroot <garethpeat@gmail.com>
2018-12-13 14:57:10 +0000

    Make consistent change to sources list

--- src/components/RelatedTargets/Detail.js
+++ src/components/RelatedTargets/Detail.js
@@ -98,15 +98,13 @@ const RelatedTargetsDetail = ({ ensgId, symbol, sources }) => {
       </Typography>
       <Typography variant="caption">
         Source{sources.length > 1 ? 's' : null}:{' '}
-        {sources.map(d => (
-          <a
-            key={d.name}
-            href={d.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {d.name}
-          </a>
+        {sources.map((d, i) => (
+          <React.Fragment key={d.name}>
+            {i > 0 ? ' ' : null}
+            <a href={d.url} target="_blank" rel="noopener noreferrer">
+              {d.name}
+            </a>
+          </React.Fragment>
         ))}
       </Typography>
       <Query query={query} variables={{ ensgId }}>
