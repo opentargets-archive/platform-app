1d7a8f16e5ea552d56f8164eab4e86b985fe80bc
Luca Fumis <lucaf@ebi.ac.uk>
2018-12-18 15:06:48 +0000

    Added basic close button; issue opentargets/platform#375

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,13 +10,29 @@ const styles = theme => ({
   modalContents: {
     width: '90%',
     margin: '10% auto',
+    padding: '10px',
+  },
+  modalCloseButton: {
+    textAlign: 'center',
+    cursor: 'pointer',
+    float: 'right',
+    '& span': {
+      fontSize: '0.75em',
+      display: 'block',
+    },
   },
 });
 
 const Modal = ({ classes, open, onClose, children }) => {
   return (
     <MuiModal className={classes.modalContainer} open={open} onClose={onClose}>
-      <Paper className={classes.modalContents}>{children}</Paper>
+      <Paper className={classes.modalContents}>
+        <div className={classes.modalCloseButton} onClick={onClose}>
+          <i class="far fa-times-circle fa-2x" />
+          <span>Close</span>
+        </div>
+        {children}
+      </Paper>
     </MuiModal>
   );
 };
