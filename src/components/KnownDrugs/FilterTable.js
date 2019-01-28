2b93c4dceb9fb6e972df15dd3e7b7e94cdd89d00
Luca Fumis <lucaf@ebi.ac.uk>
2019-01-28 11:07:44 +0000

    Fixed data mapping and added summary counts

--- src/components/KnownDrugs/FilterTable.js
+++ src/components/KnownDrugs/FilterTable.js
@@ -6,25 +6,45 @@ import * as d3 from 'd3';
 
 import DCContainer from '../DCContainer';
 import { OtTable } from 'ot-ui';
+// import TrialsHistogram from './TrialsHistogram';
+import classNames from 'classnames';
 
 const columns = [
   {
-    id: 'disease.name',
+    id: 'disease',
     label: 'Disease',
     renderCell: d => d.disease.name,
+    comparator: (a, b) =>
+      a.disease.name > b.disease.name
+        ? 1
+        : a.disease.name === b.disease.name
+        ? 0
+        : -1,
   },
   {
-    id: 'clinicalTrial.phase',
+    id: 'phase',
     label: 'Phase',
     renderCell: d => d.clinicalTrial.phase,
+    comparator: (a, b) =>
+      a.clinicalTrial.phase > b.clinicalTrial.phase
+        ? 1
+        : a.clinicalTrial.phase === b.clinicalTrial.phase
+        ? 0
+        : -1,
   },
   {
-    id: 'clinicalTrial.status',
+    id: 'status',
     label: 'Status',
     renderCell: d => d.clinicalTrial.status,
+    comparator: (a, b) =>
+      a.clinicalTrial.status > b.clinicalTrial.status
+        ? 1
+        : a.clinicalTrial.status === b.clinicalTrial.status
+        ? 0
+        : -1,
   },
   {
-    id: 'clinicalTrial.source',
+    id: 'source',
     label: 'Source',
     renderCell: d => (
       <a
@@ -35,20 +55,29 @@ const columns = [
         {d.clinicalTrial.sourceName}
       </a>
     ),
+    comparator: (a, b) =>
+      a.clinicalTrial.sourceName > b.clinicalTrial.sourceName
+        ? 1
+        : a.clinicalTrial.sourceName === b.clinicalTrial.sourceName
+        ? 0
+        : -1,
   },
-
   {
-    id: 'drug.name',
+    id: 'drug',
     label: 'Drug',
     renderCell: d => d.drug.name,
+    comparator: (a, b) =>
+      a.drug.name > b.drug.name ? 1 : a.drug.name === b.drug.name ? 0 : -1,
   },
   {
-    id: 'drug.type',
+    id: 'type',
     label: 'Type',
     renderCell: d => d.drug.type,
+    comparator: (a, b) =>
+      a.drug.type > b.drug.type ? 1 : a.drug.type === b.drug.type ? 0 : -1,
   },
   {
-    id: 'mechanismOfAction.name',
+    id: 'mechanism',
     label: 'Mechanism of Action',
     renderCell: d => (
       <React.Fragment>
@@ -63,16 +92,34 @@ const columns = [
         </a>
       </React.Fragment>
     ),
+    comparator: (a, b) =>
+      a.mechanismOfAction.sourceName > b.mechanismOfAction.sourceName
+        ? 1
+        : a.mechanismOfAction.sourceName === b.mechanismOfAction.sourceName
+        ? 0
+        : -1,
   },
   {
-    id: 'drug.activity',
+    id: 'activity',
     label: 'Activity',
     renderCell: d => d.drug.activity,
+    comparator: (a, b) =>
+      a.drug.activity > b.drug.activity
+        ? 1
+        : a.drug.activity === b.drug.activity
+        ? 0
+        : -1,
   },
   {
-    id: 'target.symbol',
+    id: 'target',
     label: 'Target',
     renderCell: d => d.target.symbol,
+    comparator: (a, b) =>
+      a.target.symbol > b.target.symbol
+        ? 1
+        : a.target.symbol === b.target.symbol
+        ? 0
+        : -1,
   },
 ];
 
@@ -86,6 +133,30 @@ const styles = theme => ({
   },
   dcChartContainer: {
     padding: '8px',
+  },
+
+  countLabelSection: {
+    width: '220px',
+    float: 'left',
+  },
+  countLabel: {
+    fontWeight: 'bold',
+    padding: '8px',
+    borderRadius: '50%',
+    color: '#FFF',
+    width: '36px',
+    height: '36px',
+    display: 'inline-block',
+    textAlign: 'center',
+  },
+  countLabelDrug: {
+    backgroundColor: '#38954C',
+  },
+  countLabelTarget: {
+    backgroundColor: '#7b196a',
+  },
+  countLabelDisease: {
+    backgroundColor: '#d36141',
   },
 });
 
@@ -102,6 +173,41 @@ class KnownDrugsDetail extends React.Component {
     return (
       <React.Fragment>
         <div className={classes.dcChartContainer}>
+          <div className={classes.countLabelSection}>
+            <strong>Summary</strong>
+            <p>
+              <span
+                id="unique-drugs-count"
+                className={classNames(
+                  classes.countLabel,
+                  classes.countLabelDrug
+                )}
+              />{' '}
+              unique drugs
+            </p>
+            <p>
+              <span
+                id="associated-targets-count"
+                className={classNames(
+                  classes.countLabel,
+                  classes.countLabelTarget
+                )}
+              />{' '}
+              associated targets
+            </p>
+            <p>
+              <span
+                id="associated-diseases-count"
+                className={classNames(
+                  classes.countLabel,
+                  classes.countLabelDisease
+                )}
+              />{' '}
+              associated diseases
+            </p>
+            <div className="clearfix" />
+          </div>
+
           <DCContainer id="dc-trial-by-status-chart" title="Trials by Status" />
           <DCContainer id="dc-trial-by-phase-chart" title="Trials by Phase" />
           <DCContainer id="dc-trial-by-drug-chart" title="Trials by Drug" />
@@ -109,137 +215,184 @@ class KnownDrugsDetail extends React.Component {
             id="dc-drug-by-activity-chart"
             title="Drug by Activity"
           />
-          <DCContainer
+          {/* <DCContainer
             id="dc-drug-and-disease-by-activity-chart"
             title="Activity by (Disease, Drug)"
-          />
+          /> */}
         </div>
         <OtTable
           loading={false}
           error={null}
           columns={columns}
           data={filteredRows}
+          tableLayout="fixed"
         />
       </React.Fragment>
     );
   }
+
   renderCharts = () => {
     const { rows } = this.props;
-
+    console.log(rows[0]);
     // connect
     const ndx = crossfilter(rows);
 
     // dimensions
-    const dimStatus = ndx.dimension(d => d.status || 'UNKNOWN');
-    const dimPhase = ndx.dimension(d => d.phase);
-    const dimActivity = ndx.dimension(d => d.activity);
-    const dimDrugAndDisease = ndx.dimension(d => [d.drugName, d.efoLabel]);
-    const dimDrug = ndx.dimension(d => d.drugName);
+    const dimStatus = ndx.dimension(d => d.clinicalTrial.status || 'UNKNOWN');
+    const dimPhase = ndx.dimension(d => d.clinicalTrial.phase);
+    const dimActivity = ndx.dimension(d => d.drug.activity);
+    const dimDrugAndDisease = ndx.dimension(d => [d.drug.name, d.disease.name]);
+    const dimDrug = ndx.dimension(d => d.drug.name);
 
     // groups
+    const drugCount = ndx.groupAll().reduce(
+      (p, d) => {
+        p[d.drug.id] = 1;
+        return p;
+      },
+      (p, d) => {
+        delete p[d.drug.id];
+        return p;
+      },
+      () => ({})
+    );
+
+    const targetCount = ndx.groupAll().reduce(
+      (p, d) => {
+        p[d.target.id] = 1;
+        return p;
+      },
+      (p, d) => {
+        delete p[d.target.id];
+        return p;
+      },
+      () => ({})
+    );
+
+    const diseaseCount = ndx.groupAll().reduce(
+      (p, d) => {
+        p[d.disease.id] = 1;
+        return p;
+      },
+      (p, d) => {
+        delete p[d.disease.id];
+        return p;
+      },
+      () => ({})
+    );
+
     const groupTrialByStatus = dimStatus.group().reduce(
       (p, d) => {
-        if (d.evidenceUrl in p.trialCounts) {
-          p.trialCounts[d.evidenceUrl] += 1;
+        if (d.clinicalTrial.sourceUrl in p.trialCounts) {
+          p.trialCounts[d.clinicalTrial.sourceUrl] += 1;
         } else {
-          p.trialCounts[d.evidenceUrl] = 1;
+          p.trialCounts[d.clinicalTrial.sourceUrl] = 1;
         }
         return p;
       },
       (p, d) => {
-        p.trialCounts[d.evidenceUrl] -= 1;
-        if (p.trialCounts[d.evidenceUrl] === 0) {
-          delete p.trialCounts[d.evidenceUrl];
+        p.trialCounts[d.clinicalTrial.sourceUrl] -= 1;
+        if (p.trialCounts[d.clinicalTrial.sourceUrl] === 0) {
+          delete p.trialCounts[d.clinicalTrial.sourceUrl];
         }
         return p;
       },
       () => ({ trialCounts: {} })
     );
+
     const groupTrialByPhase = dimPhase.group().reduce(
       (p, d) => {
-        if (d.evidenceUrl in p.trialCounts) {
-          p.trialCounts[d.evidenceUrl] += 1;
+        if (d.clinicalTrial.sourceUrl in p.trialCounts) {
+          p.trialCounts[d.clinicalTrial.sourceUrl] += 1;
         } else {
-          p.trialCounts[d.evidenceUrl] = 1;
+          p.trialCounts[d.clinicalTrial.sourceUrl] = 1;
         }
         return p;
       },
       (p, d) => {
-        p.trialCounts[d.evidenceUrl] -= 1;
-        if (p.trialCounts[d.evidenceUrl] === 0) {
-          delete p.trialCounts[d.evidenceUrl];
+        p.trialCounts[d.clinicalTrial.sourceUrl] -= 1;
+        if (p.trialCounts[d.clinicalTrial.sourceUrl] === 0) {
+          delete p.trialCounts[d.clinicalTrial.sourceUrl];
         }
         return p;
       },
       () => ({ trialCounts: {} })
     );
+
     const groupDrugBy = dimDrug.group().reduce(
       (p, d) => {
-        if (d.phase in p.phaseCounts) {
-          p.phaseCounts[d.phase] += 1;
+        if (d.clinicalTrial.phase in p.phaseCounts) {
+          p.phaseCounts[d.clinicalTrial.phase] += 1;
         } else {
-          p.phaseCounts[d.phase] = 1;
+          p.phaseCounts[d.clinicalTrial.phase] = 1;
         }
 
-        if (d.evidenceUrl in p.trialCounts) {
-          p.trialCounts[d.evidenceUrl] += 1;
+        if (d.clinicalTrial.sourceUrl in p.trialCounts) {
+          p.trialCounts[d.clinicalTrial.sourceUrl] += 1;
         } else {
-          p.trialCounts[d.evidenceUrl] = 1;
+          p.trialCounts[d.clinicalTrial.sourceUrl] = 1;
         }
 
         return p;
       },
       (p, d) => {
-        p.phaseCounts[d.phase] -= 1;
-        if (p.phaseCounts[d.phase] === 0) {
-          delete p.phaseCounts[d.phase];
+        p.phaseCounts[d.clinicalTrial.phase] -= 1;
+        if (p.phaseCounts[d.clinicalTrial.phase] === 0) {
+          delete p.phaseCounts[d.clinicalTrial.phase];
         }
 
-        p.trialCounts[d.evidenceUrl] -= 1;
-        if (p.trialCounts[d.evidenceUrl] === 0) {
-          delete p.trialCounts[d.evidenceUrl];
+        p.trialCounts[d.clinicalTrial.sourceUrl] -= 1;
+        if (p.trialCounts[d.clinicalTrial.sourceUrl] === 0) {
+          delete p.trialCounts[d.clinicalTrial.sourceUrl];
         }
 
         return p;
       },
       () => ({ phaseCounts: {}, trialCounts: {} })
     );
+
     const activityMap = {
       AGONIST: 'agonist',
       ANTAGONIST: 'antagonist',
       UP_OR_DOWN: 'upOrDown',
     };
+
     const groupDrugByActivity = dimActivity.group().reduce(
       (p, d) => {
-        if (d.drugName in p) {
-          p[d.drugName] += 1;
+        if (d.drug.name in p) {
+          p[d.drug.name] += 1;
         } else {
-          p[d.drugName] = 1;
+          p[d.drug.name] = 1;
         }
         return p;
       },
       (p, d) => {
-        p[d.drugName] -= 1;
-        if (p[d.drugName] === 0) {
-          delete p[d.drugName];
+        p[d.drug.name] -= 1;
+        if (p[d.drug.name] === 0) {
+          delete p[d.drug.name];
         }
         return p;
       },
       () => ({})
     );
+
     const groupDrugAndDiseaseByActivity = dimDrugAndDisease.group().reduce(
       (p, d) => {
-        p[activityMap[d.activity]] += 1;
+        p[activityMap[d.drug.activity]] += 1;
         return p;
       },
       (p, d) => {
-        p[activityMap[d.activity]] -= 1;
+        p[activityMap[d.drug.activity]] -= 1;
         return p;
       },
       () => ({ agonist: 0, antagonist: 0, upOrDown: 0 })
     );
 
     // charts
+    const drugCountLabel = dc.numberDisplay('#unique-drugs-count');
+    const targetsCountLabel = dc.numberDisplay('#associated-targets-count');
+    const diseasesCountLabel = dc.numberDisplay('#associated-diseases-count');
+
     const chartTrialByStatus = dc.rowChart('#dc-trial-by-status-chart');
     const chartTrialByPhase = dc.rowChart('#dc-trial-by-phase-chart');
     const chartTrialByDrug = dc.rowChart('#dc-trial-by-drug-chart');
@@ -247,6 +400,24 @@ class KnownDrugsDetail extends React.Component {
     const chartDrugAndDiseaseByActivity = dc.heatMap(
       '#dc-drug-and-disease-by-activity-chart'
     );
+
+    drugCountLabel
+      .group(drugCount)
+      .formatNumber(d3.format('d'))
+      .valueAccessor(d => Object.keys(d).length)
+      .render();
+
+    targetsCountLabel
+      .group(targetCount)
+      .formatNumber(d3.format('d'))
+      .valueAccessor(d => Object.keys(d).length)
+      .render();
+
+    diseasesCountLabel
+      .group(diseaseCount)
+      .formatNumber(d3.format('d'))
+      .valueAccessor(d => Object.keys(d).length)
+      .render();
 
     chartTrialByStatus
       .width(280)
