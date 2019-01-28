10d1a439e2ab1c45a88078c79e2e2a6c89d06c4a
peatroot <garethpeat@gmail.com>
2019-01-28 13:33:10 +0000

    Mock search response

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,41 +7,82 @@ import { AdvancedSearch as OtSearch } from 'ot-ui';
 
 import SearchOption from './SearchOption';
 
-const SEARCH_QUERY = gql`
-  query SearchQuery($queryString: String!) {
-    search(queryString: $queryString) {
-      totalGenes
-      totalVariants
-      totalStudies
-      genes {
-        id
-        symbol
-        chromosome
-        start
-        end
-      }
-      variants {
-        variant {
-          id
-          rsId
-          chromosome
-          position
-          refAllele
-          altAllele
-        }
-      }
-      studies {
-        studyId
-        traitReported
-        pmid
-        pubAuthor
-        pubDate
-        pubJournal
-        nInitial
-      }
-    }
-  }
-`;
+// const SEARCH_QUERY = gql`
+//   query SearchQuery($queryString: String!) {
+//     search(queryString: $queryString) {
+//       totalGenes
+//       totalVariants
+//       totalStudies
+//       genes {
+//         id
+//         symbol
+//         chromosome
+//         start
+//         end
+//       }
+//       variants {
+//         variant {
+//           id
+//           rsId
+//           chromosome
+//           position
+//           refAllele
+//           altAllele
+//         }
+//       }
+//       studies {
+//         studyId
+//         traitReported
+//         pmid
+//         pubAuthor
+//         pubDate
+//         pubJournal
+//         nInitial
+//       }
+//     }
+//   }
+// `;
+
+const MOCK_SEARCH_DATA = {
+  genes: [
+    { id: 'ENSG00000157764', symbol: 'BRAF' },
+    { id: 'ENSG00000089234', symbol: 'BRAP' },
+    { id: 'ENSG00000224775', symbol: 'BRAFP1' },
+    { id: 'ENSG00000171862', symbol: 'PTEN' },
+  ],
+  diseases: [
+    { id: 'EFO_0000756', name: 'melanoma' },
+    { id: 'EFO_0000313', name: 'carcinoma' },
+    { id: 'EFO_0000305', name: 'breast carcinoma' },
+    { id: 'EFO_0000183', name: 'Hodgkins lymphoma' },
+    { id: 'EFO_0000220', name: 'acute lymphoblastic leukemia' },
+    { id: 'EFO_1000068', name: 'acute leukemia' },
+  ],
+};
+
+const mockQuery = queryString => {
+  // console.log(queryString);
+  const queryStringLower = queryString.toLowerCase();
+  const genes = MOCK_SEARCH_DATA.genes.filter(d => {
+    // console.log(d.symbol, d.symbol.startsWith(queryStringLower));
+    return (
+      d.symbol.toLowerCase().startsWith(queryStringLower) ||
+      d.id.toLowerCase().startsWith(queryStringLower)
+    );
+  });
+  const diseases = MOCK_SEARCH_DATA.diseases.filter(
+    d =>
+      d.name.toLowerCase().startsWith(queryStringLower) ||
+      d.id.toLowerCase().startsWith(queryStringLower)
+  );
+  // console.log(genes, diseases);
+  return {
+    totalGenes: genes.length,
+    totalDiseases: diseases.length,
+    genes,
+    diseases,
+  };
+};
 
 const asGroupedOptions = data => {
   return [
@@ -50,12 +91,8 @@ const asGroupedOptions = data => {
       options: data.genes.map(d => ({ ...d, groupType: 'gene' })),
     },
     {
-      label: 'Variants',
-      options: data.variants.map(d => ({ ...d, groupType: 'variant' })),
-    },
-    {
-      label: 'Studies',
-      options: data.studies.map(d => ({ ...d, groupType: 'study' })),
+      label: 'Diseases',
+      options: data.diseases.map(d => ({ ...d, groupType: 'disease' })),
     },
   ];
 };
@@ -84,23 +121,26 @@ class Search extends React.Component {
       return;
     }
 
-    const { client } = this.props;
-    return client
-      .query({
-        query: SEARCH_QUERY,
-        variables: { queryString: inputValue },
-      })
-      .then(response => {
-        if (response.data && response.data.search) {
-          return asGroupedOptions(response.data.search);
-        } else {
-          return asGroupedOptions({
-            genes: [],
-            variants: [],
-            studies: [],
-          });
-        }
-      });
+    // const { client } = this.props;
+    // return client
+    //   .query({
+    //     query: SEARCH_QUERY,
+    //     variables: { queryString: inputValue },
+    //   })
+    //   .then(response => {
+    //     if (response.data && response.data.search) {
+    //       return asGroupedOptions(response.data.search);
+    //     } else {
+    //       return asGroupedOptions({
+    //         genes: [],
+    //         variants: [],
+    //         studies: [],
+    //       });
+    //     }
+    //   });
+    const options = asGroupedOptions(mockQuery(inputValue));
+    console.log(options);
+    return Promise.resolve(options);
   };
   handleFocus = () => {};
   render() {
--- src/components/SearchOption.js
+++ src/components/SearchOption.js
@@ -5,46 +5,14 @@ const Option = ({ data }) => {
   switch (data.groupType) {
     case 'gene':
       return (
-        <SearchOption
-          heading={data.symbol}
-          subheading={`${data.chromosome}:${commaSeparate(
-            data.start
-          )}-${commaSeparate(data.end)}`}
-          extra={data.id}
-        />
+        <SearchOption heading={data.symbol} subheading={''} extra={data.id} />
       );
-    case 'variant':
+    case 'disease':
       return (
-        <SearchOption
-          heading={data.variant.id}
-          subheading={data.variant.rsId}
-        />
-      );
-    case 'study':
-      const pubYear = new Date(data.pubDate).getFullYear();
-      return (
-        <SearchOption
-          heading={data.traitReported}
-          subheading={`${data.pubAuthor} (${pubYear})`}
-          extra={`${
-            data.pubJournal ? data.pubJournal : ''
-          } N Study: ${commaSeparate(data.nInitial)}`}
-        />
-      );
-    case 'study-overlap':
-      return (
-        <SearchOption
-          heading={data.traitReported}
-          subheading={`${data.pubAuthor} (${new Date(
-            data.pubDate
-          ).getFullYear()})`}
-          extra={data.pubJournal}
-          count={`${data.overlappingLociCount} overlapping loci`}
-          proportion={data.rootOverlapProportion}
-        />
+        <SearchOption heading={data.name} subheading={''} extra={data.id} />
       );
     default:
-      throw Error('Unexpected groupType. Should be gene, variant or study.');
+      throw Error('Unexpected groupType. Should be gene or disease.');
   }
 };
 
