461455f5450cfc25badcd8f4fb0bfa7448148334
Alfredo Miranda <alfredo@miranda.io>
2019-03-27 16:02:34 +0000

    Add Intersection component

---
+++ src/components/Intersection.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { PALETTE } from 'ot-ui';
+
+const PADDING = 3;
+const RADIUS = 48;
+
+const styles = () => {
+  return {
+    svg: {
+      display: 'block',
+    },
+  };
+};
+
+const Intersection = ({ classes, id, a, ab, b }) => {
+  return (
+    <svg
+      className={classes.svg}
+      width={2.8 * RADIUS + 6}
+      height={2 * RADIUS + 6}
+    >
+      <defs>
+        <clipPath id={id}>
+          <circle
+            r={RADIUS}
+            cx={1.8 * RADIUS + PADDING}
+            cy={RADIUS + PADDING}
+          />
+        </clipPath>
+      </defs>
+      <circle
+        r={RADIUS}
+        cx={RADIUS + PADDING}
+        cy={RADIUS + PADDING}
+        fill={PALETTE.purple}
+        clipPath={`url(#${id})`}
+      />
+      <circle
+        r={RADIUS}
+        cx={RADIUS + PADDING}
+        cy={RADIUS + PADDING}
+        stroke={PALETTE.darkblue}
+        strokeWidth="3"
+        fill="none"
+      />
+      <circle
+        r={RADIUS}
+        cx={1.8 * RADIUS + PADDING}
+        cy={RADIUS + PADDING}
+        stroke={PALETTE.darkblue}
+        strokeWidth="3"
+        fill="none"
+      />
+      <text x="26" y="50%" textAnchor="middle" fill={PALETTE.darkgrey}>
+        {a}
+      </text>
+      <text x="70" y="50%" textAnchor="middle" fill="white">
+        {ab}
+      </text>
+      <text x="115" y="50%" textAnchor="middle" fill={PALETTE.darkgrey}>
+        {b}
+      </text>
+    </svg>
+  );
+};
+
+export default withStyles(styles)(Intersection);
