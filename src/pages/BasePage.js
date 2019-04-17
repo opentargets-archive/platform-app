5ea4c6b8dc50277f5aabc83f4322aa8002782002
Gareth Peat <peatroot@users.noreply.github.com>
2019-04-17 09:24:13 +0100

    Harmonise use of ot-ui Page component (#40)
    * Harmonise usage of ot-ui Page component
    
    * Remove unused classes prop

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -1,33 +1,20 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
-import Grid from '@material-ui/core/Grid';
-import withStyles from '@material-ui/core/styles/withStyles';
-import { NavBar, Footer } from 'ot-ui';
+import { Page, NavBar, Footer } from 'ot-ui';
 
 import { externalLinks } from '../constants';
 
-const styles = theme => ({
-  container: {
-    padding: '40px 24px',
-  },
-});
+const BasePage = ({ children }) => (
+  <Page
+    header={<NavBar name="Platform" />}
+    footer={<Footer externalLinks={externalLinks} />}
+  >
+    <Helmet
+      defaultTitle="Open Targets Platform"
+      titleTemplate="%s | Open Targets Platform"
+    />
+    {children}
+  </Page>
+);
 
-const BasePage = ({ children, classes }) => {
-  return (
-    <Fragment>
-      <Helmet
-        defaultTitle="Open Targets Platform"
-        titleTemplate="%s | Open Targets Platform"
-      />
-      <NavBar name="Platform" />
-      <Grid className={classes.container} container justify="space-around">
-        <Grid item md={11}>
-          {children}
-        </Grid>
-      </Grid>
-      <Footer externalLinks={externalLinks} />
-    </Fragment>
-  );
-};
-
-export default withStyles(styles)(BasePage);
+export default BasePage;
