1f0311dd2f509e42ac0393c84e137c9eede6d37a
Gareth Peat <peatroot@users.noreply.github.com>
2019-05-02 11:33:40 +0100

    Set up disease page header consistent with target page (#42)
    * Add src/sigma to .gitignore
    
    * Set up the DiseasePage header and tabs

---
+++ src/pages/DiseaseAssociationsPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+
+const DiseaseAssociationsPage = ({ match }) => {
+  const { efoId } = match.params;
+  return <div>association content</div>;
+};
+
+export default DiseaseAssociationsPage;
