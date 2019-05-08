0e1678d84ecf1c102213ddc423bf49181cff37bb
Gareth Peat <peatroot@users.noreply.github.com>
2019-05-08 09:37:10 +0100

    Add gene-tree detail (#45)
    * Add homology skeleton
    
    * Add cttv.targetGeneTree dep
    
    * Get targetGeneTree working
    
    * Add orthology table
    
    * Use speciesId field for ensembl links
    
    * Support orthology table download

--- src/components/OverviewTab.js
+++ src/components/OverviewTab.js
@@ -18,6 +18,7 @@ import RNAAndProteinExpressionWidget from './RNAAndProteinExpression';
 import MousePhenotypesWidget from './MousePhenotypes';
 import TargetTractabilityWidget from './TargetTractability';
 import VariationWidget from './Variation';
+import HomologyWidget from './Homology';
 
 const overviewQuery = gql`
   query TargetQuery($ensgId: String!) {
@@ -137,6 +138,12 @@ const overviewQuery = gql`
             name
           }
         }
+        homology {
+          sources {
+            url
+            name
+          }
+        }
       }
     }
   }
@@ -190,6 +197,7 @@ class OverviewTab extends Component {
             mousePhenotypes,
             tractability,
             variation,
+            homology,
           } = data.target.summaries;
 
           return (
@@ -293,6 +301,13 @@ class OverviewTab extends Component {
                     symbol={symbol}
                     name={name}
                     variation={variation}
+                  />
+                )}
+                {HomologyWidget.widgetName.includes(lowerCaseTerm) && (
+                  <HomologyWidget
+                    ensgId={ensgId}
+                    symbol={symbol}
+                    homology={homology}
                   />
                 )}
               </Grid>
