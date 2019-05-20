287eb8d33ba14d2e436f2c86cbc3f32b728db4d1
Luca Fumis <lucaf@ebi.ac.uk>
2019-05-20 19:34:30 +0100

    Bibliography summary widget; issue opentargets/platform#540

---
+++ src/components/Bibliography/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import FilterTable from './FilterTable';
+
+const BibliographyDetail = ({ ensgId, symbol, sources }) => {
+  return <React.Fragment>Bibliography placeholder</React.Fragment>;
+};
+
+export default BibliographyDetail;
