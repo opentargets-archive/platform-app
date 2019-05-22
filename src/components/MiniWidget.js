5ae94c5b26dd6c51390e602e36375e2b4c77b945
peatroot <garethpeat@gmail.com>
2019-05-22 13:02:20 +0100

    Add scroll to behaviour on mini widgets

--- src/components/MiniWidget.js
+++ src/components/MiniWidget.js
@@ -4,9 +4,9 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
 
-const MiniWidget = ({ name, icon, hasData }) => (
+const MiniWidget = ({ name, icon, hasData, onClick }) => (
   <Grid item xs={4} md={3} lg={2}>
-    <Card style={{ height: '100%' }}>
+    <Card style={{ height: '100%' }} onClick={onClick}>
       <CardHeader
         avatar={
           <Avatar>
--- src/components/MiniWidgetBar.js
+++ src/components/MiniWidgetBar.js
@@ -3,11 +3,11 @@ import Grid from '@material-ui/core/Grid';
 
 import MiniWidget from './MiniWidget';
 
-const MiniWidgetBar = ({ data }) => (
+const MiniWidgetBar = ({ data, onWidgetClick }) => (
   <div style={{ paddingTop: 8, paddingBottom: 8 }}>
     <Grid container spacing={8}>
       {data.map(d => (
-        <MiniWidget key={d.id} {...d} />
+        <MiniWidget key={d.id} {...d} onClick={() => onWidgetClick(d.id)} />
       ))}
     </Grid>
   </div>
