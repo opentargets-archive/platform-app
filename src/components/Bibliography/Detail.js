9be9ed4b548f23efd9d16855125b163210b73e71
Luca Fumis <lucaf@ebi.ac.uk>
2019-05-23 18:27:14 +0100

    Add detail view; issue opentargets/platform#542

--- src/components/Bibliography/Detail.js
+++ src/components/Bibliography/Detail.js
@@ -1,9 +1,162 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import Select from 'react-select';
+import Chip from '@material-ui/core/Chip';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-import FilterTable from './FilterTable';
+import { Typography, Button } from 'ot-ui';
 
-const BibliographyDetail = ({ ensgId, symbol, sources }) => {
-  return <React.Fragment>Bibliography placeholder</React.Fragment>;
-};
+import Publication from './Publication';
 
-export default BibliographyDetail;
+const linkUrl = 'https://link.opentargets.io/';
+const aggtype = [
+  { value: 'top_chunks_significant_terms', label: 'Concepts' },
+  { value: 'genes', label: 'Genes' },
+  { value: 'diseases', label: 'Diseases' },
+  // {value: 'phenotypes', label: 'Phenotypes'}, // phenotypes don't return any hits at the moment, so leaving out...
+  { value: 'drugs', label: 'Drugs' },
+  { value: 'journal_abbr_significant_terms', label: 'Journal' },
+  { value: 'authors_significant_terms', label: 'Authors' },
+  // {value: 'pub_date_histogram', label: 'publication date'}
+];
+const styles = theme => ({
+  icon: {
+    width: '50px',
+    height: '50px',
+    fill: '#5a5f5f',
+  },
+  iconNoData: {
+    fill: '#e2dfdf',
+  },
+});
+
+class BibliographyDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bibliographyCount: 0,
+      hasData: false,
+      hasError: false,
+      aggregations: {},
+      selectedAggregation: aggtype[0],
+      hits: [],
+    };
+  }
+
+  // Handler for drop down menu
+  aggtypeFilterHandler = selection => {
+    // console.log(selection);
+    this.setState({ selectedAggregation: selection });
+  };
+
+  // Get the aggregations, i.e. the data for the chips
+  getAggregationsData = ensgId => {
+    fetch(linkUrl + 'search?query=' + ensgId + '&aggs=true&size=0')
+      .then(res => res.json())
+      .then(
+        resp => {
+          this.setState({
+            bibliographyCount: resp.hits.total,
+            hasData: resp.hits.total > 0,
+            aggregations: resp.aggregations,
+          });
+        },
+        error => {
+          this.setState({
+            aggregations: {},
+            hasError: true,
+          });
+        }
+      );
+  };
+
+  // Get the data for the publications
+  getLiteratureData = ensgId => {
+    fetch(linkUrl + 'search?query=' + ensgId)
+      .then(res => res.json())
+      .then(
+        resp => {
+          console.log('literature: ', resp.hits);
+          this.setState({
+            hits: [resp.hits],
+          });
+        },
+        error => {
+          this.setState({
+            hits: [],
+            hasError: true,
+          });
+        }
+      );
+  };
+
+  componentDidMount() {
+    const { ensgId } = this.props;
+
+    // we make 2 calls: one for chips and one for papers
+    // get aggregation data for chips
+    this.getAggregationsData(ensgId);
+
+    // get papers
+    this.getLiteratureData(ensgId);
+  }
+
+  render() {
+    // console.log(this.props);
+    const {
+      bibliographyCount,
+      aggregations,
+      selectedAggregation,
+      hits,
+    } = this.state;
+    const { classes } = this.props;
+    console.log('hits: ', this.state.hits);
+    return (
+      <Fragment>
+        {/* Chips */}
+        <Fragment>
+          {aggregations[selectedAggregation.value]
+            ? aggregations[selectedAggregation.value].buckets.map((agg, i) => (
+                <Chip color="secondary" key={i} label={agg.label || agg.key} />
+              ))
+            : null}
+        </Fragment>
+
+        {/* Dropdown menu */}
+        <Select
+          options={aggtype}
+          defaultValue={selectedAggregation}
+          onChange={this.aggtypeFilterHandler}
+        />
+
+        {/* Total result */}
+        <Typography>Showing 10 of {bibliographyCount} results</Typography>
+
+        {/* Publications */}
+        <Fragment>
+          {(hits[0] || { hits: [] }).hits.map((hit, i) => {
+            return (
+              <Publication
+                key={i}
+                pmId={hit._source.pub_id}
+                title={hit._source.title}
+                authors={hit._source.authors || []}
+                journal={{
+                  title: hit._source.journal.title,
+                  date: hit._source.pub_date,
+                  ref: hit._source.journal_reference,
+                }}
+              />
+            );
+          })}
+        </Fragment>
+
+        {/* Load more */}
+        <Fragment>
+          <Button>Load more papers</Button>
+        </Fragment>
+      </Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(BibliographyDetail);
