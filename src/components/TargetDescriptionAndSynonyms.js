c42c203d3412d676747a16049bdfb4e706c68d60
peatroot <garethpeat@gmail.com>
2019-05-28 09:52:01 +0100

    Place description and synonyms side by side on large screens

--- src/components/TargetDescriptionAndSynonyms.js
+++ src/components/TargetDescriptionAndSynonyms.js
@@ -7,7 +7,7 @@ import LongText from './LongText';
 
 const summaryStyles = theme => ({
   description: {
-    marginBottom: '30px',
+    margin: '30px 0',
   },
   synonymText: {
     display: 'inline-block',
@@ -23,34 +23,65 @@ const summaryStyles = theme => ({
   },
 });
 
+// const TargetDescriptionAndSynonyms = ({ classes, synonyms, description }) => (
+//   <Fragment>
+//     <Grid className={classes.description} container>
+//       <Typography variant="body2">
+//         <LongText lineLimit={3}>{description}</LongText>
+//       </Typography>
+//     </Grid>
+//     <Grid container>
+//       <Grid item md={1}>
+//         <Typography className={classes.synonymText} variant="subtitle2">
+//           Synonyms:
+//         </Typography>
+//       </Grid>
+//       <Grid item md={10}>
+//         {synonyms.map(synonym => {
+//           return (
+//             <Typography
+//               key={synonym}
+//               className={classes.synonym}
+//               variant="caption"
+//             >
+//               {synonym}
+//             </Typography>
+//           );
+//         })}
+//       </Grid>
+//     </Grid>
+//   </Fragment>
+// );
+
 const TargetDescriptionAndSynonyms = ({ classes, synonyms, description }) => (
-  <Fragment>
-    <Grid className={classes.description} container>
+  <Grid className={classes.description} container>
+    <Grid item xs={12} md={6}>
       <Typography variant="body2">
-        <LongText lineLimit={3}>{description}</LongText>
+        {description ? (
+          <LongText lineLimit={3}>{description}</LongText>
+        ) : (
+          'No description available.'
+        )}
       </Typography>
     </Grid>
-    <Grid container>
-      <Grid item md={1}>
-        <Typography className={classes.synonymText} variant="subtitle2">
-          Synonyms:
-        </Typography>
-      </Grid>
-      <Grid item md={10}>
-        {synonyms.map(synonym => {
-          return (
-            <Typography
-              key={synonym}
-              className={classes.synonym}
-              variant="caption"
-            >
-              {synonym}
-            </Typography>
-          );
-        })}
-      </Grid>
+    <Grid item xs={12} md={6}>
+      <Typography className={classes.synonymText} variant="subtitle2">
+        Synonyms
+      </Typography>
+      <br />
+      {synonyms.map(synonym => {
+        return (
+          <Typography
+            key={synonym}
+            className={classes.synonym}
+            variant="caption"
+          >
+            {synonym}
+          </Typography>
+        );
+      })}
     </Grid>
-  </Fragment>
+  </Grid>
 );
 
 export default withStyles(summaryStyles)(TargetDescriptionAndSynonyms);
