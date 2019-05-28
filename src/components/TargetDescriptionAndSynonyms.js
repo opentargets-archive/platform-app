f842d14cf73e7280de6407fe399755fedb6aae8b
peatroot <garethpeat@gmail.com>
2019-05-28 15:22:34 +0100

    Add right padding column in description/synonyms when wide

--- src/components/TargetDescriptionAndSynonyms.js
+++ src/components/TargetDescriptionAndSynonyms.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Hidden from '@material-ui/core/Hidden';
 import Grid from '@material-ui/core/Grid';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
@@ -7,7 +8,7 @@ import LongText from './LongText';
 
 const summaryStyles = theme => ({
   description: {
-    margin: '30px 0',
+    margin: '10px 0',
   },
   synonymText: {
     display: 'inline-block',
@@ -23,39 +24,9 @@ const summaryStyles = theme => ({
   },
 });
 
-// const TargetDescriptionAndSynonyms = ({ classes, synonyms, description }) => (
-//   <Fragment>
-//     <Grid className={classes.description} container>
-//       <Typography variant="body2">
-//         <LongText lineLimit={3}>{description}</LongText>
-//       </Typography>
-//     </Grid>
-//     <Grid container>
-//       <Grid item md={1}>
-//         <Typography className={classes.synonymText} variant="subtitle2">
-//           Synonyms:
-//         </Typography>
-//       </Grid>
-//       <Grid item md={10}>
-//         {synonyms.map(synonym => {
-//           return (
-//             <Typography
-//               key={synonym}
-//               className={classes.synonym}
-//               variant="caption"
-//             >
-//               {synonym}
-//             </Typography>
-//           );
-//         })}
-//       </Grid>
-//     </Grid>
-//   </Fragment>
-// );
-
 const TargetDescriptionAndSynonyms = ({ classes, synonyms, description }) => (
   <Grid className={classes.description} container>
-    <Grid item xs={12} md={6}>
+    <Grid item xs={12} md={5}>
       <Typography className={classes.synonymText} variant="subtitle2">
         Description
       </Typography>
@@ -68,23 +39,23 @@ const TargetDescriptionAndSynonyms = ({ classes, synonyms, description }) => (
         )}
       </Typography>
     </Grid>
-    <Grid item xs={12} md={6}>
+    <Hidden smDown>
+      <Grid item md={1} />
+    </Hidden>
+    <Grid item xs={12} md={5}>
       <Typography className={classes.synonymText} variant="subtitle2">
         Synonyms
       </Typography>
       <br />
-      {synonyms.map(synonym => {
-        return (
-          <Typography
-            key={synonym}
-            className={classes.synonym}
-            variant="caption"
-          >
-            {synonym}
-          </Typography>
-        );
-      })}
+      {synonyms.map(synonym => (
+        <Typography key={synonym} className={classes.synonym} variant="caption">
+          {synonym}
+        </Typography>
+      ))}
     </Grid>
+    <Hidden smDown>
+      <Grid item md={1} />
+    </Hidden>
   </Grid>
 );
 
