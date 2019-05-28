a558a3878d3056f0b1e0dd724d506c69aa970b31
Luca Fumis <lucaf@ebi.ac.uk>
2019-05-28 17:57:24 +0100

    Updated bibliography props to non-target specific

--- src/components/Bibliography/index.js
+++ src/components/Bibliography/index.js
@@ -69,6 +69,7 @@ class BibliographyWidget extends Component {
             ensgId={ensgId}
             symbol={symbol}
             sources={sources}
+            searchTerm={{ key: ensgId, label: symbol }}
           />
         }
         detailHeader={{
