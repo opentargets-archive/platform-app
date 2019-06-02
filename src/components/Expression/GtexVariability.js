cfdf09648904c1ffd24d77c9bc57e0566df07520
Alfredo Miranda <alfredo@miranda.io>
2019-06-02 16:40:49 +0100

    Display boxes

--- src/components/Expression/GtexVariability.js
+++ src/components/Expression/GtexVariability.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
-const width = 500;
-const boxHeight = 10;
-const margin = { top: 40, right: 20, bottom: 20, left: 100 };
+const width = 800;
+const boxHeight = 20;
+const boxPadding = boxHeight / 4;
+const margin = { top: 40, right: 20, bottom: 20, left: 220 };
 
 class GtexVariability extends Component {
   boxPlotRef = React.createRef();
@@ -12,27 +13,31 @@ class GtexVariability extends Component {
   xAxis = d3.axisTop();
   yAxis = d3.axisLeft();
   x = d3.scaleLinear();
-  y = d3.scaleBand();
+  y = d3.scalePoint().padding(0.5);
 
   render() {
     const { data } = this.props;
 
-    const height = data.length * boxHeight;
-
-    console.log('data', data);
+    const height = data.length * boxHeight + margin.top + margin.bottom;
 
     return (
       <svg height={height} width={width}>
-        <text x="35" y="15">
+        <text x={margin.left} y="15">
           Normalised expression (RPKM)
         </text>
         <g
+          className="boxplot"
           ref={this.boxPlotRef}
           transform={`translate(${margin.left}, ${margin.top})`}
-        >
-          <g ref={this.xAxisRef} />
-          <g ref={this.yAxisRef} transform={`translate(${margin.left}, 0)`} />
-        </g>
+        />
+        <g
+          ref={this.xAxisRef}
+          transform={`translate(${margin.left}, ${margin.top})`}
+        />
+        <g
+          ref={this.yAxisRef}
+          transform={`translate(${margin.left}, ${margin.top})`}
+        />
       </svg>
     );
   }
@@ -46,12 +51,26 @@ class GtexVariability extends Component {
   }
 
   _render() {
+    const { x, y } = this;
     const data = this.props.data
       .slice()
       .sort((a, b) => b.data.median - a.data.median);
 
-    const height = data.length * boxHeight;
-    /* const boxPlot = d3.select(this.boxPlotRef.current);
+    console.log('data', data);
+
+    const height = data.length * boxHeight + margin.top + margin.bottom;
+    const rectHeight = boxHeight - 2 * boxPadding;
+    const xMax = d3.max(data, d => {
+      return d3.max(d.data.outliers);
+    });
+
+    x.domain([0, xMax]).range([0, width - margin.left - margin.right]);
+    y.domain(data.map(d => d.tissueSiteDetailId.replace(/_/g, ' '))).range([
+      0,
+      height - margin.top - margin.bottom,
+    ]);
+
+    const boxPlot = d3.select(this.boxPlotRef.current);
 
     const boxContainer = boxPlot
       .selectAll('g')
@@ -59,21 +78,28 @@ class GtexVariability extends Component {
       .enter()
       .append('g');
 
-    boxContainer.append('rect');
+    boxContainer
+      .append('line')
+      .attr('x1', d => x(d.data.lowerLimit))
+      .attr('x2', d => x(d.data.upperLimit))
+      .attr('y1', d => y(d.tissueSiteDetailId.replace(/_/g, ' ')))
+      .attr('y2', d => y(d.tissueSiteDetailId.replace(/_/g, ' ')))
+      .attr('stroke', 'black');
 
-    boxContainer.append('line');
-    boxContainer.append('line');
-    */
+    boxContainer
+      .append('rect')
+      .attr('x', d => x(d.data.q1))
+      .attr(
+        'y',
+        d => y(d.tissueSiteDetailId.replace(/_/g, ' ')) - rectHeight / 2
+      )
+      .attr('width', d => x(d.data.q3) - x(d.data.q1))
+      .attr('height', rectHeight);
 
-    // this.x.domain().range();
-    this.y
-      .domain(data.map(d => d.tissueSiteDetailId.replace(/_/g, ' ')))
-      .range([0, height - margin.top - margin.bottom]);
+    const xAxis = d3.axisTop(x);
+    const yAxis = d3.axisLeft(y);
 
-    // const xAxis = d3.axisTop(this.x);
-    const yAxis = d3.axisLeft(this.y);
-
-    // d3.select(this.xAxisRef.current).call(xAxis);
+    d3.select(this.xAxisRef.current).call(xAxis);
     d3.select(this.yAxisRef.current).call(yAxis);
   }
 }
