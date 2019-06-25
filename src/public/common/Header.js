426d94bf68416fbad9dc6b11165a74a983a3b947
peatroot <garethpeat@gmail.com>
2019-06-25 11:16:42 +0100

    Refactor header for common functionality

---
+++ src/public/common/Header.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from 'react';
+import Grid from '@material-ui/core/Grid';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  titleContainer: {
+    marginBottom: '10px',
+  },
+  mainIcon: {
+    width: '40px !important',
+    height: '65px !important',
+    fill: `${theme.palette.primary.main} !important`,
+    marginRight: '12px !important',
+  },
+  title: {
+    color: theme.palette.primary.main,
+    fontWeight: 500,
+  },
+  subtitle: {
+    display: 'flex',
+    paddingLeft: '5px',
+    alignItems: 'center',
+  },
+});
+
+const Header = ({
+  classes,
+  Icon,
+  title,
+  subtitle,
+  externalLinks,
+  rightContent,
+}) => (
+  <Fragment>
+    <Grid className={classes.titleContainer} container justify="space-between">
+      <Grid item>
+        <Grid container>
+          <Grid item>
+            <Icon classes={{ root: classes.mainIcon }} />
+          </Grid>
+          <Grid item>
+            <Grid container>
+              <Typography className={classes.title} variant="h4">
+                {title}
+              </Typography>
+              <Typography className={classes.subtitle} variant="subtitle2">
+                {subtitle}
+              </Typography>
+            </Grid>
+            <Grid container>
+              <Typography>{externalLinks}</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>{rightContent}</Grid>
+    </Grid>
+  </Fragment>
+);
+
+export default withStyles(styles)(Header);
--- src/public/target/Header.js
+++ src/public/target/Header.js
@@ -1,9 +1,8 @@
-import React, { Fragment } from 'react';
-import Grid from '@material-ui/core/Grid';
-import withStyles from '@material-ui/core/styles/withStyles';
-import Typography from '@material-ui/core/Typography';
+import React from 'react';
 
+import Header from '../common/Header';
 import TargetIcon from '../../icons/TargetIcon';
+import GeneticsLink from './GeneticsLink';
 import Ensembl from './externalLinks/Ensembl';
 import UniProt from './externalLinks/UniProt';
 import GeneCards from './externalLinks/GeneCards';
@@ -11,80 +10,23 @@ import HGNC from './externalLinks/HGNC';
 import TEP from './externalLinks/TEP';
 import CRISPRdepmap from './externalLinks/CRISPRdepmap';
 
-const summaryStyles = theme => ({
-  titleContainer: {
-    marginBottom: '10px',
-  },
-  targetIcon: {
-    width: '40px',
-    height: '65px',
-    fill: theme.palette.primary.main,
-    marginRight: '12px',
-  },
-  symbol: {
-    color: theme.palette.primary.main,
-    fontWeight: 500,
-  },
-  name: {
-    display: 'flex',
-    paddingLeft: '5px',
-    alignItems: 'center',
-  },
-  geneticsLink: {
-    alignItems: 'center',
-    display: 'flex',
-    height: '35px',
-    fontSize: '13px',
-    fontWeight: '500',
-    backgroundColor: theme.palette.primary.main,
-    color: 'white',
-    padding: '0 10px',
-    textDecoration: 'none',
-  },
-});
-
-const TargetHeader = ({ classes, ensgId, symbol, name, uniprotId }) => (
-  <Fragment>
-    <Grid className={classes.titleContainer} container justify="space-between">
-      <Grid item>
-        <Grid container>
-          <Grid item>
-            <TargetIcon className={classes.targetIcon} />
-          </Grid>
-          <Grid item>
-            <Grid container>
-              <Typography className={classes.symbol} variant="h4">
-                {symbol}
-              </Typography>
-              <Typography className={classes.name} variant="subtitle2">
-                {name}
-              </Typography>
-            </Grid>
-            <Grid container>
-              <Typography>
-                <Ensembl ensgId={ensgId} first />
-                <UniProt uniprotId={uniprotId} />
-                <GeneCards symbol={symbol} />
-                <HGNC symbol={symbol} />
-                <CRISPRdepmap symbol={symbol} />
-                <TEP ensgId={ensgId} symbol={symbol} />
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <a
-          className={classes.geneticsLink}
-          href={`https://genetics.opentargets.org/gene/${ensgId}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View {symbol} profile in Open Targets Genetics
-        </a>
-      </Grid>
-    </Grid>
-  </Fragment>
+const TargetHeader = ({ ensgId, uniprotId, symbol, name }) => (
+  <Header
+    title={symbol}
+    subtitle={name}
+    Icon={TargetIcon}
+    externalLinks={
+      <React.Fragment>
+        <Ensembl ensgId={ensgId} first />
+        <UniProt uniprotId={uniprotId} />
+        <GeneCards symbol={symbol} />
+        <HGNC symbol={symbol} />
+        <CRISPRdepmap symbol={symbol} />
+        <TEP ensgId={ensgId} symbol={symbol} />
+      </React.Fragment>
+    }
+    rightContent={<GeneticsLink ensgId={ensgId} symbol={symbol} />}
+  />
 );
 
-export default withStyles(summaryStyles)(TargetHeader);
+export default TargetHeader;
