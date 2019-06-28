0aae8abfc2003b448892da68b0fd7c1e759bb842
peatroot <garethpeat@gmail.com>
2019-06-28 15:31:14 +0100

    Abstract out common bibliography summary

---
+++ src/public/common/sections/Bibliography/Summary.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { commaSeparate } from 'ot-ui';
+
+import { getStats } from './custom/Api';
+
+class Summary extends React.Component {
+  state = {
+    bibliographyCount: 0,
+  };
+  componentDidMount() {
+    const { key, setHasSummaryData, setHasSummaryError } = this.props;
+    getStats([{ key }]).then(
+      result => {
+        this.setState({
+          bibliographyCount: result.hits.total,
+        });
+        setHasSummaryData(result.hits.total > 0);
+      },
+      error => {
+        setHasSummaryError(true);
+      }
+    );
+  }
+  render() {
+    const { bibliographyCount } = this.state;
+    return (
+      <React.Fragment>
+        {commaSeparate(bibliographyCount)} publication
+        {bibliographyCount === 1 ? '' : 's'}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Summary;
--- src/public/disease/sections/Bibliography/Summary.js
+++ src/public/disease/sections/Bibliography/Summary.js
@@ -1,36 +1,9 @@
 import React from 'react';
 
-import { commaSeparate } from 'ot-ui';
+import BibliographySummary from '../../../common/sections/Bibliography/Summary';
 
-import { getStats } from '../../../common/sections/Bibliography/custom/Api';
-
-class Summary extends React.Component {
-  state = {
-    bibliographyCount: 0,
-  };
-  componentDidMount() {
-    const { efoId, setHasSummaryData, setHasSummaryError } = this.props;
-    getStats([{ key: efoId }]).then(
-      result => {
-        this.setState({
-          bibliographyCount: result.hits.total,
-        });
-        setHasSummaryData(result.hits.total > 0);
-      },
-      error => {
-        setHasSummaryError(true);
-      }
-    );
-  }
-  render() {
-    const { bibliographyCount } = this.state;
-    return (
-      <React.Fragment>
-        {commaSeparate(bibliographyCount)} publication
-        {bibliographyCount === 1 ? '' : 's'}
-      </React.Fragment>
-    );
-  }
-}
+const Summary = ({ efoId, ...rest }) => (
+  <BibliographySummary key={efoId} {...rest} />
+);
 
 export default Summary;
