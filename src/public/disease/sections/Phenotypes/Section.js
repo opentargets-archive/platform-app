f23664a06aeae08e8df197fa9021796e5ce1280b
peatroot <garethpeat@gmail.com>
2019-07-01 11:33:44 +0100

    Add phenotypes section

--- src/public/disease/sections/Phenotypes/Section.js
+++ src/public/disease/sections/Phenotypes/Section.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import * as d3 from 'd3';
 
-const Section = ({ name }) => (
-  <React.Fragment>
-    TODO: Write a section component for <strong>{name}</strong>.
-  </React.Fragment>
-);
+import { Link } from 'ot-ui';
+
+const Section = ({ name, data }) => {
+  const { rows } = data;
+  return (
+    <ul>
+      {rows
+        .sort((a, b) =>
+          d3.ascending(a.name.toLowerCase(), b.name.toLowerCase())
+        )
+        .map(d => (
+          <li key={d.id}>
+            <Link external to={d.url}>
+              {d.name}
+            </Link>
+          </li>
+        ))}
+    </ul>
+  );
+};
 
 export default Section;
