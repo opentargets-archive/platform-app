1f4a5a8338169664c58d7c08cbef07f289c59a71
peatroot <garethpeat@gmail.com>
2019-07-03 11:52:11 +0100

    Update disease descriptions

--- src/public/disease/sections/KnownDrugs/Description.js
+++ src/public/disease/sections/KnownDrugs/Description.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Description = ({ name }) => (
   <React.Fragment>
-    TODO: Write a description component for <strong>{name}</strong>.
+    Drugs in clinical trials or approved for <strong>{name}</strong>.
   </React.Fragment>
 );
 
