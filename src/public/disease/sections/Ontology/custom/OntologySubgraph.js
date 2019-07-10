24380a13e6642c7248cb22b2dab37a5c6221f2e8
peatroot <garethpeat@gmail.com>
2019-07-10 09:57:54 +0100

    Make OntologySubgraph horizontal

--- src/public/disease/sections/Ontology/custom/OntologySubgraph.js
+++ src/public/disease/sections/Ontology/custom/OntologySubgraph.js
@@ -2,8 +2,56 @@ import React from 'react';
 import { withContentRect } from 'react-measure';
 import * as d3Base from 'd3';
 import * as d3DagBase from 'd3-dag';
+import withTheme from '@material-ui/core/styles/withTheme';
 
 const d3 = Object.assign({}, d3Base, d3DagBase);
+
+const separatorIgnoreEdges = (a, b) =>
+  (a.data !== undefined) + (b.data !== undefined);
+
+const getLayoutGenerator = (innerWidth, innerHeight) =>
+  d3
+    .sugiyama()
+    .size([innerHeight, innerWidth])
+    .layering(d3.layeringLongestPath().topDown(false))
+    .decross(d3.decrossTwoLayer())
+    .coord(d3.coordGreedy())
+    .separation(separatorIgnoreEdges);
+
+const getMaxLayerCount = (dag, edgeSeparation = false) => {
+  // use layout (but only to get layer counts, coords don't matter here)
+  const layout = getLayoutGenerator(100, 100);
+  layout(dag);
+
+  // init
+  const layerCount = dag.reduce((c, n) => Math.max(c, n.layer), 0) + 1;
+  const layerNodeCounts = Array(layerCount + 1).fill(0);
+  const layerDummyNodeCounts = Array(layerCount + 1).fill(0);
+
+  // nodes and dummy nodes (for edges) per layer
+  dag.each(n => {
+    layerNodeCounts[n.layer] += 1;
+  });
+  dag.links().forEach(l =>
+    l.data.points.forEach((_, i) => {
+      layerDummyNodeCounts[l.source.layer + i + 1] += 1;
+    })
+  );
+
+  // overall per layer
+  const layerCounts = Array(layerCount).fill(0);
+  for (let i = 0; i < layerCount; i++) {
+    if (!edgeSeparation && i === layerCount - 1) {
+      layerCounts[i] = layerNodeCounts[i];
+    } else {
+      layerCounts[i] = layerNodeCounts[i] + layerDummyNodeCounts[i];
+    }
+  }
+
+  return edgeSeparation
+    ? Math.max(...layerCounts)
+    : Math.max(...layerNodeCounts);
+};
 
 class DAGViewer extends React.Component {
   state = {};
@@ -14,23 +62,21 @@ class DAGViewer extends React.Component {
   }
 
   render() {
-    const { measureRef, efoId, subgraph } = this.props;
+    const { measureRef, efoId, subgraph, theme } = this.props;
     const { width } = this.state;
-    const height = Math.min(width, 700);
+    // const height = Math.min(width, 700);
     const margin = { top: 50, right: 80, bottom: 50, left: 80 };
     const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
 
     // create dag
     let dag = d3.dagStratify()(Object.values(subgraph));
 
+    // compute height (based on dag nodes per layer)
+    const height = margin.top + margin.bottom + getMaxLayerCount(dag) * 10;
+    const innerHeight = height - margin.top - margin.bottom;
+
     // create layout generator
-    let layout = d3
-      .sugiyama()
-      .size([innerWidth, innerHeight])
-      .layering(d3.layeringLongestPath())
-      .decross(d3.decrossTwoLayer())
-      .coord(d3.coordGreedy());
+    const layout = getLayoutGenerator(innerWidth, innerHeight);
 
     // compute layout
     layout(dag);
@@ -38,9 +84,24 @@ class DAGViewer extends React.Component {
     // edge generator
     const line = d3
       .line()
-      .curve(d3.curveMonotoneY)
-      .x(d => d.x)
-      .y(d => d.y);
+      .curve(d3.curveMonotoneX)
+      .x(d => d.y)
+      .y(d => d.x);
+
+    // colors
+    const colorMap = {
+      ancestor: theme.palette.primary.main,
+      chosen: theme.palette.secondary.main,
+      descendant: theme.palette.primary.light,
+    };
+
+    const nodeRadius = 4;
+    const nodeStrokeColor = theme.palette.grey[300];
+    const edgeStrokeColor = theme.palette.grey[300];
+
+    // legend
+    const xLegendSeparation = 80;
+    const yLegend = -20;
 
     return (
       <div ref={measureRef}>
@@ -52,11 +113,34 @@ class DAGViewer extends React.Component {
             viewBox={`0 0 ${width} ${height}`}
           >
             <g transform={`translate(${margin.left},${margin.top})`}>
+              <circle
+                cx={0}
+                cy={yLegend}
+                r={nodeRadius}
+                fill={colorMap.chosen}
+                stroke={nodeStrokeColor}
+              />
+              <circle
+                cx={xLegendSeparation}
+                cy={yLegend}
+                r={nodeRadius}
+                fill={colorMap.ancestor}
+                stroke={nodeStrokeColor}
+              />
+              <circle
+                cx={xLegendSeparation * 2}
+                cy={yLegend}
+                r={nodeRadius}
+                fill={colorMap.descendant}
+                stroke={nodeStrokeColor}
+              />
+            </g>
+            <g transform={`translate(${margin.left},${margin.top})`}>
               {dag.links().map(d => (
                 <path
                   key={`${d.source.id}-${d.target.id}`}
                   fill="none"
-                  stroke="#bbb"
+                  stroke={edgeStrokeColor}
                   d={line(d.data.points)}
                 />
               ))}
@@ -65,11 +149,11 @@ class DAGViewer extends React.Component {
               {dag.descendants().map(d => (
                 <circle
                   key={d.id}
-                  fill="blue"
-                  stroke="#bbb"
-                  cx={d.x}
-                  cy={d.y}
-                  r={4}
+                  fill={colorMap[d.data.nodeType]}
+                  stroke={nodeStrokeColor}
+                  cx={d.y}
+                  cy={d.x}
+                  r={nodeRadius}
                 />
               ))}
             </g>
@@ -80,4 +164,4 @@ class DAGViewer extends React.Component {
   }
 }
 
-export default withContentRect('bounds')(DAGViewer);
+export default withTheme()(withContentRect('bounds')(DAGViewer));
