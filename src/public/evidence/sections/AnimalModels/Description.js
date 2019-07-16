3b4f18293391ed513c34b92e6c216834917ad9d4
Gareth Peat <peatroot@users.noreply.github.com>
2019-07-16 16:44:02 +0100

    Add animal models evidence section (#72)
    * Add animal models evidence summary
    
    * Add animal models evidence detail
    
    * Reuse MouseModelAllelicComposition

--- src/public/evidence/sections/AnimalModels/Description.js
+++ src/public/evidence/sections/AnimalModels/Description.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import { Link } from 'ot-ui';
+
 const Description = ({ target, disease }) => (
   <React.Fragment>
-    TODO: Write Description component for <strong>{target.symbol}</strong> and{' '}
-    <strong>{disease.name}</strong>.
+    Animal models relating <strong>{target.symbol}</strong> and{' '}
+    <strong>{disease.name}</strong>, mapping between human and mouse phenotypes
+    through{' '}
+    <Link
+      external
+      to="https://docs.targetvalidation.org/data-sources/animal-models#phenodigm"
+    >
+      PhenoDigm
+    </Link>
+    .
   </React.Fragment>
 );
 
