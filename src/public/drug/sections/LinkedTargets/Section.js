13aaed9b1df50d5d0e60084b53de59994eedb9a5
peatroot <garethpeat@gmail.com>
2019-07-19 12:18:48 +0100

    Remove ensembl link from linked targets

--- src/public/drug/sections/LinkedTargets/Section.js
+++ src/public/drug/sections/LinkedTargets/Section.js
@@ -8,20 +8,6 @@ const columns = [
     label: 'Symbol',
     renderCell: d => <Link to={`/target/${d.id}`}>{d.symbol}</Link>,
   },
-  {
-    id: 'id',
-    label: 'Ensembl ID',
-    renderCell: d => (
-      <Link
-        external
-        to={`https://www.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=${
-          d.id
-        }`}
-      >
-        {d.id}
-      </Link>
-    ),
-  },
 ];
 
 const Section = ({ data }) => (
