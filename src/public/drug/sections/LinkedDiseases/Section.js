24d021f60b0cc399d46f602b18c70c9ed6d5b426
peatroot <garethpeat@gmail.com>
2019-07-19 12:17:31 +0100

    Add therapeutic areas to linked diseases

--- src/public/drug/sections/LinkedDiseases/Section.js
+++ src/public/drug/sections/LinkedDiseases/Section.js
@@ -9,13 +9,15 @@ const columns = [
     renderCell: d => <Link to={`/disease/${d.id}`}>{d.name}</Link>,
   },
   {
-    id: 'id',
-    label: 'EFO ID',
-    renderCell: d => (
-      <Link external to={`http://www.ebi.ac.uk/efo/${d.id}`}>
-        {d.id}
-      </Link>
-    ),
+    id: 'therapeuticAreas',
+    label: 'Therapeutic Areas',
+    renderCell: d =>
+      d.therapeuticAreas.map((t, i) => (
+        <React.Fragment key={i}>
+          {i > 0 ? ', ' : null}
+          <Link to={`/disease/${t.id}`}>{t.name}</Link>
+        </React.Fragment>
+      )),
   },
 ];
 
