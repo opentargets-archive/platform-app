afb9701d9f2074dc142af7d981d288fb8102157e
peatroot <garethpeat@gmail.com>
2019-07-23 15:24:58 +0100

    Dynamic import of protvista

--- src/public/target/sections/ProteinInformation/custom/ProtVistaRenderer.js
+++ src/public/target/sections/ProteinInformation/custom/ProtVistaRenderer.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import ProtVista from 'ProtVista';
+import * as d3 from 'd3';
+
 import 'ProtVista/build/css/main.css';
 
 class ProtVistaRenderer extends React.Component {
+  state = {};
   componentDidMount() {
-    const { uniprotId } = this.props;
-    this.protVista = new ProtVista({
-      el: '#protvista',
-      uniprotacc: uniprotId,
-      exclusions: ['seqInfo'],
+    import('ProtVista').then(({ default: ProtVista }) => {
+      this.setState({ ProtVista });
     });
+  }
+  componentDidUpdate() {
+    const { ProtVista } = this.state;
+    if (ProtVista) {
+      // remove any previous content
+      d3.select('#protvista')
+        .selectAll('*')
+        .remove();
+
+      // restart the plugin
+      const { uniprotId } = this.props;
+      this.protVista = new ProtVista({
+        el: '#protvista',
+        uniprotacc: uniprotId,
+        exclusions: ['seqInfo'],
+      });
+    }
   }
   render() {
     return <div id="protvista" />;
