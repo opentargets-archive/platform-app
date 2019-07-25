7fd995dd310b9011350565729bf700afa6b63808
Alfredo Miranda <alfredo@miranda.io>
2019-07-25 09:57:55 +0100

    Code split by page

--- src/public/App.js
+++ src/public/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component, Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 
 import { OtUiThemeProvider } from 'ot-ui';
-
-import HomePage from './home/Page';
-import TargetPage from './target/Page';
-import DiseasePage from './disease/Page';
-import DrugPage from './drug/Page';
-import EvidencePage from './evidence/Page';
 import initLocalStorage from './common/initLocalStorage';
 import theme from './theme';
+
+const HomePage = lazy(() => import('./home/Page'));
+const TargetPage = lazy(() => import('./target/Page'));
+const DiseasePage = lazy(() => import('./disease/Page'));
+const DrugPage = lazy(() => import('./drug/Page'));
+const EvidencePage = lazy(() => import('./evidence/Page'));
 
 const client = new ApolloClient({
   link: new HttpLink({
@@ -31,13 +31,18 @@ class App extends Component {
       <ApolloProvider client={client}>
         <OtUiThemeProvider theme={theme}>
           <Router>
-            <React.Fragment>
-              <Route exact path="/" component={HomePage} />
-              <Route path="/target/:ensgId" component={TargetPage} />
-              <Route path="/disease/:efoId" component={DiseasePage} />
-              <Route path="/drug/:chemblId" component={DrugPage} />
-              <Route path="/evidence/:ensgId/:efoId" component={EvidencePage} />
-            </React.Fragment>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/target/:ensgId" component={TargetPage} />
+                <Route path="/disease/:efoId" component={DiseasePage} />
+                <Route path="/drug/:chemblId" component={DrugPage} />
+                <Route
+                  path="/evidence/:ensgId/:efoId"
+                  component={EvidencePage}
+                />
+              </Switch>
+            </Suspense>
           </Router>
         </OtUiThemeProvider>
       </ApolloProvider>
