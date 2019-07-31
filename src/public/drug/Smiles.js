9a2275e3738d995c7d709fd46474f0d896432f8c
Alfredo Miranda <alfredo@miranda.io>
2019-07-31 15:16:38 +0100

    Add box around smiles

--- src/public/drug/Smiles.js
+++ src/public/drug/Smiles.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import SmilesDrawer from 'smiles-drawer';
+import Card from '@material-ui/core/Card';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-class SmilesHelper extends Component {
+const styles = theme => ({
+  container: {
+    border: `1px solid ${theme.palette.grey[300]}`,
+  },
+});
+
+let SmilesHelper = class extends Component {
   componentDidMount() {
     const { smiles, chemblId } = this.props;
     const smilesDrawer = new SmilesDrawer.Drawer({
       width: 450,
-      height: 300,
+      height: 240,
+      padding: 5,
     });
     SmilesDrawer.parse(
       smiles,
@@ -20,10 +29,16 @@ class SmilesHelper extends Component {
   }
 
   render() {
-    const { chemblId } = this.props;
-    return <canvas id={chemblId} />;
+    const { chemblId, classes } = this.props;
+    return (
+      <Card className={classes.container} elevation={0}>
+        <canvas id={chemblId} />
+      </Card>
+    );
   }
-}
+};
+
+SmilesHelper = withStyles(styles)(SmilesHelper);
 
 class Smiles extends Component {
   state = {
