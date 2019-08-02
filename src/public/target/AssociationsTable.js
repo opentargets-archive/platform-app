419da4d961092b4d56197bb908fc9286eb955797
peatroot <garethpeat@gmail.com>
2019-08-02 10:55:54 +0100

    Abstract out Histogram component

--- src/public/target/AssociationsTable.js
+++ src/public/target/AssociationsTable.js
@@ -11,53 +11,9 @@ import { Link, significantFigures } from 'ot-ui';
 import BaseAssociationsTable from '../common/AssociationsTable';
 import VerticalSlider from '../common/VerticalSlider';
 import HeatmapCell from '../common/HeatmapCell';
+// import Histogram from '../common/Histogram';
 
 const hideEmptyColumns = true;
-
-class Histogram extends React.Component {
-  update = () => {
-    const { id, data } = this.props;
-    const maxLength = d3.max(data, d => d.length);
-    const barsContainer = d3.select(`#histogram-${id} g`);
-
-    const bars = barsContainer.selectAll('rect').data(data);
-
-    bars
-      .enter()
-      .append('rect')
-      .merge(bars)
-      .attr('x', d => 1 - (maxLength ? d.length / maxLength : 0))
-      .attr('y', d => 1 - d.x1)
-      .attr('width', d => (maxLength ? d.length / maxLength : 0))
-      .attr('height', d => d.x1 - d.x0);
-
-    bars.exit().remove();
-  };
-  componentDidMount() {
-    this.update();
-  }
-  componentDidUpdate() {
-    this.update();
-  }
-  render() {
-    const { id, color } = this.props;
-    return (
-      <svg
-        id={`histogram-${id}`}
-        style={{ position: 'absolute', bottom: 0, left: 0 }}
-        preserveAspectRatio="none"
-        width="100%"
-        height="100%"
-        viewBox="0 0 1 1"
-      >
-        <g fill={color} />
-        <g>
-          <line x1="1" y1="0" x2="1" y2="1" stroke="#ccc" strokeWidth="0.04" />
-        </g>
-      </svg>
-    );
-  }
-}
 
 const columns = (
   dataSources,
