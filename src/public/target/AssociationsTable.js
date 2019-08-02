3fd2402bf10e16628a9fb24c940ac435ce7db061
peatroot <garethpeat@gmail.com>
2019-08-02 10:37:19 +0100

    Abstract out HeatmapCell component

--- src/public/target/AssociationsTable.js
+++ src/public/target/AssociationsTable.js
@@ -10,30 +10,9 @@ import { Link, significantFigures } from 'ot-ui';
 
 import BaseAssociationsTable from '../common/AssociationsTable';
 import VerticalSlider from '../common/VerticalSlider';
+import HeatmapCell from '../common/HeatmapCell';
 
 const hideEmptyColumns = true;
-
-const HeatmapCell = ({ value, colorScale, onClick, selected }) => {
-  const a = 'white';
-  const b = '#E0E0E0';
-  const c = 'red';
-  return (
-    <span
-      onClick={onClick}
-      style={{
-        display: 'inline-block',
-        width: '16px',
-        height: '16px',
-        border: `${selected ? 2 : 1}px solid ${selected ? c : b}`,
-        background:
-          value > 0
-            ? colorScale(value)
-            : `repeating-linear-gradient(45deg,${a},${a} 2px,${b} 2px,${b} 4px)`,
-      }}
-      title={`Score: ${value > 0 ? significantFigures(value) : 'N/A'}`}
-    />
-  );
-};
 
 class Histogram extends React.Component {
   update = () => {
