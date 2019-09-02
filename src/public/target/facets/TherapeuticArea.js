57fa2abdf8a6e634c8f41df857e35605d3473b82
peatroot <garethpeat@gmail.com>
2019-09-02 10:37:33 +0100

    Simplify code

--- src/public/target/facets/TherapeuticArea.js
+++ src/public/target/facets/TherapeuticArea.js
@@ -25,40 +25,37 @@ export const stateDefault = [];
 export const stateToInput = state =>
   state.length > 0 ? { efoIds: state } : null;
 
-export const FacetComponent = ({ state, data, onFacetChange }) => {
-  return (
-    <div>
-      <FormControl component="fieldset">
-        <FormLabel component="legend">{name}</FormLabel>
+export const FacetComponent = ({ state, data, onFacetChange }) => (
+  <div>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">{name}</FormLabel>
+      <FormGroup>
+        {data.items.map(item => (
+          <FormControlLabel
+            key={item.id}
+            control={
+              <Checkbox
+                checked={state.indexOf(item.id) >= 0}
+                onChange={() => {
+                  let newState;
+                  if (state.indexOf(item.id) >= 0) {
+                    // switch off
+                    newState = state.filter(d => d !== item.id);
+                  } else {
+                    // switch on
+                    newState = [item.id, ...state];
+                  }
 
-        <FormGroup>
-          {data.items.map(item => (
-            <FormControlLabel
-              key={item.id}
-              control={
-                <Checkbox
-                  checked={state.indexOf(item.id) >= 0}
-                  onChange={() => {
-                    let newState;
-                    if (state.indexOf(item.id) >= 0) {
-                      // switch off
-                      newState = state.filter(d => d !== item.id);
-                    } else {
-                      // switch on
-                      newState = [item.id, ...state];
-                    }
-
-                    // update
-                    onFacetChange(newState);
-                  }}
-                  value={item.id}
-                />
-              }
-              label={`${item.name} (${item.count})`}
-            />
-          ))}
-        </FormGroup>
-      </FormControl>
-    </div>
-  );
-};
+                  // update
+                  onFacetChange(newState);
+                }}
+                value={item.id}
+              />
+            }
+            label={`${item.name} (${item.count})`}
+          />
+        ))}
+      </FormGroup>
+    </FormControl>
+  </div>
+);
