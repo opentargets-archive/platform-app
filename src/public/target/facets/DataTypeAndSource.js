f70b527f7462ae5e157a00e90dd8e3c0dc49f9ec
peatroot <garethpeat@gmail.com>
2019-09-02 12:00:52 +0100

    Add datatypes facet without datasources

--- src/public/target/facets/DataTypeAndSource.js
+++ src/public/target/facets/DataTypeAndSource.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import gql from 'graphql-tag';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+
+import FacetCheckbox from '../../common/FacetCheckbox';
 
 export const id = 'dataTypeAndSource';
 export const name = 'Data Type and Source';
@@ -21,4 +26,52 @@ export const facetQuery = gql`
   }
 `;
 
-export const FacetComponent = () => <div>data type and source</div>;
+export const stateDefault = {
+  dataTypeIds: [],
+  dataSourceIds: [],
+};
+export const stateToInput = state => {
+  const input = {};
+  if (state.dataTypeIds.length > 0) {
+    input.dataTypeIds = state.dataTypeIds;
+  }
+  if (state.dataSourceIds.length > 0) {
+    input.dataSourceIds = state.dataSourceIds;
+  }
+  return input;
+};
+
+export const FacetComponent = ({ state, data, onFacetChange }) => (
+  <div>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">{name}</FormLabel>
+      <FormGroup>
+        {data.items.map(item => (
+          <FacetCheckbox
+            key={item.id}
+            checked={state.dataTypeIds.indexOf(item.id) >= 0}
+            onChange={() => {
+              let newDataTypeIds;
+              if (state.dataTypeIds.indexOf(item.id) >= 0) {
+                // switch off
+                newDataTypeIds = state.dataTypeIds.filter(d => d !== item.id);
+              } else {
+                // switch on
+                newDataTypeIds = [item.id, ...state.dataTypeIds];
+              }
+              const newState = {
+                ...state,
+                dataTypeIds: newDataTypeIds,
+              };
+
+              // update
+              onFacetChange(newState);
+            }}
+            value={item.id}
+            label={`${item.name} (${item.count})`}
+          />
+        ))}
+      </FormGroup>
+    </FormControl>
+  </div>
+);
