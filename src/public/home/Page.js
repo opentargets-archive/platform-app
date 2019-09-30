b85fdf795e4e43a0cec805dd6e3b22114f6f89ab
Alfredo Miranda <alfredo@miranda.io>
2019-09-30 11:27:40 +0100

    Use particles.js lib

--- src/public/home/Page.js
+++ src/public/home/Page.js
@@ -1,25 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import 'particles.js';
 
 import { Link } from 'ot-ui';
 
-const HomePage = () => {
-  return (
-    <Fragment>
-      <div>Home</div>
-      <div>
-        <Link to="/target/ENSG00000091831">Target page</Link>
-      </div>
-      <div>
-        <Link to="/disease/EFO_0000384">Disease page</Link>
-      </div>
-      <div>
-        <Link to="/drug/CHEMBL2111100">Drug page</Link>
-      </div>
-      <div>
-        <Link to="/evidence/ENSG00000091831/EFO_0000305">Evidence page</Link>
-      </div>
-    </Fragment>
-  );
-};
+import { particlesConfig } from '../../constants';
 
-export default HomePage;
+const styles = theme => ({
+  splash: {
+    backgroundColor: theme.palette.primary.main,
+  },
+});
+
+class HomePage extends Component {
+  componentDidMount() {
+    window.particlesJS('splash', particlesConfig, function() {
+      console.log('particles.js config loaded');
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Fragment>
+        <div id="splash" className={classes.splash}></div>
+        <div>Home</div>
+        <div>
+          <Link to="/target/ENSG00000091831">Target page</Link>
+        </div>
+        <div>
+          <Link to="/disease/EFO_0000384">Disease page</Link>
+        </div>
+        <div>
+          <Link to="/drug/CHEMBL2111100">Drug page</Link>
+        </div>
+        <div>
+          <Link to="/evidence/ENSG00000091831/EFO_0000305">Evidence page</Link>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(HomePage);
