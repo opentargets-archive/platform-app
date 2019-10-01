240b71fe57366243fc1f5904f4707722aa488d41
Gareth Peat <peatroot@users.noreply.github.com>
2019-10-01 12:24:51 +0100

    Handle GXA widget errors (#105)
    * Bump expression atlas widget version
    
    * Add handler for expression atlas to fail gracefully

--- src/public/target/sections/Expression/custom/AtlasTab.js
+++ src/public/target/sections/Expression/custom/AtlasTab.js
@@ -6,7 +6,40 @@ const ExpressionAtlasHeatmap = lazy(() =>
   import('expression-atlas-heatmap-highcharts')
 );
 
-const AtlasTab = ({ ensgId }) => {
+class AtlasHandler extends React.Component {
+  state = {
+    hasError: false,
+  };
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    if (error) {
+      return { hasError: true };
+    } else {
+      return null;
+    }
+  }
+  render() {
+    const { ensgId, symbol } = this.props;
+    const { hasError } = this.state;
+    return hasError ? (
+      <Typography color="error">
+        There was an error loading the Expression Atlas plugin for {symbol}.
+      </Typography>
+    ) : (
+      <Suspense fallback={<div>Loading...</div>}>
+        <ExpressionAtlasHeatmap
+          query={{
+            species: 'homo sapiens',
+            gene: ensgId,
+            target: 'heatmapContainer',
+          }}
+        />
+      </Suspense>
+    );
+  }
+}
+
+const AtlasTab = ({ ensgId, symbol }) => {
   return (
     <Fragment>
       <Typography variant="caption">
@@ -17,12 +50,8 @@ const AtlasTab = ({ ensgId }) => {
         >
           Expression Atlas
         </Link>
+        <AtlasHandler {...{ ensgId, symbol }} />
       </Typography>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ExpressionAtlasHeatmap
-          query={{ species: 'homo sapiens', gene: ensgId }}
-        />
-      </Suspense>
     </Fragment>
   );
 };
