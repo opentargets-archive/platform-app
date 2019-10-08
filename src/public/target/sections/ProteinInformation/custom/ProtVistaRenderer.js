e708db85db56c5d992399988b5e5005afce807b1
Alfredo Miranda <alfredo@miranda.io>
2019-10-08 15:05:09 +0100

    Add litemol styles

--- src/public/target/sections/ProteinInformation/custom/ProtVistaRenderer.js
+++ src/public/target/sections/ProteinInformation/custom/ProtVistaRenderer.js
@@ -1,35 +1,17 @@
 import React from 'react';
-import * as d3 from 'd3';
+import ProtvistaUniprot from 'protvista-uniprot';
+import 'litemol/dist/css/LiteMol-plugin.css';
 
-import 'ProtVista/build/css/main.css';
-
-class ProtVistaRenderer extends React.Component {
-  state = {};
-  componentDidMount() {
-    import('ProtVista').then(({ default: ProtVista }) => {
-      this.setState({ ProtVista });
-    });
-  }
-  componentDidUpdate() {
-    const { ProtVista } = this.state;
-    if (ProtVista) {
-      // remove any previous content
-      d3.select('#protvista')
-        .selectAll('*')
-        .remove();
-
-      // restart the plugin
-      const { uniprotId } = this.props;
-      this.protVista = new ProtVista({
-        el: '#protvista',
-        uniprotacc: uniprotId,
-        exclusions: ['seqInfo'],
-      });
-    }
-  }
-  render() {
-    return <div id="protvista" />;
+function loadWebComponent(name, wc) {
+  if (window.customElements && !window.customElements.get(name)) {
+    window.customElements.define(name, wc);
   }
 }
+
+loadWebComponent('protvista-uniprot', ProtvistaUniprot);
+
+const ProtVistaRenderer = ({ uniprotId }) => {
+  return <protvista-uniprot accession={uniprotId} />;
+};
 
 export default ProtVistaRenderer;
