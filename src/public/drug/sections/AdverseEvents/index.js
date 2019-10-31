b97986bf09da3ae5e3e32491b2610d20000f9035
Luca Fumis <lucaf@ebi.ac.uk>
2019-10-31 11:23:25 +0000

    Adverse events: implement hasSummaryData function; issue opentargets/platform#670

--- src/public/drug/sections/AdverseEvents/index.js
+++ src/public/drug/sections/AdverseEvents/index.js
@@ -3,7 +3,7 @@ import { loader } from 'graphql.macro';
 export const id = 'adverseEvents';
 export const name = 'Adverse Events';
 
-export const hasSummaryData = () => true; // TODO: override this
+export const hasSummaryData = ({ eventsCount }) => eventsCount > 0;
 
 export const summaryQuery = loader('./summaryQuery.gql');
 export const sectionQuery = loader('./sectionQuery.gql');
