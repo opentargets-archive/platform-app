069315c027a1b9da8ef78c7811fa8e9c4239817b
Luca Fumis <lucaf@ebi.ac.uk>
2019-11-05 16:06:30 +0000

    Adverse events: add cell render and table download; issue opentargets/platform#670

--- src/public/drug/sections/AdverseEvents/Section.js
+++ src/public/drug/sections/AdverseEvents/Section.js
@@ -1,37 +1,81 @@
 import React from 'react';
+import _ from 'lodash';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-import { Link, OtTableRF } from 'ot-ui';
+import { Link, OtTableRF, DataDownloader } from 'ot-ui';
+import LevelBar from '../../../common/LevelBar';
 
-const columns = [
-  {
-    id: 'event',
-    label: 'Adverse event',
+const styles = theme => ({
+  levelBarContainer: {
+    display: 'flex',
+    alignItems: 'center',
   },
-  {
-    id: 'count',
-    label: 'Number of reported events',
+  levelBar: {
+    backgroundColor: theme.palette.primary.main,
+    borderRight: `1px solid ${theme.palette.primary.main}`,
+    height: '10px',
+    marginRight: '5px',
   },
-  {
-    id: 'llr',
-    label: 'Log likelihood ratio',
-  },
-];
+});
 
-const Section = ({ data }) => (
-  <React.Fragment>
-    A list of post-marketing adverse events submitted by healthcare
-    professionals to the FDA Adverse Event Reporting System (FAERS). The list
-    only contains adverse events with a log likelihood ratio above a critical
-    value (CV) threshold - see our{' '}
-    <Link to="#">adverse event report documentation page</Link> for more
-    information.
-    <OtTableRF
-      loading={false}
-      error={false}
-      columns={columns}
-      data={data.rows}
-    />
-  </React.Fragment>
-);
+const Section = ({ classes, data, name }) => {
+  const maxLlr = data.rows[0].llr;
+  const columns = [
+    {
+      id: 'event',
+      label: 'Adverse event',
+      renderCell: d => _.upperFirst(d.event),
+      width: '35%',
+    },
+    {
+      id: 'count',
+      label: 'Number of reported events',
+      width: '15%',
+    },
+    {
+      id: 'llr',
+      label: `Log likelihood ratio (CV = ${data.critval})`,
+      renderCell: d => {
+        const w = ((d.llr / maxLlr) * 85).toFixed(2); // scale to max 85% of the width to allows space for label
+        return (
+          <div className={classes.levelBarContainer}>
+            <div
+              className={classes.levelBar}
+              style={{
+                width: `${w}%`,
+              }}
+            ></div>
+            <div>{d.llr.toFixed(2)}</div>
+          </div>
+        );
+      },
+      width: '50%',
+    },
+  ];
 
-export default Section;
+  return (
+    <React.Fragment>
+      A list of post-marketing adverse events submitted by healthcare
+      professionals to the FDA Adverse Event Reporting System (FAERS). The list
+      only contains adverse events with a log likelihood ratio above a critical
+      value (CV) threshold - see our{' '}
+      <Link to="#">adverse event report documentation page</Link> for more
+      information.
+      <DataDownloader
+        tableHeaders={columns}
+        rows={data.rows}
+        fileStem={`${name}-pharmacovigilance`}
+      />
+      <OtTableRF
+        loading={false}
+        error={false}
+        columns={columns}
+        data={data.rows}
+        pageSize={data.rows.length || 25}
+      />
+    </React.Fragment>
+  );
+};
+
+// export default Section;
+export default withStyles(styles)(Section);
