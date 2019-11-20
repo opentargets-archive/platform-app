69309ddd6229bb032118d1a4a91f37af034e4207
Luca Fumis <lucaf@ebi.ac.uk>
2019-11-20 10:46:00 +0000

    Update hallmark description (rewrite); issue opentargets/platform#780

--- src/public/target/sections/CancerHallmarks/Description.js
+++ src/public/target/sections/CancerHallmarks/Description.js
@@ -5,12 +5,11 @@ import { Link } from 'ot-ui';
 const Description = () => (
   <React.Fragment>
     Essential alterations in cell physiology that dictate malignant growth.
-    Cancer hallmarks were originally described by{' '}
+    Cancer hallmarks, originally described by{' '}
     <Link external to="https://www.cell.com/abstract/S0092-8674(00)81683-9">
       Hanahan and Weinberg (2000)
     </Link>{' '}
-    and are manually curated by COSMIC and integrated into the Cancer Gene
-    Census.
+    are manually curated by COSMIC and integrated in the Cancer Gene Census.
   </React.Fragment>
 );
 
