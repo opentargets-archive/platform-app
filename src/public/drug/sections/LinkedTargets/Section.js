fcebd133f87c387f984d55ae9724e22c67cef620
Luca Fumis <lucaf@ebi.ac.uk>
2019-12-11 09:47:35 +0000

    Lf issue772 (#116)
    * Add name to drug target table; issue opentargets/platform#772
    
    * Add download option for drug targers; issue opentargets/platform#772

--- src/public/drug/sections/LinkedTargets/Section.js
+++ src/public/drug/sections/LinkedTargets/Section.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-import { Link, OtTableRF } from 'ot-ui';
+import { Link, OtTableRF, DataDownloader } from 'ot-ui';
 
 const columns = [
   {
     id: 'symbol',
     label: 'Symbol',
     renderCell: d => <Link to={`/target/${d.id}`}>{d.symbol}</Link>,
+    width: '20%',
+  },
+  {
+    id: 'name',
+    label: 'Name',
   },
 ];
 
-const Section = ({ data }) => (
-  <OtTableRF loading={false} error={false} columns={columns} data={data.rows} />
+const Section = ({ data, name }) => (
+  <Fragment>
+    <DataDownloader
+      tableHeaders={columns}
+      rows={data.rows}
+      fileStem={`${name}-targets`}
+    />
+    <OtTableRF
+      loading={false}
+      error={false}
+      columns={columns}
+      data={data.rows}
+    />
+  </Fragment>
 );
 
 export default Section;
