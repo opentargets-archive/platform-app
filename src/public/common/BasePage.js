404c4fd7bcab06819f16499289ab2e665208393e
Luca Fumis <lucaf@ebi.ac.uk>
2020-01-17 17:45:13 +0000

    Add menu to pages; issue opentargets/platform#239

--- src/public/common/BasePage.js
+++ src/public/common/BasePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Page, NavBar, Footer } from 'ot-ui';
 
-import { externalLinks } from '../../constants';
+import { externalLinks, mainMenuItems } from '../../constants';
 import Search from './search/Search';
 
 const BasePage = ({ children }) => (
   <Page
-    header={<NavBar name="Platform" search={<Search />} />}
+    header={
+      <NavBar name="Platform" search={<Search />} items={mainMenuItems} />
+    }
     footer={<Footer externalLinks={externalLinks} />}
   >
     <Helmet
