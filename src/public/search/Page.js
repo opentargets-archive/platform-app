eae95a84b11c1b4d46d0a6b0d5bcb24ec7d9a463
Alfredo Miranda <alfredo@miranda.io>
2020-02-03 14:39:25 +0000

    Add drug details fields

--- src/public/search/Page.js
+++ src/public/search/Page.js
@@ -120,7 +120,39 @@ const DiseaseDetail = ({ data }) => {
 };
 
 const DrugDetail = ({ data }) => {
-  return 'Drug detail';
+  return (
+    <>
+      <CardHeader title={data.name} />
+      <CardContent>
+        <Typography variant="h6">Drug Type</Typography>
+        <Typography>{data.drugType}</Typography>
+        <Typography variant="h6">Maximum Clinical Trial Phase</Typography>
+        <Typography>{data.maximumClinicalTrialPhase}</Typography>
+        {data.hasBeenWithdrawn && (
+          <>
+            <Typography variant="h6">Withdrawn message</Typography>
+            <Typography>{data.withdrawnNotice.year}</Typography>
+          </>
+        )}
+        <Typography variant="h6">Indications</Typography>
+        {data.linkedDiseases.rows.map(disease => (
+          <span key={disease}>{disease}</span>
+        ))}
+        <Typography variant="h6">Drug targets</Typography>
+        {data.linkedTargets.rows.map(target => (
+          <span key={target.id}>{target.approvedSymbol}</span>
+        ))}
+        <Typography variant="h6">Synonyms</Typography>
+        {data.synonyms.map(synonym => (
+          <span key={synonym}>{synonym}</span>
+        ))}
+        <Typography variant="h6">Trade names</Typography>
+        {data.tradeNames.map(tradeName => (
+          <span key={tradeName}>{tradeName}</span>
+        ))}
+      </CardContent>
+    </>
+  );
 };
 
 const TopHitDetails = ({ data }) => {
