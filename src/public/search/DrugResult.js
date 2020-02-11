a0e6b4dfd01bee2ee4c641d6bed20fc6794c91fd
Alfredo Miranda <alfredo@miranda.io>
2020-02-11 16:16:37 +0000

    Style result components

--- src/public/search/DrugResult.js
+++ src/public/search/DrugResult.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
 import { Link } from 'ot-ui';
+import DrugIcon from '../../icons/DrugIcon';
 
-const DrugResult = ({ data }) => {
+const styles = theme => ({
+  container: {
+    marginBottom: '20px',
+  },
+  icon: {
+    color: theme.palette.primary.main,
+    verticalAlign: 'bottom',
+  },
+});
+
+const DrugResult = ({ classes, data }) => {
   return (
-    <div>
+    <div className={classes.container}>
       <Link to={`drug/${data.id}`}>{data.name}</Link>
+      <Typography>
+        <DrugIcon className={classes.icon} />
+        Drug
+      </Typography>
     </div>
   );
 };
 
-export default DrugResult;
+export default withStyles(styles)(DrugResult);
--- src/public/search/TargetResult.js
+++ src/public/search/TargetResult.js
@@ -2,17 +2,32 @@ import React from 'react';
 import Clampy from '@clampy-js/react-clampy';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'ot-ui';
+import TargetIcon from '../../icons/TargetIcon';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-const TargetResult = ({ data }) => {
+const styles = theme => ({
+  container: {
+    marginBottom: '20px',
+  },
+  icon: {
+    color: theme.palette.primary.main,
+    verticalAlign: 'bottom',
+  },
+});
+
+const TargetResult = ({ classes, data }) => {
   return (
-    <>
+    <div className={classes.container}>
       <Link to={`/target/${data.id}`}>{data.approvedSymbol}</Link>
       <Typography component="div">
         <Clampy clampSize="4">{data.proteinAnnotations.functions[0]}</Clampy>
       </Typography>
-      <Typography>Target</Typography>
-    </>
+      <Typography>
+        <TargetIcon className={classes.icon} />
+        Target
+      </Typography>
+    </div>
   );
 };
 
-export default TargetResult;
+export default withStyles(styles)(TargetResult);
