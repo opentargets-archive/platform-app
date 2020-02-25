4bcf9fc1715a2e8156219d69382b35758b5f9a14
Alfredo Miranda <alfredo@miranda.io>
2020-02-25 12:07:23 +0000

    Refactor ClassicAssociations component

--- src/public/target/ClassicAssociations.js
+++ src/public/target/ClassicAssociations.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import _ from 'lodash';
 import gql from 'graphql-tag';
 import { print } from 'graphql/language/printer';
-import { Query } from '@apollo/react-components';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -83,232 +83,209 @@ const facetsStateDefault = facets.reduce((acc, f) => {
   return acc;
 }, {});
 
-class ClassicAssociations extends React.Component {
-  state = {
-    first: 50,
-    page: 0,
-    pageCursors: {},
-    facets: facetsStateDefault,
-    search: '',
-    searchDebouced: '',
-    sortBy: { field: 'SCORE_OVERALL', ascending: false },
-    tab: 'table',
-  };
-  handlePaginationChange = (forward, nextPageCursor) => {
-    const { page, pageCursors } = this.state;
+const ClassicAssociations = ({ ensgId, symbol }) => {
+  const [first, setFirst] = useState(50);
+  const [page, setPage] = useState(0);
+  const [pageCursors, setPageCursors] = useState({});
+  const [facetsState, setFacetsState] = useState(facetsStateDefault);
+  const [search, setSearch] = useState('');
+  const [searchDebouced, setSearchDebouced] = useState('');
+  const [sortBy, setSortBy] = useState({
+    field: 'SCORE_OVERALL',
+    ascending: false,
+  });
+  const [tab, setTab] = useState('table');
+
+  const handlePaginationChange = (forward, nextPageCursor) => {
     if (forward) {
       // go to next page
-      this.setState({
-        page: page + 1,
-        pageCursors: { ...pageCursors, [page + 1]: nextPageCursor },
-      });
+      setPage(page + 1);
+      setPageCursors({ ...pageCursors, [page + 1]: nextPageCursor });
     } else {
       // go to previous page
-      this.setState({ page: page - 1 });
+      setPage(page - 1);
     }
   };
-  handleSortByChange = sortBy => {
-    this.setState({ sortBy, page: 0 });
+  const handleSortByChange = sortBy => {
+    setSortBy(sortBy);
+    setPage(0);
   };
-  handleSearchChange = search => {
-    this.setState({ search });
-    this.handleSearchDeboucedChange(search);
+  const handleSearchChange = search => {
+    setSearch(search);
+    handleSearchDeboucedChange(search);
   };
-  handleSearchDeboucedChange = _.debounce(searchDebouced => {
-    this.setState({ searchDebouced, page: 0 });
+  const handleSearchDeboucedChange = _.debounce(searchDebouced => {
+    setSearchDebouced(searchDebouced);
+    setPage(0);
   }, 500);
-  handleFacetChange = facetId => state => {
-    const { facets: facetsState } = this.state;
-    this.setState({
-      facets: { ...facetsState, [facetId]: state },
-      page: 0,
-    });
+  const handleFacetChange = facetId => state => {
+    setFacetsState({ ...facetsState, [facetId]: state });
+    setPage(0);
   };
-  handleTabChange = (event, tab) => {
+  const handleTabChange = (event, tab) => {
     if (tab === 'table') {
       // Heatmap table renders 50 rows.
-      this.setState({ tab, first: 50, page: 0 });
+      setTab(tab);
+      setFirst(50);
+      setPage(0);
     } else {
       // Bubbles and dag render top 10000 by association score descending
       // as there's no way to sort by columns. Facet should still work.
-      this.setState({
-        tab,
-        first: 10000,
-        page: 0,
-        sortBy: { field: 'SCORE_OVERALL', ascending: false },
-      });
+      setTab(tab);
+      setFirst(10000);
+      setPage(0);
+      setSortBy({ field: 'SCORE_OVERALL', ascending: false });
     }
   };
-  render() {
-    const { ensgId, symbol } = this.props;
-    const {
-      search,
-      searchDebouced,
+
+  const facetsInput = facets
+    .map(f => ({ ...f, input: f.stateToInput(facetsState[f.id]) }))
+    .filter(f => f.input)
+    .reduce((acc, f) => {
+      acc[f.id] = f.input;
+      return acc;
+    }, {});
+  const after = page > 0 ? pageCursors[page] : null;
+
+  const { loading, error, data } = useQuery(associationsQuery, {
+    variables: {
+      ensgId,
       sortBy,
-      facets: facetsState,
-      page,
-      pageCursors,
-      first: rowsPerPage,
-      tab,
-    } = this.state;
-    const facetsInput = facets
-      .map(f => ({ ...f, input: f.stateToInput(facetsState[f.id]) }))
-      .filter(f => f.input)
-      .reduce((acc, f) => {
-        acc[f.id] = f.input;
-        return acc;
-      }, {});
-    const after = page > 0 ? pageCursors[page] : null;
-    return (
-      <Query
-        query={associationsQuery}
-        variables={{
-          ensgId,
-          sortBy,
-          search: searchDebouced ? searchDebouced : null,
-          facets: facetsInput,
-          first: rowsPerPage,
-          after,
-        }}
-      >
-        {({ loading, error, data }) => {
-          if (error) {
-            return null;
-          }
+      search: searchDebouced ? searchDebouced : null,
+      facets: facetsInput,
+      first,
+      after,
+    },
+  });
 
-          let edges;
-          let totalCount;
-          let facetsData;
-          let pageInfo;
-          let efo;
-          if (
-            loading &&
-            !(data && data.efo && data.target && data.target.diseasesConnection)
-          ) {
-            edges = [];
-            efo = { nodes: [], therapeuticAreas: [] };
-          } else {
-            edges = data.target.diseasesConnection.edges;
-            totalCount = data.target.diseasesConnection.totalCount;
-            facetsData = data.target.diseasesConnection.facets;
-            pageInfo = data.target.diseasesConnection.pageInfo;
-            efo = data.efo;
-          }
+  if (error) return null;
 
-          const rows = edges.map(({ node, ...rest }) => ({
-            disease: node,
-            data: {
-              name: node.name,
-              id: node.id,
-              target: { ensgId },
-              score: rest.score,
-            }, // for tooltip
-            ...rest,
-          }));
-          const dataTypes =
-            rows.length > 0
-              ? rows[0].scoresByDataType.map(d => d.dataTypeId)
-              : [];
-          return (
-            <Grid style={{ marginTop: '8px' }} container spacing={16}>
-              <Grid item xs={12}>
-                <Typography variant="h6">
-                  <strong>{commaSeparate(totalCount)} diseases</strong>{' '}
-                  associated with <strong>{symbol}</strong>
-                </Typography>
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <Card elevation={0}>
-                  <CardContent>
-                    <Typography variant="h6">Filter by</Typography>
-                    <div style={{ paddingTop: '8px', paddingBottom: '4px' }}>
-                      <TextField
-                        id="associations-search"
-                        label="Disease Name"
-                        value={search}
-                        onChange={event =>
-                          this.handleSearchChange(event.target.value)
-                        }
-                        fullWidth
-                        variant="outlined"
-                      />
-                    </div>
-                    {facetsData
-                      ? facets.map(f => (
-                          <FacetContainer key={f.id} name={f.name}>
-                            <f.FacetComponent
-                              state={facetsState[f.id]}
-                              data={facetsData[f.id]}
-                              onFacetChange={this.handleFacetChange(f.id)}
-                            />
-                          </FacetContainer>
-                        ))
-                      : null}
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} md={9}>
-                <Tabs
-                  value={tab}
-                  onChange={this.handleTabChange}
-                  variant="scrollable"
-                  scrollButtons="auto"
-                >
-                  <Tab value="table" label="Table" />
-                  <Tab value="bubbles" label="Bubbles" />
-                  <Tab value="dag" label="Graph" />
-                </Tabs>
-                <Card elevation={0}>
-                  <CardContent>
-                    {/* table view */}
-                    {tab === 'table' && (
-                      <ClassicAssociationsTable
-                        ensgId={ensgId}
-                        symbol={symbol}
-                        rows={rows}
-                        dataTypes={dataTypes}
-                        search={search}
-                        facets={facetsInput}
-                        sortBy={sortBy}
-                        onSortByChange={this.handleSortByChange}
-                        page={page}
-                        rowsPerPage={rowsPerPage}
-                        totalCount={totalCount}
-                        pageInfo={pageInfo}
-                        onPaginationChange={this.handlePaginationChange}
-                      />
-                    )}
-
-                    {/* bubbles view */}
-                    {tab === 'bubbles' && (
-                      <ClassicAssociationsBubbles
-                        ensgId={ensgId}
-                        symbol={symbol}
-                        data={rows}
-                        efo={efo}
-                        selectedTherapeuticAreas={facetsState.therapeuticArea} // hack!
-                      />
-                    )}
-
-                    {/* dag view */}
-                    {tab === 'dag' && (
-                      <ClassicAssociationsDAG
-                        ensgId={ensgId}
-                        symbol={symbol}
-                        data={rows}
-                        efo={efo}
-                        selectedTherapeuticAreas={facetsState.therapeuticArea} // hack!
-                      />
-                    )}
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
-          );
-        }}
-      </Query>
-    );
+  let edges;
+  let totalCount;
+  let facetsData;
+  let pageInfo;
+  let efo;
+  if (
+    loading &&
+    !(data && data.efo && data.target && data.target.diseasesConnection)
+  ) {
+    edges = [];
+    efo = { nodes: [], therapeuticAreas: [] };
+  } else {
+    edges = data.target.diseasesConnection.edges;
+    totalCount = data.target.diseasesConnection.totalCount;
+    facetsData = data.target.diseasesConnection.facets;
+    pageInfo = data.target.diseasesConnection.pageInfo;
+    efo = data.efo;
   }
-}
+
+  const rows = edges.map(({ node, ...rest }) => ({
+    disease: node,
+    data: {
+      name: node.name,
+      id: node.id,
+      target: { ensgId },
+      score: rest.score,
+    }, // for tooltip
+    ...rest,
+  }));
+  const dataTypes =
+    rows.length > 0 ? rows[0].scoresByDataType.map(d => d.dataTypeId) : [];
+
+  return (
+    <Grid style={{ marginTop: '8px' }} container spacing={16}>
+      <Grid item xs={12}>
+        <Typography variant="h6">
+          <strong>{commaSeparate(totalCount)} diseases</strong> associated with{' '}
+          <strong>{symbol}</strong>
+        </Typography>
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card elevation={0}>
+          <CardContent>
+            <Typography variant="h6">Filter by</Typography>
+            <div style={{ paddingTop: '8px', paddingBottom: '4px' }}>
+              <TextField
+                id="associations-search"
+                label="Disease Name"
+                value={search}
+                onChange={event => handleSearchChange(event.target.value)}
+                fullWidth
+                variant="outlined"
+              />
+            </div>
+            {facetsData
+              ? facets.map(f => (
+                  <FacetContainer key={f.id} name={f.name}>
+                    <f.FacetComponent
+                      state={facetsState[f.id]}
+                      data={facetsData[f.id]}
+                      onFacetChange={handleFacetChange(f.id)}
+                    />
+                  </FacetContainer>
+                ))
+              : null}
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={9}>
+        <Tabs
+          value={tab}
+          onChange={handleTabChange}
+          variant="scrollable"
+          scrollButtons="auto"
+        >
+          <Tab value="table" label="Table" />
+          <Tab value="bubbles" label="Bubbles" />
+          <Tab value="dag" label="Graph" />
+        </Tabs>
+        <Card elevation={0}>
+          <CardContent>
+            {/* table view */}
+            {tab === 'table' && (
+              <ClassicAssociationsTable
+                ensgId={ensgId}
+                symbol={symbol}
+                rows={rows}
+                dataTypes={dataTypes}
+                search={search}
+                facets={facetsInput}
+                sortBy={sortBy}
+                onSortByChange={handleSortByChange}
+                page={page}
+                rowsPerPage={first}
+                totalCount={totalCount}
+                pageInfo={pageInfo}
+                onPaginationChange={handlePaginationChange}
+              />
+            )}
+
+            {/* bubbles view */}
+            {tab === 'bubbles' && (
+              <ClassicAssociationsBubbles
+                ensgId={ensgId}
+                symbol={symbol}
+                data={rows}
+                efo={efo}
+                selectedTherapeuticAreas={facetsState.therapeuticArea} // hack!
+              />
+            )}
+
+            {/* dag view */}
+            {tab === 'dag' && (
+              <ClassicAssociationsDAG
+                ensgId={ensgId}
+                symbol={symbol}
+                data={rows}
+                efo={efo}
+                selectedTherapeuticAreas={facetsState.therapeuticArea} // hack!
+              />
+            )}
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default ClassicAssociations;
