3c279c8b4de1f903093c725c5b415ce86eec1992
Luca Fumis <lucaf@ebi.ac.uk>
2020-03-24 18:57:38 +0000

    Update IntOGen section description; issue opentargets/platform#863

--- src/public/evidenceByDatatype/sections/Somatic/Section.js
+++ src/public/evidenceByDatatype/sections/Somatic/Section.js
@@ -6,6 +6,8 @@ import EVASomaticSection from '../../../evidence/sections/EVASomatic/Section';
 import CancerGeneCensusSection from '../../../evidence/sections/CancerGeneCensus/Section';
 import IntOGenSection from '../../../evidence/sections/IntOGen/Section';
 
+import { Link } from 'ot-ui';
+
 const Section = ({ ensgId, efoId, data }) => (
   <React.Fragment>
     {data.uniProtSomatic && data.uniProtSomatic.rows.length > 0 ? (
@@ -42,7 +44,13 @@ const Section = ({ ensgId, efoId, data }) => (
     {data.intogen && data.intogen.rows.length > 0 ? (
       <React.Fragment>
         <Typography>
-          Evidence from <strong>IntOGen</strong>.
+          Evidence from <strong>IntOGen</strong>, a database that collects and
+          analyses somatic mutations in thousands of tumor genomes to identify
+          cancer driver genes. For more information, see{' '}
+          <Link to="http://europepmc.org/article/MED/25759023">
+            Gonzalez-Perez A et al., 2013
+          </Link>
+          .
         </Typography>
         <IntOGenSection {...{ ensgId, efoId, data: data.intogen }} />
       </React.Fragment>
