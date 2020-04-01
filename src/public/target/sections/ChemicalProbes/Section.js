bddab4628caadb29361a84a816e1bff86b0d304a
Luca Fumis <lucaf@ebi.ac.uk>
2020-04-01 13:07:49 +0100

    Finalize chemical probes table; opentargets/platform#898

--- src/public/target/sections/ChemicalProbes/Section.js
+++ src/public/target/sections/ChemicalProbes/Section.js
@@ -3,64 +3,57 @@ import Typography from '@material-ui/core/Typography';
 
 import { Link, OtTableRF, DataDownloader } from 'ot-ui';
 
-// const columns = [
-//   {
-//     id: 'chemicalProbe',
-//     label: 'Probe',
-//   },
-//   {
-//     id: 'sources',
-//     label: 'Sources',
-//     renderCell: rowData => (
-//       <React.Fragment>
-//         {rowData.sources.map((d, i, a) => (
-//           <React.Fragment key={i}>
-//             <Link external to={d.url}>
-//               {d.name}
-//             </Link>
-//             {i < a.length - 1 ? ' / ' : ''}
-//           </React.Fragment>
-//         ))}
-//       </React.Fragment>
-//     ),
-//     export: rowData => rowData.sources.map(d => d.url).join(', '),
-//   },
-//   {
-//     id: 'note',
-//     label: 'Notes',
-//   },
-// ];
-
-// const Section = ({ ensgId, symbol, data }) => (
-//   <React.Fragment>
-//     {data.rows.length > 0 ? (
-//       <React.Fragment>
-//         <DataDownloader
-//           tableHeaders={columns}
-//           rows={data.rows}
-//           fileStem={`${symbol}-chemical-probes`}
-//         />
-//         <OtTableRF columns={columns} data={data.rows} />
-//       </React.Fragment>
-//     ) : null}
-//     {data.probeMinerUrl ? (
-//       <React.Fragment>
-//         <Typography>
-//           Potential chemical probes can be explored with{' '}
-//           <Link external to={data.probeMinerUrl}>
-//             ProbeMiner
-//           </Link>
-//           .
-//         </Typography>
-//       </React.Fragment>
-//     ) : null}
-//   </React.Fragment>
-// );
+const columns = [
+  {
+    id: 'chemicalprobe',
+    label: 'Probe',
+  },
+  {
+    id: 'sources',
+    label: 'Sources',
+    renderCell: rowData => (
+      <React.Fragment>
+        {rowData.sourcelinks.map((d, i, a) => (
+          <React.Fragment key={i}>
+            <Link external to={d.link}>
+              {d.source}
+            </Link>
+            {i < a.length - 1 ? ' / ' : ''}
+          </React.Fragment>
+        ))}
+      </React.Fragment>
+    ),
+    export: rowData => rowData.sourcelinks.map(d => d.link).join(', '),
+  },
+  {
+    id: 'note',
+    label: 'Notes',
+  },
+];
 
 const Section = ({ ensgId, symbol, data }) => (
   <React.Fragment>
-    chemical probes
-    {data.rows.length}
+    {data.rows.length > 0 ? (
+      <React.Fragment>
+        <DataDownloader
+          tableHeaders={columns}
+          rows={data.rows}
+          fileStem={`${symbol}-chemical-probes`}
+        />
+        <OtTableRF columns={columns} data={data.rows} />
+      </React.Fragment>
+    ) : null}
+    {data.probeminer ? (
+      <React.Fragment>
+        <Typography>
+          Potential chemical probes can be explored with{' '}
+          <Link external to={data.probeminer}>
+            ProbeMiner
+          </Link>
+          .
+        </Typography>
+      </React.Fragment>
+    ) : null}
   </React.Fragment>
 );
 
