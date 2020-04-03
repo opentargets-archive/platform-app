27ef16c1bfa1bf793a35cc06ee060828fff73153
Luca Fumis <lucaf@ebi.ac.uk>
2020-04-03 12:50:39 +0100

    Update biomarkers summary and section; opentargets/platform#899

--- src/public/target/sections/CancerBiomarkers/Section.js
+++ src/public/target/sections/CancerBiomarkers/Section.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import FilterTable from './custom/FilterTable';
 
-const Section = ({ ensgId, symbol, data }) => (
-  <FilterTable rows={data.rows} symbol={symbol} />
-);
+// const Section = ({ ensgId, symbol, data }) => (
+//   <FilterTable rows={data.rows} symbol={symbol} />
+// );
+
+const Section = ({ ensgId, symbol, data }) => <>Data cound: {data.count} </>;
 
 export default Section;
--- src/public/target/sections/CancerBiomarkers/index.js
+++ src/public/target/sections/CancerBiomarkers/index.js
@@ -3,7 +3,9 @@ import { loader } from 'graphql.macro';
 export const id = 'cancerBiomarkers';
 export const name = 'Cancer Biomarkers';
 
-export const hasSummaryData = ({ hasCancerBiomarkers }) => hasCancerBiomarkers;
+export const hasSummaryData = data => {
+  return data && data.uniqueBiomarkers > 0;
+};
 
 export const summaryQuery = loader('./summaryQuery.gql');
 export const sectionQuery = loader('./sectionQuery.gql');
