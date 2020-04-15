9d990fa13fb1478f630620a52e7ef6002ed71e22
Alfredo Miranda <alfredo@miranda.io>
2020-04-15 13:34:37 -0500

    Add error boundary component

---
+++ src/public/common/ErrorBoundary.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+  };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    return this.state.hasError ? (
+      <h1>Something went wrong</h1>
+    ) : (
+      this.props.children
+    );
+  }
+}
+
+export default ErrorBoundary;
