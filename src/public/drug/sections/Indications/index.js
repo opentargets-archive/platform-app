49cee97eb524880cb486edba4cf522cafc8f5c2f
Alfredo Miranda <alfredo@miranda.io>
2020-05-01 14:37:43 -0500

    Check if the drug has indications data

--- src/public/drug/sections/Indications/index.js
+++ src/public/drug/sections/Indications/index.js
@@ -3,7 +3,9 @@ import { loader } from 'graphql.macro';
 export const id = 'indications';
 export const name = 'Indications';
 
-export const hasSummaryData = ({ count }) => count > 0;
+export const hasSummaryData = data => {
+  return data && data.count > 0;
+};
 
 export const summaryQuery = loader('./summaryQuery.gql');
 export const sectionQuery = loader('./sectionQuery.gql');
