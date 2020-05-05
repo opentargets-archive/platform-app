9cc961f024bddef6e6a7f6b5a645a649e73c71f7
Alfredo Miranda <alfredo@miranda.io>
2020-05-05 17:08:28 -0500

    Fix Mechanisms of Action Summary

--- src/public/drug/sections/MechanismsOfAction/Summary.js
+++ src/public/drug/sections/MechanismsOfAction/Summary.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Summary = ({ uniqueActionTypes, uniqueTargetTypes }) => (
-  <>
-    {uniqueActionTypes.length > 0 ? uniqueActionTypes.join(' • ') : null}
-    <br />
-    {uniqueTargetTypes.length > 0 ? uniqueTargetTypes.join(' • ') : null}
-  </>
-);
+const Summary = ({ data }) => {
+  const { uniqueActionTypes, uniqueTargetTypes } = data;
+  return (
+    <>
+      {uniqueActionTypes.length > 0 ? uniqueActionTypes.join(' • ') : null}
+      <br />
+      {uniqueTargetTypes.length > 0 ? uniqueTargetTypes.join(' • ') : null}
+    </>
+  );
+};
 
 export default Summary;
