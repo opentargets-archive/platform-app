db064574d08e110f4901a4ccc5f6aed518336ba9
Javier Ferrer <jferrer@ebi.ac.uk>
2020-05-11 16:36:21 +0200

    dynamic size and colors in legend

--- src/public/common/LinearVenn.js
+++ src/public/common/LinearVenn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import withTheme from '@material-ui/core/styles/withTheme';
 
 const WIDTH = 400;
@@ -70,94 +70,130 @@ const LinearVenn = ({ theme, aOnly, aAndB, bOnly, max }) => {
   );
 };
 
-const LEGEND_SQUARE_SIZE = 15;
-const LEGEND_PADDING = 3;
-const Legend = ({ theme, a, b, aAndB }) => (
-  <svg width={WIDTH} height={LEGEND_SQUARE_SIZE * 3 + LEGEND_PADDING * 2}>
-    <defs>
-      <pattern
-        id="diagonalHatchLegend"
-        width="4"
-        height="4"
-        patternTransform="rotate(45 0 0)"
-        patternUnits="userSpaceOnUse"
-      >
-        <line
-          x1="1"
-          y1="0"
-          x2="1"
-          y2="4"
-          strokeWidth="2"
-          stroke={theme.palette.primary.main}
-        />
-        <line
-          x1="3"
-          y1="0"
-          x2="3"
-          y2="4"
-          strokeWidth="2"
-          stroke={theme.palette.secondary.main}
-        />
-      </pattern>
-    </defs>
-    <g>
-      <rect
-        x={0}
-        y={0}
-        width={LEGEND_SQUARE_SIZE}
-        height={LEGEND_SQUARE_SIZE}
-        fill={theme.palette.primary.main}
-        stroke={'white'}
-      />
-      <text
-        x={LEGEND_SQUARE_SIZE + LEGEND_PADDING}
-        y={LEGEND_SQUARE_SIZE / 2}
-        fill={theme.palette.text.primary}
-        dominantBaseline="middle"
-      >
-        {a}
-      </text>
-    </g>
-    <g transform={`translate(0,${LEGEND_SQUARE_SIZE + LEGEND_PADDING})`}>
-      <rect
-        x={0}
-        y={0}
-        width={LEGEND_SQUARE_SIZE}
-        height={LEGEND_SQUARE_SIZE}
-        fill="url(#diagonalHatchLegend)"
-        stroke={'white'}
-      />
-      <text
-        x={LEGEND_SQUARE_SIZE + LEGEND_PADDING}
-        y={LEGEND_SQUARE_SIZE / 2}
-        fill={theme.palette.text.primary}
-        dominantBaseline="middle"
-      >
-        {aAndB}
-      </text>
-    </g>
-    <g
-      transform={`translate(0,${LEGEND_SQUARE_SIZE * 2 + LEGEND_PADDING * 2})`}
+function Legend({ theme, a, b, aAndB, tooltip }) {
+  const [legendWidth, setLegendWidth] = useState(0);
+  const LEGEND_SQUARE_SIZE = 15;
+  const LEGEND_PADDING = 6;
+
+  const aText = useRef(null);
+  const bText = useRef(null);
+  const aAndBText = useRef(null);
+
+  useEffect(() => {
+    const aTextLength = aText.current?.getBBox().width;
+    const bTextLength = bText.current?.getBBox().width;
+    const aAndBTextLength = aAndBText.current?.getBBox().width;
+    const longestText = Math.max(aTextLength, bTextLength, aAndBTextLength);
+
+    setLegendWidth(LEGEND_SQUARE_SIZE + LEGEND_PADDING + longestText);
+  }, []);
+
+  return (
+    <svg
+      height={LEGEND_SQUARE_SIZE * 3 + LEGEND_PADDING * 3}
+      width={legendWidth}
     >
-      <rect
-        x={0}
-        y={0}
-        width={LEGEND_SQUARE_SIZE}
-        height={LEGEND_SQUARE_SIZE}
-        fill={theme.palette.secondary.main}
-        stroke={'white'}
-      />
-      <text
-        x={LEGEND_SQUARE_SIZE + LEGEND_PADDING}
-        y={LEGEND_SQUARE_SIZE / 2}
-        fill={theme.palette.text.primary}
-        dominantBaseline="middle"
+      <defs>
+        <pattern
+          id="diagonalHatchLegend"
+          width="4"
+          height="4"
+          patternTransform="rotate(45 0 0)"
+          patternUnits="userSpaceOnUse"
+        >
+          <line
+            x1="1"
+            y1="0"
+            x2="1"
+            y2="4"
+            strokeWidth="2"
+            stroke={theme.palette.primary.main}
+          />
+          <line
+            x1="3"
+            y1="0"
+            x2="3"
+            y2="4"
+            strokeWidth="2"
+            stroke={theme.palette.secondary.main}
+          />
+        </pattern>
+      </defs>
+      <g transform={`translate(0,${LEGEND_PADDING})`}>
+        <rect
+          x={0}
+          y={-2}
+          width={LEGEND_SQUARE_SIZE}
+          height={LEGEND_SQUARE_SIZE}
+          fill={theme.palette.primary.main}
+          stroke={'white'}
+        />
+        <text
+          x={LEGEND_SQUARE_SIZE + LEGEND_PADDING}
+          y={LEGEND_SQUARE_SIZE / 2}
+          fill={
+            tooltip
+              ? theme.palette.primary.contrastText
+              : theme.palette.text.primary
+          }
+          dominantBaseline="middle"
+          ref={aText}
+        >
+          {a}
+        </text>
+      </g>
+      <g transform={`translate(0,${LEGEND_SQUARE_SIZE + LEGEND_PADDING * 2})`}>
+        <rect
+          x={0}
+          y={-2}
+          width={LEGEND_SQUARE_SIZE}
+          height={LEGEND_SQUARE_SIZE}
+          fill="url(#diagonalHatchLegend)"
+          stroke={'white'}
+        />
+        <text
+          x={LEGEND_SQUARE_SIZE + LEGEND_PADDING}
+          y={LEGEND_SQUARE_SIZE / 2}
+          fill={
+            tooltip
+              ? theme.palette.primary.contrastText
+              : theme.palette.text.primary
+          }
+          dominantBaseline="middle"
+          ref={aAndBText}
+        >
+          {aAndB}
+        </text>
+      </g>
+      <g
+        transform={`translate(0,${LEGEND_SQUARE_SIZE * 2 +
+          LEGEND_PADDING * 3})`}
       >
-        {b}
-      </text>
-    </g>
-  </svg>
-);
+        <rect
+          x={0}
+          y={-2}
+          width={LEGEND_SQUARE_SIZE}
+          height={LEGEND_SQUARE_SIZE}
+          fill={theme.palette.secondary.main}
+          stroke={'white'}
+        />
+        <text
+          x={LEGEND_SQUARE_SIZE + LEGEND_PADDING}
+          y={LEGEND_SQUARE_SIZE / 2}
+          fill={
+            tooltip
+              ? theme.palette.primary.contrastText
+              : theme.palette.text.primary
+          }
+          dominantBaseline="middle"
+          ref={bText}
+        >
+          {b}
+        </text>
+      </g>
+    </svg>
+  );
+}
 
 export const LinearVennLegend = withTheme(Legend);
 
