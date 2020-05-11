0481c90f9e49e234505322d9664a4cb77741a627
Fedde Schaeffer <fedde@thehyve.nl>
2020-05-11 20:47:11 +0200

    Port Target Safety to the GraphQL API
    Re-enabling the Safety section on the target page.
    
    In the process, port the logic to format links based on Pubmed IDs from
    the Angular webapp and make some of the list item keys passed to React
    more robust (with the assumption that the same effect or publication
    will not appear in the same row twice).

--- src/public/target/sections/Safety/Section.js
+++ src/public/target/sections/Safety/Section.js
@@ -1,84 +1,86 @@
 import React from 'react';
-import { capitalize } from 'lodash';
+import _ from 'lodash';
 import Typography from '@material-ui/core/Typography';
 import Tooltip from '@material-ui/core/Tooltip';
 
 import { OtTableRF, Link, DataDownloader } from 'ot-ui';
 
+const Section = ({ symbol, data }) => {
+  const { adverseEffects, safetyRiskInfo } = data;
+  return (
+    <>
+      <Typography variant="h6">Known safety effects</Typography>
+      <DataDownloader
+        tableHeaders={effectsColumns}
+        rows={getAdverseDownloadData(adverseEffects)}
+        fileStem={`${symbol}-safety-effects`}
+      />
+      <OtTableRF columns={effectsColumns} data={adverseEffects} />
+      <Typography variant="h6">Safety risk information</Typography>
+      <DataDownloader
+        tableHeaders={riskColumns}
+        rows={getRiskDownloadData(safetyRiskInfo)}
+        fileStem={`${symbol}-risk-information`}
+      />
+      <OtTableRF columns={riskColumns} data={safetyRiskInfo} />
+    </>
+  );
+};
+
 const effectsColumns = [
   {
     id: 'organs_systems_affected',
     label: 'Main organs & systems affected',
-    renderCell: ({ organs_systems_affected: organs }) => {
-      return (
-        <ul>
-          {organs.map(organ => (
-            <li key={organ.code}>{organ.mapped_term}</li>
+    renderCell: ({ organsSystemsAffected }) => (
+      <ul>
+        {organsSystemsAffected
+          .filter(organ => organ.code && organ.mappedTerm)
+          .map(organ => (
+            <li key={organ.code}>{organ.mappedTerm}</li>
           ))}
-        </ul>
-      );
-    },
+      </ul>
+    ),
   },
   {
     id: 'activation_effects',
     label: 'Agonism or activation effects',
-    renderCell: ({ activation_effects: activationEffects }) => {
-      return Object.keys(activationEffects).map(key => {
-        return (
-          <React.Fragment key={key}>
-            <Typography variant="subtitle2">{capitalize(key)}</Typography>
-            <ul>
-              {activationEffects[key].map((effect, i) => (
-                <li key={i}>{effect.mapped_term || effect.term_in_paper}</li>
-              ))}
-            </ul>
-          </React.Fragment>
-        );
-      });
-    },
+    renderCell: ({ activationEffects }) =>
+      listTermsByGroup(activationEffects).map(({ groupKey, terms }) => (
+        <React.Fragment key={groupKey}>
+          <Typography variant="subtitle2">
+            {_.capitalize(_.lowerCase(groupKey))}
+          </Typography>
+          <ul>
+            {terms.map(term => (
+              <li key={`${groupKey}:${term}`}>{term}</li>
+            ))}
+          </ul>
+        </React.Fragment>
+      )),
   },
   {
     id: 'inhibition_effects',
     label: 'Antagonism or inhibition effects',
-    renderCell: ({ inhibition_effects: inhibitionEffects }) => {
-      return Object.keys(inhibitionEffects).map(key => {
-        return (
-          <React.Fragment key={key}>
-            <Typography variant="subtitle2">{capitalize(key)}</Typography>
-            <ul>
-              {inhibitionEffects[key].map((effect, i) => (
-                <li key={i}>{effect.mapped_term || effect.term_in_paper}</li>
-              ))}
-            </ul>
-          </React.Fragment>
-        );
-      });
-    },
+    renderCell: ({ inhibitionEffects }) =>
+      listTermsByGroup(inhibitionEffects).map(({ groupKey, terms }) => (
+        <React.Fragment key={groupKey}>
+          <Typography variant="subtitle2">
+            {_.capitalize(_.lowerCase(groupKey))}
+          </Typography>
+          <ul>
+            {terms.map(term => (
+              <li key={`${groupKey}:${term}`}>{term}</li>
+            ))}
+          </ul>
+        </React.Fragment>
+      )),
   },
   {
     id: 'references',
     label: 'Publications',
-    renderCell: ({ references }) => {
-      return references.map((reference, i) => {
-        const isHecatos = reference.ref_link.indexOf('hecatos') !== -1;
-        return (
-          <React.Fragment key={i}>
-            <Link external to={reference.ref_link}>
-              <Tooltip
-                title={
-                  isHecatos
-                    ? "HeCaToS Deliverable D01.5 (2015) funded by 'EU 7th Framework Programme (HEALTH-F4-2013-602156).'"
-                    : ''
-                }
-                placement="top"
-              >
-                <span>{reference.ref_label}</span>
-              </Tooltip>
-            </Link>{' '}
-          </React.Fragment>
-        );
-      });
-    },
+    renderCell: ({ references }) => (
+      <ReferencesCell>{references}</ReferencesCell>
+    ),
   },
 ];
 
@@ -86,136 +88,111 @@ const riskColumns = [
   {
     id: 'organs_systems_affected',
     label: 'Main organs & systems affected',
-    renderCell: ({ organs_systems_affected: organs }) => {
-      return (
-        <ul>
-          {organs.map((organ, i) => (
-            <li key={i}>{organ.mapped_term || organ.term_in_paper}</li>
+    renderCell: ({ organsSystemsAffected }) => (
+      <ul>
+        {organsSystemsAffected
+          .filter(
+            organ => organ.code && (organ.mappedTerm || organ.termInPaper)
+          )
+          .map(organ => (
+            <li key={organ.code}>{organ.mappedTerm || organ.termInPaper}</li>
           ))}
-        </ul>
-      );
-    },
+      </ul>
+    ),
   },
   {
     id: 'safety_liability',
     label: 'Safety liability information',
-    renderCell: ({ safety_liability }) => {
-      return safety_liability;
-    },
+    renderCell: ({ safetyLiability }) => safetyLiability,
   },
   {
     id: 'references',
     label: 'Publications',
-    renderCell: ({ references }) => {
-      return references.map((reference, i) => {
-        const isHecatos = reference.ref_link.indexOf('hecatos') !== -1;
-        return (
-          <React.Fragment key={i}>
-            <Link external to={reference.ref_link}>
-              <Tooltip
-                title={
-                  isHecatos
-                    ? "HeCaToS Deliverable D01.5 (2015) funded by 'EU 7th Framework Programme (HEALTH-F4-2013-602156).'"
-                    : ''
-                }
-                placement="top"
-              >
-                <span>{reference.ref_label}</span>
-              </Tooltip>
-            </Link>{' '}
-          </React.Fragment>
-        );
-      });
-    },
+    renderCell: ({ references }) => (
+      <ReferencesCell>{references}</ReferencesCell>
+    ),
   },
 ];
 
+const ReferencesCell = ({ children }) =>
+  children
+    .filter(reference => reference.pubmedId || reference.refLink)
+    .map(({ pubmedId, refLink, refLabel }) => {
+      const pubUrl = pubmedId
+        ? 'https://europepmc.org/abstract/MED/' + pubmedId
+        : refLink;
+      const isHecatos = refLink && refLink.indexOf('hecatos') !== -1;
+      return (
+        <React.Fragment key={`${refLabel}:${pubUrl}`}>
+          <Link external to={pubUrl}>
+            <Tooltip
+              title={
+                isHecatos
+                  ? "HeCaToS Deliverable D01.5 (2015) funded by 'EU 7th Framework Programme (HEALTH-F4-2013-602156).'"
+                  : ''
+              }
+              placement="top"
+            >
+              <span>
+                {refLabel ||
+                  (pubmedId ? `Pubmed ID: ${pubmedId}` : '(no name)')}
+              </span>
+            </Tooltip>
+          </Link>{' '}
+        </React.Fragment>
+      );
+    });
+
 const getAdverseDownloadData = rows => {
-  return rows.map(row => {
-    const activationEffects = [];
-    const inhibitionEffects = [];
+  const flattenEffects = subGraph =>
+    _.flatMap(listTermsByGroup(subGraph), ({ terms }) => terms);
 
-    Object.keys(row.activation_effects).forEach(key => {
-      row.activation_effects[key].forEach(effect => {
-        activationEffects.push(effect.mapped_term || effect.term_in_paper);
-      });
-    });
-
-    Object.keys(row.inhibition_effects).forEach(key => {
-      row.inhibition_effects[key].forEach(effect => {
-        inhibitionEffects.push(effect.mapped_term || effect.term_in_paper);
-      });
-    });
-
-    return {
-      organs_systems_affected: row.organs_systems_affected
-        .map(organ => organ.mapped_term)
-        .join(', '),
-      activation_effects: activationEffects.join(', '),
-      inhibition_effects: inhibitionEffects.join(', '),
-      references: row.references.map(ref => ref.ref_link).join(', '),
-    };
-  });
+  return rows.map(row => ({
+    organs_systems_affected: row.organsSystemsAffected
+      .map(organ => organ.mappedTerm)
+      .filter(term => term)
+      .join(', '),
+    activation_effects: flattenEffects(row.activationEffects).join(', '),
+    inhibition_effects: flattenEffects(row.inhibitionEffects).join(', '),
+    references: row.references
+      .map(ref =>
+        ref.pubmedId
+          ? 'https://europepmc.org/abstract/MED/' + ref.pubmedId
+          : ref.refLink
+      )
+      .filter(link => link)
+      .join(', '),
+  }));
 };
 
 const getRiskDownloadData = rows => {
-  return rows.map(row => {
-    return {
-      organs_systems_affected: row.organs_systems_affected
-        .map(organ => organ.mapped_term || organ.term_in_paper)
-        .join(', '),
-      safety_liability: row.safety_liability,
-      references: row.references.map(ref => ref.ref_link).join(', '),
-    };
-  });
+  return rows.map(row => ({
+    organs_systems_affected: row.organsSystemsAffected
+      .map(organ => organ.mappedTerm || organ.termInPaper)
+      .filter(term => term)
+      .join(', '),
+    safety_liability: row.safetyLiability,
+    references: row.references
+      .map(ref =>
+        ref.pubmedId
+          ? 'https://europepmc.org/abstract/MED/' + ref.pubmedId
+          : ref.refLink
+      )
+      .filter(link => link)
+      .join(', '),
+  }));
 };
 
-class Section extends React.Component {
-  state = { loading: true };
-
-  componentDidMount() {
-    // TODO: safety data should be loaded through graphql api
-    fetch(
-      `https://platform-api-qc.opentargets.io/v3/platform/private/target/${this.props.ensgId}`
-    )
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ loading: false, safety: data.safety });
-      });
-  }
-
-  render() {
-    const { symbol } = this.props;
-    const { loading, safety } = this.state;
-
-    if (!safety) {
-      return null;
-    }
-
-    const {
-      adverse_effects: adverseEffects = [],
-      safety_risk_info: safetyRiskInfo = [],
-    } = safety;
-
-    return !loading ? (
-      <React.Fragment>
-        <Typography variant="h6">Known safety effects</Typography>
-        <DataDownloader
-          tableHeaders={effectsColumns}
-          rows={getAdverseDownloadData(adverseEffects)}
-          fileStem={`${symbol}-safety-effects`}
-        />
-        <OtTableRF columns={effectsColumns} data={adverseEffects} />
-        <Typography variant="h6">Safety risk information</Typography>
-        <DataDownloader
-          tableHeaders={riskColumns}
-          rows={getRiskDownloadData(safetyRiskInfo)}
-          fileStem={`${symbol}-risk-information`}
-        />
-        <OtTableRF columns={riskColumns} data={safetyRiskInfo} />
-      </React.Fragment>
-    ) : null;
-  }
-}
+const listTermsByGroup = effectsSubGraph =>
+  Object.entries(effectsSubGraph)
+    // exclude GraphQL introspection properties
+    .filter(([key, __]) => !key.startsWith('__'))
+    .map(([groupKey, effects]) => ({
+      groupKey,
+      terms: effects
+        .map(effect => effect.mappedTerm || effect.termInPaper)
+        .filter(term => term),
+    }))
+    .filter(({ terms }) => terms.length);
 
 export default Section;
--- src/public/target/sections/Safety/index.js
+++ src/public/target/sections/Safety/index.js
@@ -1,5 +1,22 @@
+import { loader } from 'graphql.macro';
+import gql from 'graphql-tag';
+
 export const id = 'safety';
 export const name = 'Safety';
+
+export const hasSummaryData = data => data;
+
+export const summaryQuery = loader('./summaryQuery.gql');
+export const sectionQuery = gql`
+  query TargetSafetySectionQuery($ensgId: String!) {
+    target(ensemblId: $ensgId) {
+      id
+      ...targetSafetyFragment
+    }
+  }
+
+  ${summaryQuery}
+`;
 
 export { default as DescriptionComponent } from './Description';
 export { default as SummaryComponent } from './Summary';
