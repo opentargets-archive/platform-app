994a90d440d106fd6d65c2ea4f591d3ae1a32f1b
Alfredo Miranda <alfredo@miranda.io>
2020-05-13 18:37:38 -0500

    Fix error in cancer hallmarks hasSummaryData function

--- src/public/target/sections/CancerHallmarks/index.js
+++ src/public/target/sections/CancerHallmarks/index.js
@@ -3,7 +3,7 @@ import { loader } from 'graphql.macro';
 export const id = 'hallmarks';
 export const name = 'Cancer Hallmarks';
 
-export const hasSummaryData = ({ data }) => data.rows.length > 0;
+export const hasSummaryData = data => data && data.rows.length > 0;
 
 export const summaryQuery = loader('./summaryQuery.gql');
 export const sectionQuery = loader('./sectionQuery.gql');
