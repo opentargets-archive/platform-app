d7d6857f6d28a6b959b3a1ff215d55c29e62b03b
Alfredo Miranda <alfredo@miranda.io>
2020-05-13 13:50:56 -0500

    Remove unused import

--- src/public/disease/sections/Ontology/index.js
+++ src/public/disease/sections/Ontology/index.js
@@ -1,11 +1,7 @@
-import { loader } from 'graphql.macro';
-
 export const id = 'ontology';
 export const name = 'Ontology';
 
 export const hasSummaryData = () => true; // all terms are in the ontology
-
-// export const sectionQuery = loader('./sectionQuery.gql');
 
 export { default as DescriptionComponent } from './Description';
 export { default as SummaryComponent } from './Summary';
