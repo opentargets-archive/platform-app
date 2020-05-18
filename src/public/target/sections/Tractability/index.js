847063ce6659de524b02bfdd2fb79dc63ca6b0f0
Ruslan Forostianov <ruslan@thehyve.nl>
2020-05-18 15:40:32 +0200

    Show tractability section if other modal. exists
    Fixes a bug: the tractability section would not be shown
    if there is only other modalities data.

--- src/public/target/sections/Tractability/index.js
+++ src/public/target/sections/Tractability/index.js
@@ -7,7 +7,8 @@ export const shortName = 'TR';
 
 export const hasSummaryData = data =>
   _.get(data, 'antibody.buckets.length', 0) > 0 ||
-  _.get(data, 'smallmolecule.buckets.length', 0) > 0;
+  _.get(data, 'smallmolecule.buckets.length', 0) > 0 ||
+  _.get(data, 'otherModalities.buckets.length', 0) > 0;
 
 export const summaryQuery = loader('./summaryQuery.gql');
 export const sectionQuery = loader('./sectionQuery.gql');
