d098bd15115c27af9da361cdd3a3f4b4f73861cb
Ruslan Forostianov <ruslan@thehyve.nl>
2020-05-19 17:16:24 +0200

    Show message if no tractability modality buckets
    Co-authored-by: Fedde Schaeffer <fedde@thehyve.nl>

--- src/public/target/sections/Tractability/Section.js
+++ src/public/target/sections/Tractability/Section.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import _ from 'lodash';
 
-// make a style hook to share between the components in this file
+// generate classNames to share between the components in this file
 const useStyles = makeStyles(theme => ({
   table: {
     borderCollapse: 'collapse',
@@ -22,142 +22,162 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const Section = ({ data }) => {
-  const classes = useStyles();
-  const isSmallMoleculeCellPurple = bucket =>
-    _.get(data, 'smallmolecule.buckets', []).indexOf(bucket) >= 0;
-  const isAntibodyCellPurple = bucket =>
-    _.get(data, 'antibody.buckets', []).indexOf(bucket) >= 0;
-  const isOtherModalitiesCellPurple = bucket =>
-    _.get(data, 'otherModalities.buckets', []).indexOf(bucket) >= 0;
+const Section = ({ symbol, data }) => {
+  const smallMoleculeBuckets = _.get(data, 'smallmolecule.buckets', []);
+  const antibodyBuckets = _.get(data, 'antibody.buckets', []);
+  const otherModalitiesBuckets = _.get(data, 'otherModalities.buckets', []);
+
   return (
-    <Fragment>
+    <>
       <Typography variant="h6">Small molecule</Typography>
-      <table className={classes.table}>
-        <thead>
-          <tr>
-            <th className={classes.cell} colSpan="3">
-              <Typography variant="subtitle2">Clinical precedence</Typography>
-            </th>
-            <th className={classes.cell} colSpan="2">
-              <Typography variant="subtitle2">Discovery precedence</Typography>
-            </th>
-            <th className={classes.cell} colSpan="3">
-              <Typography variant="subtitle2">Predicted tractable</Typography>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {/* clinical precedence */}
-            <BucketCell isPurple={isSmallMoleculeCellPurple(1)}>
-              Phase 4
-            </BucketCell>
-            <BucketCell isPurple={isSmallMoleculeCellPurple(2)}>
-              Phase 2 or 3
-            </BucketCell>
-            <BucketCell isPurple={isSmallMoleculeCellPurple(3)}>
-              Phase 0 or 1
-            </BucketCell>
-            {/* discovery precedence */}
-            <BucketCell isPurple={isSmallMoleculeCellPurple(4)}>
-              PDB targets with ligands
-            </BucketCell>
-            <BucketCell isPurple={isSmallMoleculeCellPurple(7)}>
-              Active compounds in ChEMBL
-            </BucketCell>
-            {/* predicted tractable */}
-            <BucketCell isPurple={isSmallMoleculeCellPurple(5)}>
-              DrugEBIlity score > 0.7
-            </BucketCell>
-            <BucketCell isPurple={isSmallMoleculeCellPurple(6)}>
-              DrugEBIlity score 0 to 0.7
-            </BucketCell>
-            <BucketCell isPurple={isSmallMoleculeCellPurple(8)}>
-              Druggable genome
-            </BucketCell>
-          </tr>
-        </tbody>
-      </table>
+      {smallMoleculeBuckets.length > 0 ? (
+        <SmallMoleculeTable buckets={smallMoleculeBuckets} />
+      ) : (
+        <Typography>
+          No small molecule tractability data for {symbol} available
+        </Typography>
+      )}
       <Typography variant="h6">Antibody</Typography>
-      <table className={classes.table}>
-        <thead>
-          <tr>
-            <th className={classes.cell} colSpan="3">
-              <Typography variant="subtitle2">Clinical precedence</Typography>
-            </th>
-            <th className={classes.cell} colSpan="2">
-              <Typography variant="subtitle2">
-                Predicted tractable - high confidence
-              </Typography>
-            </th>
-            <th className={classes.cell} colSpan="4">
-              <Typography variant="subtitle2">
-                Predicted tractable - medium to low confidence
-              </Typography>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {/* clinical precedence */}
-            <BucketCell isPurple={isAntibodyCellPurple(1)}>Phase 4</BucketCell>
-            <BucketCell isPurple={isAntibodyCellPurple(2)}>
-              Phase 2 or 3
-            </BucketCell>
-            <BucketCell isPurple={isAntibodyCellPurple(3)}>
-              Phase 0 or 1
-            </BucketCell>
-            {/* predicted tractable (high) */}
-            <BucketCell isPurple={isAntibodyCellPurple(4)}>
-              UniProt location - high confidence
-            </BucketCell>
-            <BucketCell isPurple={isAntibodyCellPurple(5)}>
-              GO cell component - high confidence
-            </BucketCell>
-            {/* predicted tractable (mid-low) */}
-            <BucketCell isPurple={isAntibodyCellPurple(6)}>
-              UniProt location - low or unknown confidence
-            </BucketCell>
-            <BucketCell isPurple={isAntibodyCellPurple(7)}>
-              UniProt predicted signal peptide or transmembrane region
-            </BucketCell>
-            <BucketCell isPurple={isAntibodyCellPurple(8)}>
-              GO cell component - medium confidence
-            </BucketCell>
-            {/* predicted tractable (HPA) */}
-            <BucketCell isPurple={isAntibodyCellPurple(9)}>
-              Human Protein Atlas - high confidence
-            </BucketCell>
-          </tr>
-        </tbody>
-      </table>
+      {antibodyBuckets.length > 0 ? (
+        <AntibodyTable buckets={antibodyBuckets} />
+      ) : (
+        <Typography>
+          No antibody tractability data for {symbol} available
+        </Typography>
+      )}
       <Typography variant="h6">
         Other modalities (protein, enzyme, oligonucleotide, etc.)
       </Typography>
-      <table className={classes.table}>
-        <thead>
-          <tr>
-            <th className={classes.cell} colSpan="3">
-              <Typography variant="subtitle2">Clinical precedence</Typography>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <BucketCell isPurple={isOtherModalitiesCellPurple(1)}>
-              Phase 4
-            </BucketCell>
-            <BucketCell isPurple={isOtherModalitiesCellPurple(2)}>
-              Phase 2 or 3
-            </BucketCell>
-            <BucketCell isPurple={isOtherModalitiesCellPurple(3)}>
-              Phase 0 or 1
-            </BucketCell>
-          </tr>
-        </tbody>
-      </table>
-    </Fragment>
+      {otherModalitiesBuckets.length > 0 ? (
+        <OtherModalitiesTable buckets={otherModalitiesBuckets} />
+      ) : (
+        <Typography>
+          No other modalities tractability data for {symbol} available
+        </Typography>
+      )}
+    </>
+  );
+};
+
+const SmallMoleculeTable = ({ buckets }) => {
+  const classes = useStyles();
+  return (
+    <table className={classes.table}>
+      <thead>
+        <tr>
+          <th className={classes.cell} colSpan="3">
+            <Typography variant="subtitle2">Clinical precedence</Typography>
+          </th>
+          <th className={classes.cell} colSpan="2">
+            <Typography variant="subtitle2">Discovery precedence</Typography>
+          </th>
+          <th className={classes.cell} colSpan="3">
+            <Typography variant="subtitle2">Predicted tractable</Typography>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {/* clinical precedence */}
+          <BucketCell isPurple={buckets.includes(1)}>Phase 4</BucketCell>
+          <BucketCell isPurple={buckets.includes(2)}>Phase 2 or 3</BucketCell>
+          <BucketCell isPurple={buckets.includes(3)}>Phase 0 or 1</BucketCell>
+          {/* discovery precedence */}
+          <BucketCell isPurple={buckets.includes(4)}>
+            PDB targets with ligands
+          </BucketCell>
+          <BucketCell isPurple={buckets.includes(7)}>
+            Active compounds in ChEMBL
+          </BucketCell>
+          {/* predicted tractable */}
+          <BucketCell isPurple={buckets.includes(5)}>
+            DrugEBIlity score > 0.7
+          </BucketCell>
+          <BucketCell isPurple={buckets.includes(6)}>
+            DrugEBIlity score 0 to 0.7
+          </BucketCell>
+          <BucketCell isPurple={buckets.includes(8)}>
+            Druggable genome
+          </BucketCell>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const AntibodyTable = ({ buckets }) => {
+  const classes = useStyles();
+  return (
+    <table className={classes.table}>
+      <thead>
+        <tr>
+          <th className={classes.cell} colSpan="3">
+            <Typography variant="subtitle2">Clinical precedence</Typography>
+          </th>
+          <th className={classes.cell} colSpan="2">
+            <Typography variant="subtitle2">
+              Predicted tractable - high confidence
+            </Typography>
+          </th>
+          <th className={classes.cell} colSpan="4">
+            <Typography variant="subtitle2">
+              Predicted tractable - medium to low confidence
+            </Typography>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {/* clinical precedence */}
+          <BucketCell isPurple={buckets.includes(1)}>Phase 4</BucketCell>
+          <BucketCell isPurple={buckets.includes(2)}>Phase 2 or 3</BucketCell>
+          <BucketCell isPurple={buckets.includes(3)}>Phase 0 or 1</BucketCell>
+          {/* predicted tractable (high) */}
+          <BucketCell isPurple={buckets.includes(4)}>
+            UniProt location - high confidence
+          </BucketCell>
+          <BucketCell isPurple={buckets.includes(5)}>
+            GO cell component - high confidence
+          </BucketCell>
+          {/* predicted tractable (mid-low) */}
+          <BucketCell isPurple={buckets.includes(6)}>
+            UniProt location - low or unknown confidence
+          </BucketCell>
+          <BucketCell isPurple={buckets.includes(7)}>
+            UniProt predicted signal peptide or transmembrane region
+          </BucketCell>
+          <BucketCell isPurple={buckets.includes(8)}>
+            GO cell component - medium confidence
+          </BucketCell>
+          {/* predicted tractable (HPA) */}
+          <BucketCell isPurple={buckets.includes(9)}>
+            Human Protein Atlas - high confidence
+          </BucketCell>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const OtherModalitiesTable = ({ buckets }) => {
+  const classes = useStyles();
+  return (
+    <table className={classes.table}>
+      <thead>
+        <tr>
+          <th className={classes.cell} colSpan="3">
+            <Typography variant="subtitle2">Clinical precedence</Typography>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <BucketCell isPurple={buckets.includes(1)}>Phase 4</BucketCell>
+          <BucketCell isPurple={buckets.includes(2)}>Phase 2 or 3</BucketCell>
+          <BucketCell isPurple={buckets.includes(3)}>Phase 0 or 1</BucketCell>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
