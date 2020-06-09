181ace8ce200f71e55c1efec1a36dac1b29b6776
Alfredo Miranda <alfredo@miranda.io>
2020-06-08 19:34:21 -0500

    Remove stats section and change quick links

--- src/public/home/Page.js
+++ src/public/home/Page.js
@@ -23,15 +23,17 @@ const HomePage = () => {
           <Search autoFocus />
           <Grid container justify="space-around" style={{ marginTop: '12px' }}>
             <Link to="/target/ENSG00000091831">ESR1</Link>
+            <Link to="/target/ENSG00000123374">ENSG00000123374</Link>
             <Link to="/disease/EFO_0000384">Crohn's disease</Link>
+            <Link to="/disease/Orphanet_839">Orphanet_839</Link>
+            <Link to="/disease/EFO_0003060">EFO_0003060</Link>
             <Link to="/drug/CHEMBL2111100">MIFAMURTIDE</Link>
-            <Link to="/evidence/ENSG00000091831/EFO_0000305">
-              Evidence page
-            </Link>
+            <Link to="/drug/CHEMBL1201580">Humira</Link>
           </Grid>
         </HomeBox>
       </Grid>
-      <Stats />
+      {/* remove for integration day  */}
+      {/* <Stats /> */}
       <Footer externalLinks={externalLinks} />
     </>
   );
