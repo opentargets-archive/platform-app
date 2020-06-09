917fbc084f7fe4ed29d26b3410a870efba512b7f
Alfredo Miranda <alfredo@miranda.io>
2020-06-09 15:15:38 -0500

    Add notice message

--- src/public/home/Page.js
+++ src/public/home/Page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 import { Link, NavBar, Footer } from 'ot-ui';
@@ -9,7 +10,17 @@ import HomeBox from './HomeBox';
 import Search from '../common/search/Search';
 import { externalLinks, mainMenuItems } from '../../constants';
 
+const useStyles = makeStyles({
+  links: {
+    marginTop: '12px',
+  },
+  api: {
+    marginTop: '38px',
+  },
+});
+
 const HomePage = () => {
+  const classes = useStyles();
   return (
     <>
       <Grid container justify="center" alignItems="center">
@@ -22,7 +33,7 @@ const HomePage = () => {
         />
         <HomeBox name="Platform">
           <Search autoFocus />
-          <Grid container justify="space-around" style={{ marginTop: '12px' }}>
+          <Grid className={classes.links} container justify="space-around">
             <Link to="/target/ENSG00000091831">ESR1</Link>
             <Link to="/target/ENSG00000123374">ENSG00000123374</Link>
             <Link to="/disease/EFO_0000384">Crohn's disease</Link>
@@ -30,6 +41,20 @@ const HomePage = () => {
             <Link to="/disease/EFO_0003060">EFO_0003060</Link>
             <Link to="/drug/CHEMBL2111100">MIFAMURTIDE</Link>
             <Link to="/drug/CHEMBL1201580">Humira</Link>
+          </Grid>
+          <Grid
+            className={classes.api}
+            container
+            alignItems="center"
+            direction="column"
+          >
+            <div>Looking to access our data?</div>
+            <Link
+              to="https://api-beta-dot-open-targets-eu-dev.appspot.com"
+              external
+            >
+              Browse our GraphQL API
+            </Link>
           </Grid>
         </HomeBox>
       </Grid>
