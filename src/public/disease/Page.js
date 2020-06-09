9340b21ee6e192f2f7f85cf6853d5db71e46ccfe
Luca Fumis <lucaf@ebi.ac.uk>
2020-06-09 19:35:12 +0100

    Disease page: link to angular platform; issue opentargets/platform#1091

--- src/public/disease/Page.js
+++ src/public/disease/Page.js
@@ -25,8 +25,16 @@ const DISEASE_QUERY = gql`
 
 const DiseasePage = ({ history, location, match }) => {
   const handleChange = (event, value) => {
-    const path = value === 'overview' ? match.url : `${match.url}/${value}`;
-    history.push(path);
+    if (value.indexOf('http' === 0)) {
+      // navigte to external page: first store current page
+      // for back button to work correctly
+      // TODO: this link will be removed after alpha/beta
+      history.push(match.url);
+      window.location.replace(value);
+    } else {
+      const path = value === 'overview' ? match.url : `${match.url}/${value}`;
+      history.push(path);
+    }
   };
 
   const tab = location.pathname.endsWith('associations')
@@ -54,9 +62,13 @@ const DiseasePage = ({ history, location, match }) => {
         variant="scrollable"
         scrollButtons="auto"
       >
-        <Tab value="classic-associations" label="Associations (classic)" />
-        <Tab value="associations" label="Associations (dynamic)" />
+        {/* <Tab value="classic-associations" label="Associations (classic)" />
+        <Tab value="associations" label="Associations (dynamic)" /> */}
         <Tab value="overview" label="Profile" />
+        <Tab
+          value={`https://www.targetvalidation.org/disease/${efoId}`}
+          label="View this page in the classic view"
+        />
       </Tabs>
       <Switch>
         <Route
