8d7177fcda1fb5c3468aa30c0279c9c2acea33b3
Javier Ferrer <jferrer@ebi.ac.uk>
2020-06-11 15:24:06 +0200

    cancer hallmarks widget description (opentargets/platform#1094)

--- src/public/target/sections/CancerHallmarks/Description.js
+++ src/public/target/sections/CancerHallmarks/Description.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { Link } from 'ot-ui';
 
-const Description = () => (
+const Description = ({ symbol }) => (
   <React.Fragment>
-    Essential alterations in cell physiology that dictate malignant growth.
-    Cancer hallmarks, originally described by{' '}
-    <Link external to="https://www.cell.com/abstract/S0092-8674(00)81683-9">
-      Hanahan and Weinberg (2000)
-    </Link>{' '}
-    are manually curated by COSMIC and integrated in the Cancer Gene Census.
+    Role of <strong>{symbol}</strong> in essential alterations in cell
+    physiology that dictate malignant growth. Source:{' '}
+    <Link external to="https://cancer.sanger.ac.uk/cosmic">
+      COSMIC
+    </Link>
+    .
   </React.Fragment>
 );
 
