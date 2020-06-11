3d34891610f3cf6b743062757948120c2f07b2cf
Javier Ferrer <jferrer@ebi.ac.uk>
2020-06-11 17:30:14 +0200

    related targets widget description (opentargets/platform#1094)

--- src/public/target/sections/RelatedTargets/Description.js
+++ src/public/target/sections/RelatedTargets/Description.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'ot-ui';
 
 const Description = ({ symbol }) => (
   <React.Fragment>
-    Targets related to <strong>{symbol}</strong> based on shared disease
-    associations.
+    Targets that are similar to <strong>{symbol}</strong> based on their disease
+    association profiles. Source:{' '}
+    <Link
+      external
+      to="https://docs.targetvalidation.org/faq/how-do-you-compute-target-and-disease-similarities"
+    >
+      Open Targets
+    </Link>
+    .
   </React.Fragment>
 );
 
