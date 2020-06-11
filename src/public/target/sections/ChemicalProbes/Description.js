dce07721ebf8837080e2ac0800320383845ca0f0
Javier Ferrer <jferrer@ebi.ac.uk>
2020-06-11 15:28:38 +0200

    chemical probes widget description (opentargets/platform#1094)

--- src/public/target/sections/ChemicalProbes/Description.js
+++ src/public/target/sections/ChemicalProbes/Description.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'ot-ui';
 
 const Description = ({ symbol }) => (
   <React.Fragment>
-    Information on chemical probes that have been developed for{' '}
-    <strong>{symbol}</strong>.
+    Potent, selective and cell-permeable chemical modulators for{' '}
+    <strong>{symbol}</strong>. Source:{' '}
+    <Link external to="https://www.thesgc.org/chemical-probes">
+      SGC
+    </Link>
+    ,{' '}
+    <Link external to="https://probeminer.icr.ac.uk/">
+      ProbeMiner
+    </Link>
+    ,{' '}
+    <Link external to="https://chemicalprobes.org/">
+      ChemicalProbes.org
+    </Link>
+    .
   </React.Fragment>
 );
 
