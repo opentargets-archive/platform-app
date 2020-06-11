3db0fcead2d78a0943fac0571a91bc9c6b7110c8
Javier Ferrer <jferrer@ebi.ac.uk>
2020-06-11 17:49:55 +0200

    baseline expression widget description (opentargets/platform#1094)

--- src/public/target/sections/Expression/Description.js
+++ src/public/target/sections/Expression/Description.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import { Link } from 'ot-ui';
+
 const Description = ({ symbol }) => (
   <React.Fragment>
-    RNA and protein baseline expression for <strong>{symbol}</strong>.
+    RNA and protein baseline expression for <strong>{symbol}</strong>. Source:{' '}
+    <Link external to="https://www.ebi.ac.uk/gxa/home">
+      ExpressionAtlas
+    </Link>
+    ,{' '}
+    <Link external to="https://www.proteinatlas.org/">
+      HPA
+    </Link>{' '}
+    and{' '}
+    <Link external to="https://www.gtexportal.org/home/">
+      GTEx
+    </Link>
+    .
   </React.Fragment>
 );
 
