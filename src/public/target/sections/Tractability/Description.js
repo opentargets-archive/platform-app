2bef277bb3db2ce31b9c4eaf48cbdd23d1d5ec5d
Javier Ferrer <jferrer@ebi.ac.uk>
2020-06-11 17:35:18 +0200

    tractability widget description (opentargets/platform#1094)

--- src/public/target/sections/Tractability/Description.js
+++ src/public/target/sections/Tractability/Description.js
@@ -4,14 +4,12 @@ import { Link } from 'ot-ui';
 
 const Description = ({ symbol }) => (
   <React.Fragment>
-    Summary of tractability assessment for <strong>{symbol}</strong> for small
-    molecule, antibody, and other modalities. For more information on the
-    tractability assessment and descriptions of each bucket, please read{' '}
+    Target druggability assessment for <strong>{symbol}</strong>. Source:{' '}
     <Link
       external
       to="https://docs.targetvalidation.org/getting-started/target-tractability"
     >
-      the tractability section of our documentation
+      Open Targets
     </Link>
     .
   </React.Fragment>
