1e06bdb98103d5e8892addd6400252ec86b947e4
Javier Ferrer <jferrer@ebi.ac.uk>
2020-06-11 17:21:58 +0200

    known drugs widget description (opentargets/platform#1094)

--- src/public/target/sections/KnownDrugs/Description.js
+++ src/public/target/sections/KnownDrugs/Description.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'ot-ui';
 
 const Description = ({ name }) => (
   <React.Fragment>
-    Drugs in clinical trials or approved for <strong>{name}</strong>.
+    Clinical precedence for drugs with investigational or approved indications
+    targeting <strong>{name}</strong> according to their curated mechanism of
+    action. Source:{' '}
+    <Link external to="https://www.ebi.ac.uk/chembl/">
+      ChEMBL
+    </Link>
+    .
   </React.Fragment>
 );
 
