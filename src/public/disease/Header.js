cd229eb3e0382d1faa17b57e2ed07dc9ded80baa
Javier Ferrer <jferrer@ebi.ac.uk>
2020-06-15 11:30:41 +0200

    disease profile page title capitalization

--- src/public/disease/Header.js
+++ src/public/disease/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import { label } from '../../utils/global';
 import Header from '../common/Header';
 import DiseaseIcon from '../../icons/DiseaseIcon';
 import EFO from './externalLinks/EFO';
 
 const DiseaseHeader = ({ efoId, name }) => (
   <Header
-    title={name}
+    title={label(name)}
     subtitle={null}
     Icon={DiseaseIcon}
     externalLinks={
