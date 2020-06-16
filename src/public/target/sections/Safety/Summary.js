0cc28017c75f75b58cbca41be0f453ec22c8705a
Luca Fumis <lucaf@ebi.ac.uk>
2020-06-16 17:15:05 +0100

    Update target safety summary info; issue opentargets/platform#1112

--- src/public/target/sections/Safety/Summary.js
+++ src/public/target/sections/Safety/Summary.js
@@ -1,3 +1,22 @@
-const Summary = () => 'available';
+const Summary = ({ data }) => {
+  const safetyDataTypes = [
+    {
+      id: 'adverseEffects',
+      label: 'Known effects',
+    },
+    {
+      id: 'safetyRiskInfo',
+      label: 'Risk info',
+    },
+    {
+      id: 'experimentalToxicity',
+      label: 'Non-clinical toxicity',
+    },
+  ];
+  return safetyDataTypes
+    .filter(d => data[d.id].length > 0)
+    .map(d => d.label)
+    .join(' • ');
+};
 
 export default Summary;
