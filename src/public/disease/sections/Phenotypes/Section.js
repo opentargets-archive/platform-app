bb6c80cc5c0e6154712a16d0c7c9ef7fed58346a
Javier Ferrer <jferrer@ebi.ac.uk>
2020-06-16 16:06:23 +0200

    update phenotypes widget (opentargets/platform#1107)

--- src/public/disease/sections/Phenotypes/Section.js
+++ src/public/disease/sections/Phenotypes/Section.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import * as d3 from 'd3';
 
 import { Link } from 'ot-ui';
 
-const Section = ({ data }) => {
-  return (
-    <ul>
-      {data
-        .sort((a, b) =>
-          d3.ascending(a.name.toLowerCase(), b.name.toLowerCase())
-        )
-        .map(d => {
-          return (
-            <li key={d.url}>
-              <Link external to={d.url}>
-                {d.name}
-              </Link>
-            </li>
-          );
-        })}
-    </ul>
-  );
-};
+import Table from '../../../common/Table/Table';
+import { PaginationActionsComplete } from '../../../common/Table/TablePaginationActions';
+
+const columns = [
+  {
+    id: 'phenotype',
+    propertyPath: 'name',
+  },
+  {
+    id: 'identifier',
+    propertyPath: 'disease',
+    exportValue: d => d.url,
+    renderCell: d => (
+      <Link external to={d.url}>
+        {d.disease}
+      </Link>
+    ),
+  },
+];
+
+const Section = ({ data }) => (
+  <Table
+    columns={columns}
+    dataDownloader
+    dataDownloaderFileStem="phenotypes"
+    pagination={PaginationActionsComplete}
+    rows={data}
+    showGlobalFilter
+  />
+);
 
 export default Section;
