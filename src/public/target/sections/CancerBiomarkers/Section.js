6468dd9cb8ce6ad0a3dda9b39d1ef02b239e5813
Luca Fumis <lucaf@ebi.ac.uk>
2020-07-01 12:47:10 +0100

    Update cancer biomarkers to basic new table component; issue opentargets/platform#1122

--- src/public/target/sections/CancerBiomarkers/Section.js
+++ src/public/target/sections/CancerBiomarkers/Section.js
@@ -1,9 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import FilterTable from './custom/FilterTable';
+import _ from 'lodash';
 
-const Section = ({ ensgId, symbol, data }) => (
-  <FilterTable symbol={symbol} ensgId={ensgId} />
-);
+import { Link } from 'ot-ui';
+
+import Table from '../../../common/Table/Table';
+import { PaginationActionsComplete } from '../../../common/Table/TablePaginationActions';
+import { label } from '../../../../utils/global';
+import { naLabel } from '../../../../constants';
+
+import { loader } from 'graphql.macro';
+import { useQuery } from '@apollo/client';
+import useBatchDownloader from '../../../../hooks/useBatchDownloader';
+const BIOMARKERS_QUERY = loader('./sectionQuery.gql');
+
+const columns = [
+  {
+    id: 'id',
+    label: 'Biomarker',
+    width: '16%',
+  },
+  {
+    id: 'diseases',
+    label: 'Disease',
+    renderCell: row =>
+      row.disease ? (
+        <Link
+          external
+          to={`https://www.targetvalidation.org/disease/${row.disease.id}`}
+        >
+          {label(row.disease.name)}
+        </Link>
+      ) : (
+        <>{naLabel}</>
+      ),
+    width: '16%',
+  },
+  {
+    id: 'drugName',
+    label: 'Drug',
+  },
+
+  {
+    id: 'associationType',
+    label: 'Association',
+    renderCell: row => _.capitalize(row.associationType.replace(/_/g, ' ')),
+    width: '13%',
+  },
+  {
+    id: 'evidenceLevel',
+    label: 'Evidence',
+    width: '13%',
+  },
+  {
+    id: 'sources',
+    label: 'Sources',
+    renderCell: row => {
+      return (
+        <>
+          {row.sources.map((source, i) => (
+            <Link key={i} external to={source.link}>
+              {source.name}
+            </Link>
+          ))}
+        </>
+      );
+    },
+    width: '13%',
+  },
+
+  // {
+  //   id: 'references',
+  //   label: 'References',
+  //   filterValue: row =>
+  //     row.references.map(reference => reference.source).join(' '),
+  //   exportValue: row =>
+  //     row.references.map(reference => reference.source).join(),
+  // },
+];
+
+const Section = ({ ensgId, symbol, data }) => {
+  const [rows, setRows] = useState([]);
+
+  const getAllBiomarkers = useBatchDownloader(
+    BIOMARKERS_QUERY,
+    {
+      ensgId: ensgId,
+    },
+    'data.target.cancerBiomarkers'
+  );
+
+  useEffect(() => {
+    getAllBiomarkers().then(d => {
+      setRows(d);
+    });
+  }, []);
+
+  return (
+    <Table
+      showGlobalFilter
+      columns={columns}
+      rows={rows}
+      pagination={PaginationActionsComplete}
+      dataDownloader
+      dataDownloaderFileStem={`${symbol}-cancer-biomarkers`}
+    />
+  );
+};
 
 export default Section;
