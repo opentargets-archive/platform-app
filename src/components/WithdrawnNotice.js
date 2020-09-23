a6388b7bbffc1fcda4a67c2b0bdc4a35254a31eb
Javier Ferrer <javier.f.g@um.es>
2020-09-23 11:27:56 +0100

    fix order and sizing

--- src/components/WithdrawnNotice.js
+++ src/components/WithdrawnNotice.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { makeStyles, Tooltip, Typography } from '@material-ui/core';
+import { Box, makeStyles, Tooltip, Typography } from '@material-ui/core';
 import WarningIcon from '@material-ui/icons/Warning';
 
 const useStyles = makeStyles(theme => ({
+  container: {
+    marginBottom: '.5rem',
+  },
   text: {
     color: 'white',
   },
-  tooltip: {
-    backgroundColor: theme.palette.error.main,
-  },
   popper: {
     opacity: 1,
+  },
+  tooltip: {
+    backgroundColor: theme.palette.error.main,
   },
   warningIcon: {
     position: 'relative',
@@ -24,37 +27,39 @@ function WithdrawnNotice({ withdrawnNotice }) {
   if (!withdrawnNotice) return null;
 
   return (
-    <Typography variant="subtitle2" color="secondary">
-      <Tooltip
-        classes={{ tooltip: classes.tooltip, popper: classes.popper }}
-        title={
-          <Typography className={classes.text}>
-            <strong>Class: </strong>
-            {withdrawnNotice.classes
-              ? withdrawnNotice.classes.join(', ')
-              : 'Unknown'}
-            <br />
-            <strong>Reason: </strong>
-            {withdrawnNotice.reasons
-              ? withdrawnNotice.reasons.join(', ')
-              : 'Unknown'}
-            <br />
-            <strong>Year first withdrawn: </strong>
-            {withdrawnNotice.year}
-            <br />
-            <strong>Withdrawn in: </strong>
-            {withdrawnNotice.countries.length > 0
-              ? withdrawnNotice.countries.join(', ')
-              : null}
-            <br />
-          </Typography>
-        }
-        placement="right"
-      >
-        <WarningIcon className={classes.warningIcon} />
-      </Tooltip>{' '}
-      Withdrawn Drug
-    </Typography>
+    <Box className={classes.container}>
+      <Typography variant="subtitle2" color="secondary">
+        <Tooltip
+          classes={{ tooltip: classes.tooltip, popper: classes.popper }}
+          title={
+            <Typography className={classes.text}>
+              <strong>Class: </strong>
+              {withdrawnNotice.classes
+                ? withdrawnNotice.classes.join(', ')
+                : 'Unknown'}
+              <br />
+              <strong>Reason: </strong>
+              {withdrawnNotice.reasons
+                ? withdrawnNotice.reasons.join(', ')
+                : 'Unknown'}
+              <br />
+              <strong>Year first withdrawn: </strong>
+              {withdrawnNotice.year}
+              <br />
+              <strong>Withdrawn in: </strong>
+              {withdrawnNotice.countries.length > 0
+                ? withdrawnNotice.countries.join(', ')
+                : null}
+              <br />
+            </Typography>
+          }
+          placement="right"
+        >
+          <WarningIcon className={classes.warningIcon} />
+        </Tooltip>{' '}
+        Withdrawn Drug
+      </Typography>
+    </Box>
   );
 }
 
