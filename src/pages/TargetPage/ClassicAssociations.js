3be93e80564dc443dcd140bba204f593aafe8252
Alfredo Miranda <alfredo@miranda.io>
2020-09-24 11:50:38 +0100

    Decouple total count query

--- src/pages/TargetPage/ClassicAssociations.js
+++ src/pages/TargetPage/ClassicAssociations.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import Typography from '@material-ui/core/Typography';
-
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import { Grid, Card, CardContent, Typography } from '@material-ui/core';
+import { client3 } from '../../client';
 import ClassicAssociationsTable from './ClassicAssociationsTable';
 
+const ASSOCIATIONS_COUNT_QUERY = gql`
+  query TargetAssociationsQuery($ensemblId: String!) {
+    target(ensemblId: $ensemblId) {
+      associatedDiseases {
+        count
+      }
+    }
+  }
+`;
+
 function ClassicAssociations({ ensgId, symbol }) {
+  const { data } = useQuery(ASSOCIATIONS_COUNT_QUERY, {
+    variables: {
+      ensemblId: ensgId,
+    },
+    client: client3,
+  });
+
   return (
     <Grid style={{ marginTop: '8px' }} container spacing={2}>
-      {/* <Grid item xs={12}>
+      <Grid item xs={12}>
         <Typography variant="h6">
-          <strong>XYZ diseases</strong> associated with{' '}
-          <strong>{symbol}</strong>
+          {data ? (
+            <strong>{data.target.associatedDiseases.count}</strong>
+          ) : null}
+          <strong> diseases</strong> associated with <strong>{symbol}</strong>
         </Typography>
       </Grid>
       <Grid item xs={12} md={9}>
         <Card elevation={0} style={{ overflow: 'visible' }}>
-          <CardContent> */}
-      <ClassicAssociationsTable ensgId={ensgId} symbol={symbol} />
-      {/* </CardContent>
+          <CardContent>
+            <ClassicAssociationsTable ensgId={ensgId} symbol={symbol} />
+          </CardContent>
         </Card>
-      </Grid> */}
+      </Grid>
     </Grid>
   );
 }
