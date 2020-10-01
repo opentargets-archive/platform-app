0a4f795f419c1cf236d8662bbff033e7ed45bf11
Javier Ferrer <javier.f.g@um.es>
2020-10-01 16:50:54 +0100

    update counts on facet change

--- src/components/Facets/Facets.js
+++ src/components/Facets/Facets.js
@@ -1,31 +1,30 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Skeleton } from '@material-ui/lab';
-import { useQuery } from '@apollo/client';
 
-import { client3 } from '../../client';
-import { dataAccessor, getQuery } from './queries';
 import Facet from './Facet';
 import {
   getFilters,
-  getIdFromEntity,
   hassAllChildrenChecked,
+  prepareFacetData,
+  updateFacetCounts,
   setAllChildren,
   traverse,
 } from './utils';
 
-function Facets({ entity, id, onChange }) {
-  const client = client3;
-  const query = getQuery(entity);
-  const variables = { [getIdFromEntity(entity)]: id };
-  const { loading, error, data } = useQuery(query, { variables, client });
-  const facetsData = useMemo(() => dataAccessor(data, entity), [data, entity]);
+function Facets({ loading, data, onChange }) {
   const [facets, setFacets] = useState([]);
 
   useEffect(
     () => {
-      setFacets(facetsData);
+      if (!data) return;
+
+      if (!facets.length) {
+        setFacets(prepareFacetData(data));
+      } else {
+        setFacets(facets => updateFacetCounts(facets, data));
+      }
     },
-    [facetsData]
+    [data, facets.length]
   );
 
   const handleFilterChange = changePath => {
@@ -38,21 +37,17 @@ function Facets({ entity, id, onChange }) {
     if (!newValue) parent.checked = false;
     if (hassAllChildrenChecked(parent)) parent.checked = true;
     setAllChildren(node, newValue);
-    setFacets(newFacets);
 
     const filters = getFilters(newFacets);
 
     onChange(filters);
-    console.log('filters', filters);
   };
-
-  if (error) return null;
 
   return (
     <>
       <h4>Filter by</h4>
 
-      {loading ? (
+      {loading && !facets.length ? (
         <Skeleton variant="rect" height={48} />
       ) : (
         facets.map(facet => (
