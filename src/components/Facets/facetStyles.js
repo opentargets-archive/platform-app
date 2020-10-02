4ffa08a0ad9d4ea70886713a31e0e722c12a7a1f
Javier Ferrer <javier.f.g@um.es>
2020-10-02 11:48:16 +0100

    facet label ellipsis and tooltip

--- src/components/Facets/facetStyles.js
+++ src/components/Facets/facetStyles.js
@@ -1,18 +1,46 @@
 const { makeStyles } = require('@material-ui/core');
 
 const facetStyles = makeStyles(theme => ({
+  accordionSummaryContent: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: 0,
+  },
+
+  clearButtonRoot: {
+    height: 'unset',
+  },
+
   facetRoot: {
     width: '100%',
   },
-  treeItemLabelRoot: {
-    display: 'flex',
+  facetSummary: {
     alignItems: 'center',
+    display: 'flex',
     justifyContent: 'space-between',
-    padding: '0 .5rem',
   },
-  treeItemLabelText: {
-    fontSize: '.9rem',
-    flexGrow: 1,
+
+  countLabel: {
+    fontSize: '.7rem',
+    fontWeight: 'bold',
+    color: theme.palette.grey[500],
+  },
+
+  formControlLabelRoot: {
+    width: '88%',
+  },
+  FormControlLabelLabel: {
+    fontSize: '.9 rem',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  },
+
+  treeItemLabel: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '93%',
   },
 }));
 
