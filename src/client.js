9a43c7544603b276a29c3b745e7721514572537b
Alfredo Miranda <alfredo@miranda.io>
2020-10-06 09:53:01 +0100

    Test graphql endpoint

--- src/client.js
+++ src/client.js
@@ -12,7 +12,8 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://platform-api-alpha.opentargets.io/api/v4/graphql',
+    // uri: 'https://platform-api-alpha.opentargets.io/api/v4/graphql',
+    uri: 'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
   }),
   cache: new InMemoryCache({ fragmentMatcher }),
 });
