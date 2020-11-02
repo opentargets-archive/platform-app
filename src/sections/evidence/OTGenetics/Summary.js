a354cf72da5637317aee8834c5a6b994731b63db
Javier Ferrer <javier.f.g@um.es>
2020-11-02 14:07:45 +0000

    evidence open targets genetics section

--- src/sections/evidence/OTGenetics/Summary.js
+++ src/sections/evidence/OTGenetics/Summary.js
@@ -1,4 +1,39 @@
-const Summary = ({ variantCount }) =>
-  `${variantCount} variant${variantCount === 1 ? '' : 's'}`;
+import React from 'react';
+import { gql } from '@apollo/client';
+
+import SummaryItem from '../../../components/Summary/SummaryItem';
+import usePlatformApi from '../../../hooks/usePlatformApi';
+
+const OPEN_TARGETS_GENETICS_SUMMARY_FRAGMENT = gql`
+  fragment OpenTargetsGeneticsSummaryFragment on Disease {
+    evidences(
+      ensemblIds: [$ensgId]
+      enableIndirect: true
+      datasourceIds: ["ot_genetics_portal"]
+    ) {
+      count
+    }
+  }
+`;
+
+function Summary({ definition }) {
+  const request = usePlatformApi(OPEN_TARGETS_GENETICS_SUMMARY_FRAGMENT);
+
+  return (
+    <SummaryItem
+      definition={definition}
+      request={request}
+      renderSummary={data =>
+        `${data.evidences.count} entr${
+          data.evidences.count === 1 ? 'y' : 'ies'
+        }`
+      }
+    />
+  );
+}
+
+Summary.fragments = {
+  OpenTargetsGeneticsSummaryFragment: OPEN_TARGETS_GENETICS_SUMMARY_FRAGMENT,
+};
 
 export default Summary;
--- src/sections/evidence/OTGenetics/index.js
+++ src/sections/evidence/OTGenetics/index.js
@@ -1,13 +1,9 @@
-import { loader } from 'graphql.macro';
+export const definition = {
+  id: 'otGenetics',
+  name: 'Open Targets Genetics',
+  shortName: 'OG',
+  hasData: data => data.evidences.count > 0,
+};
 
-export const id = 'otGenetics';
-export const name = 'Open Targets Genetics Portal';
-
-export const hasSummaryData = ({ variantCount }) => variantCount > 0;
-
-export const summaryQuery = loader('./summaryQuery.gql');
-export const sectionQuery = loader('./sectionQuery.gql');
-
-export { default as DescriptionComponent } from './Description';
-export { default as SummaryComponent } from './Summary';
-export { default as SectionComponent } from './Section';
+export { default as Summary } from './Summary';
+export { default as Body } from './Body';
