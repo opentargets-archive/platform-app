13823f93082c7dcf03c379375c649d1da191ec82
Javier Ferrer <javier.f.g@um.es>
2020-11-02 10:48:56 +0000

    parameterized client

--- src/contexts/PlatformApiProvider.js
+++ src/contexts/PlatformApiProvider.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 
+import productionClient from '../client';
+
 const PlatformApiContext = React.createContext();
 
-function PlatformApiProvider({ entity, query, variables, children }) {
-  const request = useQuery(query, { variables });
+function PlatformApiProvider({
+  entity,
+  query,
+  client = productionClient,
+  variables,
+  children,
+}) {
+  const request = useQuery(query, { client, variables });
 
   return (
     <PlatformApiContext.Provider value={{ ...request, entity }}>
