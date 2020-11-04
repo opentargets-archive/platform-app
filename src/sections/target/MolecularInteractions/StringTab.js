11b3cdf8bf806222a303c482f648558e8a364419
Luca Fumis <lucaf@ebi.ac.uk>
2020-11-04 18:25:01 +0000

    Update String heatmap table

--- src/sections/target/MolecularInteractions/StringTab.js
+++ src/sections/target/MolecularInteractions/StringTab.js
@@ -2,18 +2,14 @@ import React, { Fragment, useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { loader } from 'graphql.macro';
 import client from '../../../client';
-import { withTheme } from '@material-ui/core';
+import { withTheme, makeStyles, Link } from '@material-ui/core';
 
 import DataTable from '../../../components/Table/DataTable';
-import { MethodIconText, MethodIconArrow } from './custom/MethodIcons';
+import Legend from '../../../components/Legend';
+import { colorRange } from '../../../constants';
 
 import Grid from '@material-ui/core/Grid';
 
-import {
-  Heatmap,
-  HeatmapTable,
-  HeatmapCell,
-} from '../../../components/Heatmap';
 import * as d3 from 'd3';
 
 const INTERACTIONS_QUERY = loader('./sectionStringQuery.gql');
@@ -29,8 +25,81 @@ const getData = (ensgId, sourceDatabase, index, size) => {
   });
 };
 
-const columns = {
-  interactions: [
+const useStyles = makeStyles({
+  root: {
+    overflow: 'visible',
+    padding: '2rem 3rem 0 0',
+  },
+  table: {
+    tableLayout: 'fixed',
+  },
+  sortLabel: {
+    top: '8px',
+  },
+  innerLabel: {
+    position: 'absolute',
+    display: 'inline-block',
+    transformOrigin: '-20px 20px',
+    bottom: 0,
+    transform: 'rotate(315deg)',
+    marginBottom: '5px',
+  },
+  nameHeaderCell: {
+    width: '15%',
+    borderBottom: 0,
+    height: '140px',
+    verticalAlign: 'bottom',
+    textAlign: 'end',
+    paddingBottom: '.4rem',
+  },
+  headerCell: {
+    position: 'relative',
+    borderBottom: 0,
+    height: '140px',
+    whiteSpace: 'nowrap',
+    textAlign: 'center',
+    verticalAlign: 'bottom',
+  },
+  overallCell: {
+    border: 0,
+    textAlign: 'center',
+    paddingTop: '1px',
+    paddingBottom: '1px',
+    paddingLeft: '1px',
+    paddingRight: '10px',
+  },
+  cell: {
+    border: 0,
+    height: '20px',
+    textAlign: 'center',
+    padding: '1px 1px',
+    '&:last-child': {
+      paddingRight: 0,
+    },
+  },
+  colorSpan: {
+    display: 'block',
+    height: '20px',
+    border: '1px solid #eeefef',
+  },
+  nameCell: {
+    border: 0,
+    // width: '20%',
+    padding: '0 0.5rem 0 0',
+    '&:first-child': {
+      paddingLeft: 0,
+    },
+  },
+  nameContainer: {
+    display: 'block',
+    textAlign: 'end',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+  },
+});
+
+function getColumns(classes) {
+  return [
     {
       id: 'partner',
       label: (
@@ -40,12 +109,12 @@ const columns = {
           <Typography variant="caption">Species (if not human)</Typography> */}
         </>
       ),
-      verticalHeader: true,
-      align: 'center',
-      // firstInHeaderGroup: true,
-      // lastInHeaderGroup: true,
+      classes: {
+        headerCell: classes.nameHeaderCell,
+        cell: classes.nameCell,
+      },
       renderCell: row => (
-        <>
+        <span className={classes.nameContainer}>
           {row.targetB ? row.targetB.approvedSymbol : row.intB}
           {/* {row.organism.mnemonic.toLowerCase() !== 'human' ? (
             <>
@@ -55,7 +124,7 @@ const columns = {
               </Typography>
             </>
           ) : null} */}
-        </>
+        </span>
       ),
     },
     {
@@ -67,103 +136,162 @@ const columns = {
           interaction score
         </>
       ),
-      verticalHeader: true,
-      align: 'center',
-      // renderCell: row => row.scoring,
-      renderCell: row => (
-        <HeatmapCell value={row.scoring} colorScale={colorScale} />
-      ),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row => getHeatmapCell(row.scoring, classes),
     },
     {
       id: 'neighbourhood',
       label: 'Neighbourhood',
-      verticalHeader: true,
-      align: 'center',
-      // firstInHeaderGroup: false,
-      // lastInHeaderGroup: false,
-      renderCell: row => getScoreForColumn(row.evidences, 'gene neighbourhood'),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row =>
+        getHeatmapCell(
+          getScoreForColumn(row.evidences, 'gene neighbourhood'),
+          classes
+        ),
     },
     {
       id: 'geneFusion',
       label: 'Gene fusion',
-      verticalHeader: true,
-      align: 'center',
-      renderCell: row => getScoreForColumn(row.evidences, 'domain fusion'),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row =>
+        getHeatmapCell(
+          getScoreForColumn(row.evidences, 'domain fusion'),
+          classes
+        ),
     },
     {
       id: 'occurance',
       label: 'Co-occurrance',
-      verticalHeader: true,
-      align: 'center',
-      renderCell: row => getScoreForColumn(row.evidences, 'cooccurence'),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row =>
+        getHeatmapCell(
+          getScoreForColumn(row.evidences, 'cooccurence'),
+          classes
+        ),
     },
     {
       id: 'expression',
       label: 'Co-expression',
-      verticalHeader: true,
-      align: 'center',
-      renderCell: row => getScoreForColumn(row.evidences, 'coexpression'),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row =>
+        getHeatmapCell(
+          getScoreForColumn(row.evidences, 'coexpression'),
+          classes
+        ),
     },
     {
       id: 'experiments',
       label: 'Experiments',
-      verticalHeader: true,
-      align: 'center',
-      renderCell: row => getScoreForColumn(row.evidences, 'experimental'),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row =>
+        getHeatmapCell(
+          getScoreForColumn(row.evidences, 'experimental'),
+          classes
+        ),
     },
     {
       id: 'databases',
       label: 'Databases',
-      verticalHeader: true,
-      align: 'center',
-      renderCell: row => getScoreForColumn(row.evidences, 'database'),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row =>
+        getHeatmapCell(getScoreForColumn(row.evidences, 'database'), classes),
     },
     {
       id: 'textMining',
       label: 'Text mining',
-      verticalHeader: true,
-      align: 'center',
-      renderCell: row => getScoreForColumn(row.evidences, 'text mining'),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row =>
+        getHeatmapCell(
+          getScoreForColumn(row.evidences, 'text mining'),
+          classes
+        ),
     },
     {
       id: 'homology',
       label: 'Homology',
-      verticalHeader: true,
-      align: 'center',
-      renderCell: row => getScoreForColumn(row.evidences, 'by homology'),
+      classes: {
+        headerCell: classes.headerCell,
+        cell: classes.cell,
+        sortLabel: classes.sortLabel,
+        innerLabel: classes.innerLabel,
+      },
+      renderCell: row =>
+        getHeatmapCell(
+          getScoreForColumn(row.evidences, 'by homology'),
+          classes
+        ),
     },
-  ],
-};
+  ];
+}
 
 const getScoreForColumn = (evidences, id) => {
-  const score = evidences
+  return evidences
     .filter(e => e.interactionDetectionMethodShortName === id)
-    .map(e => e.evidenceScore)[0];
-  return <HeatmapCell value={score} colorScale={colorScale} />;
+    .map(e => e.evidenceScore)[0]; // TODO: the [0] is to catch a data error: remove when fixed.
+};
+
+const getHeatmapCell = (score, classes) => {
+  return (
+    <span
+      className={classes.colorSpan}
+      title={score || 'No data'}
+      style={{ backgroundColor: color(score) }}
+    />
+  );
 };
 
 const id = 'string';
 const index = 0;
 const size = 5000;
-
-// TODO: re-enable for heatmap
-// const scaleAssociation = d3
-//   .scaleLinear()
-//   .domain([0, 1])
-//   .range([
-//     '#f00',
-//     '#fff'
-//   ])
-//   .unknown('#fff');
-
-const colorScale = d3
-  .scaleLinear()
-  .domain([0, Math.PI ** 2 / 6])
-  // .range(['#fff', theme.palette.primary.main]);
-  .range(['#fff', '#0e5299']);
+const color = d3
+  .scaleQuantize()
+  .domain([0, 1])
+  .range(colorRange);
 
 function StringTab({ ensgId, symbol }) {
   const [data, setData] = useState([]);
+  const classes = useStyles();
+  const columns = getColumns(classes);
 
   // load tab data when new tab selected (also on first load)
   useEffect(
@@ -181,21 +309,16 @@ function StringTab({ ensgId, symbol }) {
     <Grid container spacing={4}>
       <Grid item xs={12}>
         {/* table 1: this is the only table and will need to be a HEATMAP */}
-        {/* <DataTable
+        <DataTable
           showGlobalFilter
-          columns={columns.interactions}
+          columns={columns}
           rows={data}
           dataDownloader
           dataDownloaderFileStem={`${symbol}-molecular-interactions-string`}
-          hover
-        /> */}
-        <HeatmapTable
-          loading={false}
-          error={false}
-          columns={columns.interactions}
-          headerGroups={[]}
-          data={data}
+          fixed
+          classes={{ root: classes.root, table: classes.table }}
         />
+        <Legend />
       </Grid>
     </Grid>
   );
