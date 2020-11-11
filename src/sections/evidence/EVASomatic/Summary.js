fdfc76c87f550ff23daebdd90341b8eaa1c21d0d
Alfredo Miranda <alfredo@miranda.io>
2020-11-11 17:51:04 +0000

    Implement EVA somatic evidence table

--- src/sections/evidence/EVASomatic/Summary.js
+++ src/sections/evidence/EVASomatic/Summary.js
@@ -1,4 +1,36 @@
-const Summary = ({ variantCount }) =>
-  `${variantCount} variant${variantCount === 1 ? '' : 's'}`;
+import React from 'react';
+import { gql } from '@apollo/client';
+import usePlatformApi from '../../../hooks/usePlatformApi';
+import SummaryItem from '../../../components/Summary/SummaryItem';
+
+const EVA_SOMATIC_SUMMARY = gql`
+  fragment evaSomaticSummary on Disease {
+    evaSomaticSummary: evidences(
+      ensemblIds: [$ensgId]
+      enableIndirect: true
+      datasourceIds: ["eva_somatic"]
+    ) {
+      count
+    }
+  }
+`;
+
+function Summary({ definition }) {
+  const request = usePlatformApi(EVA_SOMATIC_SUMMARY);
+  return (
+    <SummaryItem
+      definition={definition}
+      request={request}
+      renderSummary={({ evaSomaticSummary }) => {
+        const { count } = evaSomaticSummary;
+        return `${count} variant${count > 1 ? 's' : ''}`;
+      }}
+    />
+  );
+}
+
+Summary.fragments = {
+  evaSomaticSummary: EVA_SOMATIC_SUMMARY,
+};
 
 export default Summary;
