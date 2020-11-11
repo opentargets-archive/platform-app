11ff18abc38dc00182a5bc3cffc92b916ffbc8f3
Alfredo Miranda <alfredo@miranda.io>
2020-11-11 13:25:22 +0000

    Implement EVA evidence table

--- src/sections/evidence/EVA/Summary.js
+++ src/sections/evidence/EVA/Summary.js
@@ -1,4 +1,36 @@
-const Summary = ({ variantCount }) =>
-  `${variantCount} variant${variantCount === 1 ? '' : 's'}`;
+import React from 'react';
+import { gql } from '@apollo/client';
+import usePlatformApi from '../../../hooks/usePlatformApi';
+import SummaryItem from '../../../components/Summary/SummaryItem';
+
+const EVA_SUMMARY = gql`
+  fragment evaSummary on Disease {
+    evaSummary: evidences(
+      ensemblIds: [$ensgId]
+      enableIndirect: true
+      datasourceIds: ["eva"]
+    ) {
+      count
+    }
+  }
+`;
+
+function Summary({ definition }) {
+  const request = usePlatformApi(EVA_SUMMARY);
+  return (
+    <SummaryItem
+      definition={definition}
+      request={request}
+      renderSummary={({ evaSummary }) => {
+        const { count } = evaSummary;
+        return `${count} variant${count > 1 ? 's' : ''}`;
+      }}
+    />
+  );
+}
+
+Summary.fragments = {
+  evaSummary: EVA_SUMMARY,
+};
 
 export default Summary;
