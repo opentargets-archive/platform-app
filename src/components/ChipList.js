0a5e9bfb4324bcc11be3d07f3c49c38b58e5b2ac
Javier Ferrer <javier.f.g@um.es>
2020-11-18 10:42:14 +0000

    extract chip container component

--- src/components/ChipList.js
+++ src/components/ChipList.js
@@ -3,8 +3,7 @@ import { Box, Chip, makeStyles, Tooltip } from '@material-ui/core';
 
 import { naLabel } from '../constants';
 
-const useStyles = makeStyles(theme => ({
-  chip: { margin: '3px 5px 3px 0' },
+const useContainerStyles = makeStyles(theme => ({
   tooltip: {
     backgroundColor: theme.palette.background.paper,
     border: `1px solid ${theme.palette.grey[300]}`,
@@ -12,41 +11,47 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const useChipStyles = makeStyles({
+  chip: { margin: '3px 5px 3px 0' },
+});
+
+function ChipContainer({ item, children }) {
+  const classes = useContainerStyles();
+
+  return item.tooltip ? (
+    <Tooltip
+      placement="top"
+      interactive
+      classes={{ tooltip: classes.tooltip }}
+      title={item.tooltip}
+    >
+      {children}
+    </Tooltip>
+  ) : (
+    children
+  );
+}
+
 function ChipList({ items }) {
-  const classes = useStyles();
+  const classes = useChipStyles();
 
   if (!items || items.length === 0) return naLabel;
 
-  return items.map((item, index) => {
-    const ContainerComponent = item.tooltip
-      ? ({ children }) => (
-          <Tooltip
-            placement="top"
-            interactive
-            classes={{ tooltip: classes.tooltip }}
-            title={item.tooltip}
-          >
-            {children}
-          </Tooltip>
-        )
-      : ({ children }) => <>{children}</>;
-
-    return (
-      <ContainerComponent key={index}>
-        <Chip
-          component={!!item.url ? 'a' : Box}
-          href={item.url}
-          className={classes.chip}
-          clickable={!!item.url}
-          target="_blank"
-          noopener="true"
-          noreferrer="true"
-          color="primary"
-          label={item.label}
-        />
-      </ContainerComponent>
-    );
-  });
+  return items.map((item, index) => (
+    <ChipContainer key={index} item={item}>
+      <Chip
+        component={!!item.url ? 'a' : Box}
+        href={item.url}
+        className={classes.chip}
+        clickable={!!item.url}
+        target="_blank"
+        noopener="true"
+        noreferrer="true"
+        color="primary"
+        label={item.label}
+      />
+    </ChipContainer>
+  ));
 }
 
 export default ChipList;
