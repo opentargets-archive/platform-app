be6d959612d194f0421bc37f304a2b4422948120
Alfredo Miranda <alfredo@miranda.io>
2020-11-23 15:47:34 +0000

    Compute dag layout

--- src/pages/TargetPage/ClassicAssociationsDAG.js
+++ src/pages/TargetPage/ClassicAssociationsDAG.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import * as d3Base from 'd3';
-// import * as d3Dag from 'd3-dag';
+import * as d3Base from 'd3';
+import * as d3Dag from 'd3-dag';
 
-// const d3 = { ...d3Base, d3Dag };
+const d3 = Object.assign({}, d3Base, d3Dag);
 
 function getParentIds(diseaseId, idToDisease, assocSet) {
   const disease = idToDisease[diseaseId];
@@ -12,7 +12,6 @@ function getParentIds(diseaseId, idToDisease, assocSet) {
   while (queue.length > 0) {
     const parentId = queue.shift();
     const parentNode = idToDisease[parentId];
-
     if (assocSet[parentId] || parentNode.parentIds.length === 0) {
       parentIds.add(parentId);
     } else {
@@ -27,6 +26,39 @@ function getParentIds(diseaseId, idToDisease, assocSet) {
   return Array.from(parentIds);
 }
 
+function buildDagData(idToDisease, associations, assocSet) {
+  const dag = [];
+  const tas = new Set();
+  associations.forEach(association => {
+    const parentIds = getParentIds(
+      association.disease.id,
+      idToDisease,
+      assocSet
+    );
+
+    parentIds.forEach(parentId => {
+      const node = idToDisease[parentId];
+      if (node.parentIds.length === 0 && !assocSet[parentId]) {
+        tas.add(parentId);
+      }
+    });
+
+    dag.push({
+      id: association.disease.id,
+      parentIds,
+    });
+  });
+
+  tas.forEach(id => {
+    dag.push({
+      id,
+      parentIds: [],
+    });
+  });
+
+  return dag;
+}
+
 function ClassicAssociationsDAG({ ensemblId, symbol, efo, associations }) {
   const idToDisease = efo.reduce((acc, disease) => {
     acc[disease.id] = disease;
@@ -38,18 +70,20 @@ function ClassicAssociationsDAG({ ensemblId, symbol, efo, associations }) {
     return acc;
   }, {});
 
-  const dag = [];
-  associations.forEach(association => {
-    const parentIds = getParentIds(
-      association.disease.id,
-      idToDisease,
-      assocSet
-    );
-    dag.push({
-      id: association.disease.id,
-      parentIds,
-    });
-  });
+  const dagData = buildDagData(idToDisease, associations, assocSet);
+  const dag = d3.dagStratify()(dagData);
+
+  const layering = d3.layeringLongestPath();
+  const decross = d3.decrossTwoLayer();
+  const coord = d3.coordCenter();
+  const layout = d3
+    .sugiyama()
+    .layering(layering)
+    .decross(decross)
+    .coord(coord)
+    .nodeSize(() => [5, 5]);
+
+  layout(dag);
 
   return <div>associations dag</div>;
 }
