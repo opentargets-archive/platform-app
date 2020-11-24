7272f71339e2e97c21655dc33bd4cb5767d249e7
Javier Ferrer <jferrer@ebi.ac.uk>
2020-11-24 11:32:17 +0000

    Evidence EuropePMC section changes (#244)
    * update when children change
    
    * show more on long lists of authors
    
    * evidence europepmc fixes

--- src/components/LongText.js
+++ src/components/LongText.js
@@ -13,7 +13,7 @@ const styles = theme => ({
   },
 });
 
-const LongText = ({ classes, lineLimit, children }) => {
+const LongText = ({ classes, lineLimit, children, variant = 'body2' }) => {
   const containerRef = useRef();
   const textRef = useRef();
   const [showMore, setShowMore] = useState(false);
@@ -41,11 +41,11 @@ const LongText = ({ classes, lineLimit, children }) => {
 
       setNumberOfLines(numberOfLines);
     },
-    [lineLimit, showMore]
+    [lineLimit, showMore, children]
   );
 
   return (
-    <Typography variant="body2">
+    <Typography variant={variant}>
       <span ref={containerRef} className={classes.textContainer}>
         <span ref={textRef}>{children}</span>
       </span>
