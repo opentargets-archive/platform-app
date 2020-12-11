ea40e72a18242e0e66bb871f4720acc314d8a2fd
Javier Ferrer <javier.f.g@um.es>
2020-12-11 10:11:49 +0000

    lazy load expression atlas component bootstrap css

--- src/sections/target/Expression/AtlasTab.js
+++ src/sections/target/Expression/AtlasTab.js
@@ -1,4 +1,5 @@
 import React, { Suspense, lazy } from 'react';
+import { Helmet } from 'react-helmet';
 
 import ErrorBoundary from '../../../components/ErrorBoundary';
 
@@ -11,6 +12,16 @@ const AtlasTab = ({ ensgId, symbol }) => {
     <ErrorBoundary
       message={`There was an error loading the Expression Atlas plugin for ${symbol}`}
     >
+      <Helmet
+        link={[
+          {
+            rel: 'stylesheet',
+            type: 'text/css',
+            href:
+              'https://www.ebi.ac.uk/gxa/resources/css/customized-bootstrap-3.3.5.css',
+          },
+        ]}
+      />
       <Suspense fallback={<div>Loading...</div>}>
         <ExpressionAtlasHeatmap
           query={{
