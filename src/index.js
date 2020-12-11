fd6662a930ef88ac4ece0d512c1e0a40a9ca9a68
Javier Ferrer <javier.f.g@um.es>
2020-12-11 12:09:22 +0000

    lazy load cttv/tntvis

--- src/index.js
+++ src/index.js
@@ -6,15 +6,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Refiner from 'refiner-js';
 
-// eslint-disable-next-line
-import tnt from 'tntvis';
-// eslint-disable-next-line
-import utils from 'tnt.utils';
-// eslint-disable-next-line
-import rest from 'tnt.rest';
-
-import tooltip from './utils/tooltip';
-
 import App from './App';
 
 // styling
@@ -25,9 +16,5 @@ import './index.scss';
 if (process.env.NODE_ENV !== 'development') {
   Refiner('setProject', 'ade7da40-a960-11ea-9bbb-37035544d167');
 }
-
-// set up tntvis with modified tooltip script (breaks otherwise)
-// window.tnt = tnt;
-window.tnt.tooltip = tooltip;
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/sections/target/Variation/Body.js
+++ src/sections/target/Variation/Body.js
@@ -3,20 +3,35 @@ import { Typography } from '@material-ui/core';
 import { withContentRect } from 'react-measure';
 import * as d3 from 'd3';
 
-import board from 'tnt.genome';
-import cttvApi from 'cttv.api';
-
 import Description from './Description';
 import Link from '../../../components/Link';
 import SectionItem from '../../../components/Section/SectionItem';
-import targetGenomeBrowser from 'cttv.genome';
 
 // TODO: Currently, when a resize occurs, we just
 // redraw the widget, meaning internal state is lost.
 // Ideally, this should use componentDidUpdate, but
 // some investigation needs to be done into tntvis.
 class Section extends React.Component {
-  state = {};
+  state = { componentLoaded: false };
+
+  async componentDidMount() {
+    await import('tntvis');
+
+    const board = await import('tnt.genome');
+    this.board = board.default;
+
+    const cttvApi = await import('cttv.api');
+    this.cttvApi = cttvApi.default;
+
+    const targetGenomeBrowser = await import('cttv.genome');
+    this.targetGenomeBrowser = targetGenomeBrowser.default;
+
+    this.setState({ componentLoaded: true });
+
+    const { id: ensgId } = this.props;
+    const { width } = this.state;
+    this._render(ensgId, width);
+  }
 
   static getDerivedStateFromProps(props) {
     const { width = 600 } = props.contentRect.bounds;
@@ -24,13 +39,15 @@ class Section extends React.Component {
   }
 
   _render = (ensgId, width) => {
+    if (!this.state.componentLoaded) return;
+
     // clear previous (if existed)
     d3.select('#otTargetGenomeBrowser')
       .selectAll('*')
       .remove();
 
     // draw
-    const browser = board
+    const browser = this.board
       .genome()
       .species('human')
       .gene(ensgId)
@@ -43,11 +60,11 @@ class Section extends React.Component {
       .protocol('https')
       .domain('rest.ensembl.org');
 
-    const api = cttvApi()
+    const api = this.cttvApi()
       .prefix('https://platform-api.opentargets.io')
       .verbose(false);
 
-    const theme = targetGenomeBrowser()
+    const theme = this.targetGenomeBrowser()
       .efo(null)
       .cttvRestApi(api);
 
@@ -56,45 +73,46 @@ class Section extends React.Component {
     this.browser = browser;
   };
 
-  componentDidMount() {
-    const { id: ensgId } = this.props;
-    const { width } = this.state;
-    this._render(ensgId, width);
-  }
   componentDidUpdate() {
     const { id: ensgId } = this.props;
     const { width } = this.state;
-    if (this.browser.width() !== width) {
+    if (this.browser?.width() !== width) {
       this._render(ensgId, width);
     }
   }
   render() {
     const { definition, id: ensgId, label: symbol, measureRef } = this.props;
+    const { componentLoaded } = this.state;
+
     return (
       <SectionItem
         definition={definition}
         request={{ data: true }}
         renderDescription={() => <Description symbol={symbol} />}
-        renderBody={() => (
-          <div id="otTargetGenomeBrowserContainer" ref={measureRef}>
-            <Typography>
-              <strong>Gene information</strong>
-            </Typography>
-            <Typography variant="body2">
-              <strong>Ensembl ID: </strong>
-              <Link
-                external
-                to={`http://www.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=${ensgId}`}
-              >
-                {ensgId}
-              </Link>
-              <br />
-              <strong>Description: </strong>
-              {symbol}
-            </Typography>
-            <div id="otTargetGenomeBrowser" />
-          </div>
-        )}
+        renderBody={() => {
+          if (!componentLoaded) return <>Loading...</>;
+
+          return (
+            <div id="otTargetGenomeBrowserContainer" ref={measureRef}>
+              <Typography>
+                <strong>Gene information</strong>
+              </Typography>
+              <Typography variant="body2">
+                <strong>Ensembl ID: </strong>
+                <Link
+                  external
+                  to={`http://www.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=${ensgId}`}
+                >
+                  {ensgId}
+                </Link>
+                <br />
+                <strong>Description: </strong>
+                {symbol}
+              </Typography>
+              <div id="otTargetGenomeBrowser" />
+            </div>
+          );
+        }}
       />
     );
   }
