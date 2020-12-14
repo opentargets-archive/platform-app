177155f01c870277e92d195c12cec7f619a3611e
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 01:47:41 +0000

    Update evidence: epmc

--- src/sections/evidence/EuropePmc/Description.js
+++ src/sections/evidence/EuropePmc/Description.js
@@ -4,6 +4,12 @@ function Description({ target, disease }) {
   return (
     <>
       Articles where {target} and {disease} are found in the same sentence.
+      Text-mining method evaluating the strength of the association between{' '}
+      <strong>{target}</strong> and <strong>{disease}</strong> when they
+      co-occur in the literature. Source:{' '}
+      <Link to="http://europepmc.org" external>
+        Europe PMC
+      </Link>
     </>
   );
 }
