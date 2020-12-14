c7e4258070bef67a40f3e55b6bd750de6a6c1b49
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 02:39:49 +0000

    Update evidence: fix typo

--- src/sections/evidence/Reactome/Description.js
+++ src/sections/evidence/Reactome/Description.js
@@ -5,7 +5,8 @@ const Description = ({ symbol, name }) => (
   <>
     Literature-curated list of pathway reactions affected by genetic alterations
     or changes in gene expression that lead to disregulation of{' '}
-    <strong>{symbol}</strong> in the context of <strong>{name}</strong>. Source:{' '}
+    <strong>{symbol}</strong> in the context of <strong>{name}</strong>.
+    Source:&nbsp;
     <Link to="https://reactome.org" external>
       Reactome
     </Link>
