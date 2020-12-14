22ae819fd891a05788bea233ee12889859fac205
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 01:42:39 +0000

    Update evidence: Reactome

--- src/sections/evidence/Reactome/Description.js
+++ src/sections/evidence/Reactome/Description.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Description = ({ symbol, name }) => (
   <>
     Associated pathways between <strong>{symbol}</strong> and{' '}
-    <strong>{name}</strong>.
+    <strong>{name}</strong>. Literature-curated list of pathway reactions
+    affected by genetic alterations or changes in gene expression that lead to
+    disregulation of <strong>{symbol}</strong> in the context of{' '}
+    <strong>{name}</strong>. Source:{' '}
+    <Link to="https://reactome.org" external>
+      Reactome
+    </Link>
   </>
 );
 
