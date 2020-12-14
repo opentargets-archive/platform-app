26863eb83ef9548e92aef93d38ea939fb50c8967
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 01:50:56 +0000

    Update evidence: epmc typo

--- src/sections/evidence/EuropePmc/Description.js
+++ src/sections/evidence/EuropePmc/Description.js
@@ -3,7 +3,6 @@ import React from 'react';
 function Description({ target, disease }) {
   return (
     <>
-      Articles where {target} and {disease} are found in the same sentence.
       Text-mining method evaluating the strength of the association between{' '}
       <strong>{target}</strong> and <strong>{disease}</strong> when they
       co-occur in the literature. Source:{' '}
