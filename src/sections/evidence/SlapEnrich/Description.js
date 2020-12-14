01ee883e21f0b26462d05bbbd8a78a8c9bc42db4
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 01:39:32 +0000

    Update evidence: SLAPenrich

--- src/sections/evidence/SlapEnrich/Description.js
+++ src/sections/evidence/SlapEnrich/Description.js
@@ -4,7 +4,12 @@ function Description({ symbol, name }) {
   return (
     <>
       Genetic associations between <strong>{symbol}</strong> and{' '}
-      <strong>{name}</strong>.
+      <strong>{name}</strong>. Pathway-level analysis tool providing enrichment
+      on genomic alterations associating <strong>{symbol}</strong> with{' '}
+      <strong>{name}</strong>. Source:{' '}
+      <Link to="https://saezlab.github.io/SLAPenrich/" external>
+        SLAPenrich
+      </Link>
     </>
   );
 }
