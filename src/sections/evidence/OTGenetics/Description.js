0e356646fb0c60212cc9a5b64dacc4f7fa10313b
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 00:49:10 +0000

    Update evidence: OT Genetics Portal

--- src/sections/evidence/OTGenetics/Description.js
+++ src/sections/evidence/OTGenetics/Description.js
@@ -3,8 +3,11 @@ import React from 'react';
 function Description({ symbol, name }) {
   return (
     <>
-      Genetic associations between <strong>{symbol}</strong> and{' '}
-      <strong>{name}</strong>.
+      Genome-wide associated loci prioritisating <strong>{symbol}</strong> as
+      likely causal gene for <strong>{name}</strong>. Source:{' '}
+      <Link to="https://genetics.opentargets.org" external>
+        Open Targets Genetics Portal
+      </Link>
     </>
   );
 }
