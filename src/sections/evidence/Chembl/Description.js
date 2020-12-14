79c96c546bf74db0fa564757f008620295b07d7b
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 01:26:11 +0000

    Update evidence: Chembl

--- src/sections/evidence/Chembl/Description.js
+++ src/sections/evidence/Chembl/Description.js
@@ -4,7 +4,12 @@ function Description({ symbol, name }) {
   return (
     <>
       Drugs in clinical trials or approved for <strong>{symbol}</strong> and{' '}
-      <strong>{name}</strong>.
+      <strong>{name}</strong>. Clinical candidates and/or approved drugs
+      pharmacologically targeting <strong>{symbol}</strong> and indicated for{' '}
+      <strong>{name}</strong>. Source:{' '}
+      <Link to="https://www.ebi.ac.uk/chembl/" external>
+        ChEMBL
+      </Link>
     </>
   );
 }
