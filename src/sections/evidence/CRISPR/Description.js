a81281e7d60213e3e5564f11d672373cc53ae9ad
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 01:37:44 +0000

    Update evidence: CRISPR / Project Score

--- src/sections/evidence/CRISPR/Description.js
+++ src/sections/evidence/CRISPR/Description.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Description = ({ symbol, name }) => (
   <>
     Associated pathways between <strong>{symbol}</strong> and{' '}
-    <strong>{name}</strong>.
+    <strong>{name}</strong>. Cancer cell line dependencies identified using
+    CRISPR-Cas9 whole genome screenings pinpointing a <strong>{symbol}</strong>{' '}
+    dependency in <strong>{name}</strong>. Source:{' '}
+    <Link to="https://score.depmap.sanger.ac.uk" external>
+      Project Score
+    </Link>
   </>
 );
 
