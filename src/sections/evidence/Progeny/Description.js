deebef85cebac3e2268ffe4aff0312742cb7e3b3
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 01:41:06 +0000

    Update evidence: PROGENy

--- src/sections/evidence/Progeny/Description.js
+++ src/sections/evidence/Progeny/Description.js
@@ -4,7 +4,12 @@ function Description({ symbol, name }) {
   return (
     <>
       Genetic associations between <strong>{symbol}</strong> and{' '}
-      <strong>{name}</strong>.
+      <strong>{name}</strong>. Pathway-level analysis of gene expression
+      perturbation experiments associating <strong>{symbol}</strong> with{' '}
+      <strong>{name}</strong>. Source:{' '}
+      <Link to="https://saezlab.github.io/progeny/" external>
+        PROGENy
+      </Link>
     </>
   );
 }
