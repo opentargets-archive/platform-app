4d29ed65b52ec87680e85a84167002d41ea7444a
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 10:06:00 +0000

    Update evidence: fix typo

--- src/sections/evidence/PheWASCatalog/Description.js
+++ src/sections/evidence/PheWASCatalog/Description.js
@@ -4,7 +4,7 @@ import { Link } from 'ot-ui';
 const Description = ({ symbol, diseaseName }) => (
   <>
     Phenome-wide associated loci in the promiximty of <strong>{symbol}</strong>{' '}
-    as likely causal gene for <strong>{diseaseName}</strong>. Source{' '}
+    as likely causal gene for <strong>{diseaseName}</strong>. Source:{' '}
     <Link to="https://phewascatalog.org" external>
       PheWAS Catalog
     </Link>
