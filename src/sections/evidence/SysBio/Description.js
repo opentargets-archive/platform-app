ffbf3aa9f0e6bd44d70910f4a5cff4323e267838
Luca Fumis <lucaf@ebi.ac.uk>
2020-12-14 01:44:51 +0000

    Update evidence: gene signatures (sysbio)

--- src/sections/evidence/SysBio/Description.js
+++ src/sections/evidence/SysBio/Description.js
@@ -4,7 +4,15 @@ function Description({ symbol, name }) {
   return (
     <>
       Genetic associations between <strong>{symbol}</strong> and{' '}
-      <strong>{name}</strong>.
+      <strong>{name}</strong>. Literature-reported analysis defining gene
+      signatures causally associating <strong>{symbol}</strong> with{' '}
+      <strong>{name}</strong>. Source:{' '}
+      <Link
+        to="https://docs.targetvalidation.org/data-sources/affected-pathways#sysbio "
+        external
+      >
+        Literature
+      </Link>
     </>
   );
 }
