f65d5cc469c5904b40f2f7f98ac4229b898dfeef
Luca Fumis <lucaf@ebi.ac.uk>
2021-02-03 14:08:41 +0000

    Revert to beta API

--- src/client.js
+++ src/client.js
@@ -13,11 +13,11 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 const client = new ApolloClient({
   link: new HttpLink({
     // uri: 'https://platform-api-alpha.opentargets.io/api/v4/graphql',
-    // uri:
-    //   'https://api-beta-dot-open-targets-eu-dev.ew.r.appspot.com/api/v4/graphql',
-
     uri:
-      'https://hpo-1-0-dot-api-beta-dot-open-targets-eu-dev.ew.r.appspot.com/api/v4/graphql',
+      'https://api-beta-dot-open-targets-eu-dev.ew.r.appspot.com/api/v4/graphql',
+
+    // uri:
+    //   'https://hpo-1-0-dot-api-beta-dot-open-targets-eu-dev.ew.r.appspot.com/api/v4/graphql',
   }),
   cache: new InMemoryCache({ fragmentMatcher }),
 });
