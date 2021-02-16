7cc13e8550cd6110e67ea2aaa90195f4b4ef162f
Luca Fumis <lucaf@ebi.ac.uk>
2021-02-16 10:06:08 +0000

    Add tooltip to synonyms

--- src/components/ProfileHeader/ChipList.js
+++ src/components/ProfileHeader/ChipList.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Skeleton } from '@material-ui/lab';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Tooltip, makeStyles } from '@material-ui/core';
 
 import Chip from '../Chip';
 import LongList from '../LongList';
+import _ from 'lodash';
+
+const useContainerStyles = makeStyles(theme => ({
+  tooltip: {
+    backgroundColor: theme.palette.background.paper,
+    border: `1px solid ${theme.palette.grey[300]}`,
+    color: theme.palette.text.primary,
+  },
+}));
 
 function ChipList({ children, title, loading = false, inline }) {
+  const classes = useContainerStyles();
   if (inline && loading) return <Skeleton count={1} />;
 
   if (!children || children.length === 0) return null;
@@ -22,7 +32,26 @@ function ChipList({ children, title, loading = false, inline }) {
         <LongList
           terms={children}
           maxTerms={10}
-          render={item => <Chip key={item} label={item} title={item} />}
+          render={item => {
+            if (_.isString(item)) {
+              console.log('no string');
+              return <Chip key={item} label={item} title={item} />;
+            } else {
+              return (
+                <Tooltip
+                  placement="top"
+                  interactive
+                  classes={{ tooltip: classes.tooltip }}
+                  title={item.tooltip}
+                  key={item.label}
+                >
+                  <span>
+                    <Chip label={item.label} />
+                  </span>
+                </Tooltip>
+              );
+            }
+          }}
           size="small"
         />
       )}
