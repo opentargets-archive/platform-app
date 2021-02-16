76763adb41b5518a31635de5e8360d91e010940b
Luca Fumis <lucaf@ebi.ac.uk>
2021-02-16 10:06:08 +0000

    Add tooltip to synonyms

--- src/pages/DiseasePage/ProfileHeader.js
+++ src/pages/DiseasePage/ProfileHeader.js
@@ -6,13 +6,14 @@ import {
   ProfileHeader as BaseProfileHeader,
   ChipList,
 } from '../../components/ProfileHeader';
+// import ChipList from '../../components/ChipList';
 import usePlatformApi from '../../hooks/usePlatformApi';
 
 const DISEASE_PROFILE_HEADER_FRAGMENT = gql`
   fragment DiseaseProfileHeaderFragment on Disease {
     description
     synonyms {
-      # relation
+      relation
       terms
     }
   }
@@ -24,13 +25,17 @@ function ProfileHeader() {
   //TODO: Errors!
   if (error) return null;
 
+  const synonyms = data?.disease.synonyms
+    ?.map(s => {
+      return s.terms.map(syn => ({ label: syn, tooltip: s.relation }));
+    })
+    .reduce((acc, val) => acc.concat(val), []);
+
   return (
     <BaseProfileHeader>
       <Description loading={loading}>{data?.disease.description}</Description>
       <ChipList title="Synonyms" loading={loading}>
-        {data?.disease.synonyms
-          ?.map(s => s.terms)
-          .reduce((acc, val) => acc.concat(val), [])}
+        {synonyms}
       </ChipList>
     </BaseProfileHeader>
   );
