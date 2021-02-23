4e4f754f0a3615cccbe593889a2fe15784d7e873
Luca Fumis <lucaf@ebi.ac.uk>
2021-02-23 16:54:13 +0000

    Phenotypes polishing/updates

--- src/sections/disease/Phenotypes/index.js
+++ src/sections/disease/Phenotypes/index.js
@@ -1,7 +1,7 @@
 export const definition = {
   id: 'phenotypes',
-  name: 'Phenotypes',
-  shortName: 'PH',
+  name: 'Clinical signs and symptoms',
+  shortName: 'CS',
   hasData: data => (data.phenotypes?.count || 0) > 0,
 };
 
