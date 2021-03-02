2126d9dd71fa7a1b1ebdd632045c7fd0c496bc82
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-02 12:03:19 +0000

    another bug fix

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,5 @@
 // Configuration Object
 export const config = {
-    urlAppolloClient : typeof configUrlAppolloClient !== undefined ? configUrlAppolloClient : 'https://platform-api-beta.opentargets.io/api/v4/graphql',
-    urlAppolloClientBeta : typeof configUrlAppolloClientBeta  !== undefined ? configUrlAppolloClientBeta : 'https://platform-api-beta.opentargets.io/api/v4/graphql'
+    urlAppolloClient : typeof configUrlAppolloClient !== 'undefined' ? configUrlAppolloClient : 'https://platform-api-beta.opentargets.io/api/v4/graphql',
+    urlAppolloClientBeta : typeof configUrlAppolloClientBeta  !== 'undefined' ? configUrlAppolloClientBeta : 'https://platform-api-beta.opentargets.io/api/v4/graphql'
 };
