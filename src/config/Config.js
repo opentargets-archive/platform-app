abf32b8f20c6260f9075ef842e0089df57e5ba5f
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-02 13:21:51 +0000

    finally, ESLint, nothing to see here

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,7 @@
 // Configuration Object
 export const config = {
+    // eslint-disable-next-line
     urlAppolloClient : typeof configUrlAppolloClient !== 'undefined' ? configUrlAppolloClient : 'https://platform-api-beta.opentargets.io/api/v4/graphql',
+    // eslint-disable-next-line
     urlAppolloClientBeta : typeof configUrlAppolloClientBeta  !== 'undefined' ? configUrlAppolloClientBeta : 'https://platform-api-beta.opentargets.io/api/v4/graphql'
 };
