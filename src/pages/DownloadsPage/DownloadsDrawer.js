0f55f920d62e65916961196153273bcb72bfbdd2
Alfredo Miranda <alfredo@miranda.io>
2021-04-08 22:40:27 +0100

    Add some styling

--- src/pages/DownloadsPage/DownloadsDrawer.js
+++ src/pages/DownloadsPage/DownloadsDrawer.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
-import { Drawer, Typography, makeStyles } from '@material-ui/core';
+import { Drawer, Box, Typography, makeStyles } from '@material-ui/core';
 import ReactJson from 'react-json-view';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   backdrop: {
     '& .MuiBackdrop-root': {
       opacity: '0 !important',
     },
   },
-});
+  container: {
+    backgroundColor: theme.palette.grey[300],
+  },
+  title: {
+    backgroundColor: 'white',
+    borderBottom: '1px solid #ccc',
+    fontSize: '1.2rem',
+    fontWeight: 'bold',
+    padding: '1rem',
+  },
+}));
 
 function DownloadsDrawer({ title, data, children }) {
   const classes = useStyles();
@@ -28,15 +38,17 @@ function DownloadsDrawer({ title, data, children }) {
     <>
       <span onClick={toggleOpen}>{children}</span>
       <Drawer
-        className={classes.backdrop}
+        classes={{ root: classes.backdrop, paper: classes.container }}
         open={open}
         onClose={close}
         anchor="right"
       >
-        <Typography>{title}</Typography>
-        <Typography>Data</Typography>
-        <Typography>Schema</Typography>
-        <ReactJson src={schema} />
+        <Typography className={classes.title}>{title}</Typography>
+        <Box p={2} width={400}>
+          <Typography>Data</Typography>
+          <Typography>Schema</Typography>
+          <ReactJson src={schema} enableClipboard={false} />
+        </Box>
       </Drawer>
     </>
   );
