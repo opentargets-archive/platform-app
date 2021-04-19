d94332313fb7cf400b72b8df231266049e650691
Luca Fumis <lucaf@ebi.ac.uk>
2021-04-19 17:25:57 +0100

    Update scoring field to score

--- src/sections/target/MolecularInteractions/StringTab.js
+++ src/sections/target/MolecularInteractions/StringTab.js
@@ -139,9 +139,9 @@ function getColumns(classes) {
         sortLabel: classes.sortLabel,
         innerLabel: classes.innerLabel,
       },
-      renderCell: row => row.scoring.toFixed(3),
-      exportValue: row => row.scoring.toFixed(3),
-      filterValue: row => row.scoring.toFixed(3),
+      renderCell: row => row.score.toFixed(3),
+      exportValue: row => row.score.toFixed(3),
+      filterValue: row => row.score.toFixed(3),
     },
     {
       id: 'neighbourhood',
