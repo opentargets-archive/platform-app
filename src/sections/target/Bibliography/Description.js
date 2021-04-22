e6dc33f100e981bdb07bd8a57c3f2a881b022dd2
carcruz <me@carcruzcast.com>
2021-04-22 01:10:20 -0600

    feat: descriptions text

--- src/sections/target/Bibliography/Description.js
+++ src/sections/target/Bibliography/Description.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Link from '../../../components/Link';
+
+const url = 'http://platform-docs.opentargets.org/bibliography';
 
 function Description({ name }) {
   return (
     <>
-      Scientific literature related to <strong>{name}</strong>.
+      Scientific literature mentioning NLP-recognised entity{' '}
+      <strong>{name}</strong> and other selected co-occurring entities. Source:{' '}
+      <Link external to={url}>
+        Open Targets
+      </Link>
+      .
     </>
   );
 }
