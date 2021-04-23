acdc98badd8c6ba70119d923b044f23923c4abf2
Alfredo Miranda <alfredo@miranda.io>
2021-04-23 21:28:35 +0100

    Cleanup

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,18 +1,10 @@
 // Configuration Object
 export const config = {
   urlApi:
-    // eslint-disable-next-line
-    typeof configUrlApi !== 'undefined'
-      ? // eslint-disable-next-line
-        configUrlApi
-      : 'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
-
+    window.configUrlApi ??
+    'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
   urlApiBeta:
-    // eslint-disable-next-line
-    typeof configUrlApiBeta !== 'undefined'
-      ? // eslint-disable-next-line
-        configUrlApiBeta
-      : 'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
-  // eslint-disable-next-line
-  GTMID: typeof configGTMID !== 'undefined' ? configGTMID : null,
+    window.configUrlApiBeta ??
+    'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
+  GTMID: window.configGTMID ?? null,
 };
