d69a5100cffa91903a8e6739a82ff40a130cd401
Alfredo Miranda <alfredo@miranda.io>
2021-04-26 11:02:54 +0100

    Clean up

--- src/sections/evidence/UniProtVariants/index.js
+++ src/sections/evidence/UniProtVariants/index.js
@@ -2,10 +2,7 @@ export const definition = {
   id: 'uniprotVariants',
   name: 'UniProt variants',
   shortName: 'UV',
-  hasData: data => {
-    console.log('data', data);
-    return data.uniprotVariantsSummary.count > 0;
-  },
+  hasData: data => data.uniprotVariantsSummary.count > 0,
 };
 
 export { default as Summary } from './Summary';
