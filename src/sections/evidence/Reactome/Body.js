4a75008a952d9f2adf281a703728e43737dbb693
Luca Fumis <lucaf@ebi.ac.uk>
2021-04-27 10:49:46 +0100

    Remove unused imports

--- src/sections/evidence/Reactome/Body.js
+++ src/sections/evidence/Reactome/Body.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { loader } from 'graphql.macro';
-import { Box, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import usePlatformApi from '../../../hooks/usePlatformApi';
 import SectionItem from '../../../components/Section/SectionItem';
 import Tooltip from '../../../components/Tooltip';
