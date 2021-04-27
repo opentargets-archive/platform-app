c0af833f8653d45a017f00abe723a2a19405ea27
Alfredo Miranda <alfredo@miranda.io>
2021-04-27 09:27:23 +0100

    Set up deployment context var for google tag manager (#345)

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,16 +1,10 @@
 // Configuration Object
 export const config = {
   urlApi:
-    // eslint-disable-next-line
-    typeof configUrlApi !== 'undefined'
-      ? // eslint-disable-next-line
-        configUrlApi
-      : 'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
-
+    window.configUrlApi ??
+    'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
   urlApiBeta:
-    // eslint-disable-next-line
-    typeof configUrlApiBeta !== 'undefined'
-      ? // eslint-disable-next-line
-        configUrlApiBeta
-      : 'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
+    window.configUrlApiBeta ??
+    'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
+  googleTagManagerID: window.configGoogleTagManagerID ?? null,
 };
