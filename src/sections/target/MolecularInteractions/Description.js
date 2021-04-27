61ab80bc4a88e06ec66224e95046034e2a7e27cd
Andrew Hercules <andrewhercules@gmail.com>
2021-04-27 17:10:29 +0100

    Fixed documentation links for Molecular Interactions section; opentargets/platform#1499

--- src/sections/target/MolecularInteractions/Description.js
+++ src/sections/target/MolecularInteractions/Description.js
@@ -5,23 +5,38 @@ const Description = ({ symbol }) => (
   <React.Fragment>
     Physical and functional molecular interactions with{' '}
     <strong>{symbol}</strong>. Source:{' '}
-    <Link to="https://docs.targetvalidation.org/data-sources/" external>
+    <Link
+      to="https://platform-docs.opentargets.org/target/molecular-interactions"
+      external
+    >
       Open Targets
     </Link>
     ,{' '}
-    <Link to="https://docs.targetvalidation.org/data-sources/" external>
+    <Link
+      to="https://platform-docs.opentargets.org/target/molecular-interactions#intact"
+      external
+    >
       IntAct
     </Link>
     ,{' '}
-    <Link to="https://docs.targetvalidation.org/data-sources/" external>
+    <Link
+      to="https://platform-docs.opentargets.org/target/molecular-interactions#signor"
+      external
+    >
       Signor
     </Link>
     ,{' '}
-    <Link to="https://docs.targetvalidation.org/data-sources/" external>
+    <Link
+      to="https://platform-docs.opentargets.org/target/molecular-interactions#reactome"
+      external
+    >
       Reactome
     </Link>
     ,{' '}
-    <Link to="https://docs.targetvalidation.org/data-sources/" external>
+    <Link
+      to="https://platform-docs.opentargets.org/target/molecular-interactions#string"
+      external
+    >
       String
     </Link>
   </React.Fragment>
