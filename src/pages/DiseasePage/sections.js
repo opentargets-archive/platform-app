eaac98ba365993f22ec8bf88e78813c27cffbfe6
Alfredo Miranda <alfredo@miranda.io>
2021-05-26 13:37:09 +0100

    Implement OT projects summary

--- src/pages/DiseasePage/sections.js
+++ src/pages/DiseasePage/sections.js
@@ -3,6 +3,7 @@ import * as KnownDrugs from '../../sections/disease/KnownDrugs';
 import * as Ontology from '../../sections/disease/Ontology';
 import * as Phenotypes from '../../sections/disease/Phenotypes';
 import * as Bibliography from '../../sections/disease/Bibliography';
+import * as OTProjects from '../../sections/disease/OTProjects';
 
-const sections = [Ontology, KnownDrugs, Phenotypes, Bibliography];
+const sections = [Ontology, KnownDrugs, Phenotypes, Bibliography, OTProjects];
 export default sections;
---
+++ src/sections/disease/OTProjects/index.js
@@ -0,0 +1,9 @@
+export const definition = {
+  id: 'otarProjects',
+  name: 'Open Targets Projects',
+  shortName: 'OP',
+  hasData: () => true,
+};
+
+export { default as Summary } from './Summary';
+export { default as Body } from './Body';
