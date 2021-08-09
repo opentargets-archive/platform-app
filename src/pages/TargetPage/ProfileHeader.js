98f93a26897aaf719c9694737c46087186a0e9a4
Luca Fumis <lucaf@ebi.ac.uk>
2021-08-09 11:32:12 +0100

    Update and parse synonyms in label/source format

--- src/pages/TargetPage/ProfileHeader.js
+++ src/pages/TargetPage/ProfileHeader.js
@@ -10,6 +10,31 @@ import usePlatformApi from '../../hooks/usePlatformApi';
 
 const TARGET_PROFILE_HEADER_FRAGMENT = loader('./TargetProfileHeader.gql');
 
+/**
+ * Target synonyms from the API have a "label" and a "source"
+ * and can be lister more than once, with different sources.
+ * Parse synonyms to a unique list (label) where terms can have
+ * multiple sources in a tooltip
+ */
+const parseSynonyms = synonyms => {
+  const t = [];
+  synonyms.forEach(s => {
+    const thisSyn = t.find(
+      t => t.label.toLowerCase() === s.label.toLowerCase()
+    );
+    if (!thisSyn) {
+      t.push({ label: s.label, tooltip: [s.source] });
+    } else {
+      // if the synonym is already in the list,
+      // just add the source to its tooltip
+      thisSyn.tooltip.push(s.source);
+    }
+  });
+  // Tooltip component needs a string to display correctly
+  t.forEach(syn => (syn.tooltip = syn.tooltip.join(', ')));
+  return t;
+};
+
 function ProfileHeader() {
   const { loading, error, data } = usePlatformApi();
 
@@ -17,9 +42,7 @@ function ProfileHeader() {
   if (error) return null;
 
   const description = data?.target.proteinAnnotations?.functions?.[0];
-  const synonyms = data?.target.symbolSynonyms.concat(
-    data?.target.nameSynonyms
-  );
+  const synonyms = parseSynonyms(data?.target.synonyms || []);
 
   return (
     <BaseProfileHeader>
