091156d14dbbcde799fa2abf6c18606c0ba48931
Alfredo Miranda <alfredo@miranda.io>
2021-08-16 16:15:42 +0100

    Implement subcellular location summary section

---
+++ src/sections/target/SubcellularLocation/Body.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Body() {
+  return <div>body</div>;
+}
+
+export default Body;
---
+++ src/sections/target/SubcellularLocation/Summary.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { loader } from 'graphql.macro';
+
+import SummaryItem from '../../../components/Summary/SummaryItem';
+import usePlatformApi from '../../../hooks/usePlatformApi';
+
+const SUBCELLULAR_LOCATION_FRAGMENT = loader(
+  './SubcellularLocationFragment.gql'
+);
+
+function Summary({ definition }) {
+  const request = usePlatformApi(SUBCELLULAR_LOCATION_FRAGMENT);
+
+  return (
+    <SummaryItem
+      definition={definition}
+      request={request}
+      renderSummary={({ subcellularLocations }) => {
+        console.log('data', subcellularLocations);
+        return `${subcellularLocations.length} subcelullar locations`;
+      }}
+    />
+  );
+}
+
+Summary.fragments = {
+  SubcellularLocationFragment: SUBCELLULAR_LOCATION_FRAGMENT,
+};
+
+export default Summary;
