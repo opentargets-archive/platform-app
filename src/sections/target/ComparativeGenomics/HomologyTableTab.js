6ded7f3989746a3e22a2d3ca264d1d9849a359c9
Alfredo Miranda <alfredo@miranda.io>
2021-08-23 16:55:35 +0100

    Update

--- src/sections/target/ComparativeGenomics/HomologyTableTab.js
+++ src/sections/target/ComparativeGenomics/HomologyTableTab.js
@@ -1,52 +1,95 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
 
 import Link from '../../../components/Link';
+import Tooltip from '../../../components/Tooltip';
 import { DataTable } from '../../../components/Table';
 import { identifiersOrgLink } from '../../../utils/global';
 import { defaultRowsPerPageOptions, decimalPlaces } from '../../../constants';
 
-const columns = [
-  {
-    id: 'speciesName',
-    label: 'Species',
+const useStyles = makeStyles(theme => ({
+  star: {
+    color: theme.palette.primary.main,
   },
-  {
-    id: 'homologyType',
-    label: 'Homology type',
-    renderCell: ({ homologyType }) => homologyType.replaceAll('_', ' '),
+  container: {
+    display: 'inline-block',
+    width: '16px',
   },
-  {
-    id: 'targetGeneSymbol',
-    label: 'Homologue',
-    renderCell: ({ targetGeneId, targetGeneSymbol }) => (
-      <Link external to={identifiersOrgLink('ensembl', targetGeneId)}>
-        {targetGeneSymbol}
-      </Link>
-    ),
-  },
-  {
-    id: 'queryPercentageIdentity',
-    label: `Query %id`,
-    renderCell: ({ queryPercentageIdentity }) =>
-      queryPercentageIdentity
-        ? queryPercentageIdentity.toFixed(decimalPlaces)
-        : 'N/A',
-  },
-  {
-    id: 'targetPercentageIdentity',
-    label: `Target %id`,
-    renderCell: ({ targetPercentageIdentity }) =>
-      targetPercentageIdentity
-        ? targetPercentageIdentity.toFixed(decimalPlaces)
-        : 'N/A',
-  },
-];
+}));
+
+function getColumns(classes) {
+  return [
+    {
+      id: 'speciesName',
+      label: 'Species',
+    },
+    {
+      id: 'homologyType',
+      label: 'Homology type',
+      renderCell: ({ isHighConfidence, homologyType }) => {
+        return (
+          <>
+            <span className={classes.container}>
+              {isHighConfidence === 'NULL' ? null : (
+                <Tooltip
+                  title={
+                    isHighConfidence === '1'
+                      ? 'High confidence orthologue'
+                      : 'Low confidence orthologue'
+                  }
+                >
+                  <span>
+                    <FontAwesomeIcon
+                      className={isHighConfidence === '1' ? classes.star : ''}
+                      icon={isHighConfidence === '1' ? faStarSolid : faStar}
+                    />
+                  </span>
+                </Tooltip>
+              )}
+            </span>{' '}
+            {homologyType.replaceAll('_', ' ')}
+          </>
+        );
+      },
+    },
+    {
+      id: 'targetGeneSymbol',
+      label: 'Homologue',
+      renderCell: ({ targetGeneId, targetGeneSymbol }) => (
+        <Link external to={identifiersOrgLink('ensembl', targetGeneId)}>
+          {targetGeneSymbol}
+        </Link>
+      ),
+    },
+    {
+      id: 'queryPercentageIdentity',
+      label: `Query %id`,
+      renderCell: ({ queryPercentageIdentity }) =>
+        queryPercentageIdentity
+          ? queryPercentageIdentity.toFixed(decimalPlaces)
+          : 'N/A',
+    },
+    {
+      id: 'targetPercentageIdentity',
+      label: `Target %id`,
+      renderCell: ({ targetPercentageIdentity }) =>
+        targetPercentageIdentity
+          ? targetPercentageIdentity.toFixed(decimalPlaces)
+          : 'N/A',
+    },
+  ];
+}
 
 function HomologyTableTab({ data }) {
+  const classes = useStyles();
+
   return (
     <DataTable
       dataDownloader
-      columns={columns}
+      columns={getColumns(classes)}
       rows={data.target.homologues}
       rowsPerPageOptions={defaultRowsPerPageOptions}
     />
