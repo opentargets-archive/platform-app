22717661fc64868eebaf8e0febc663ba79bfc1a4
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2021-08-27 13:58:01 -0400

    Add Fusion tables into Somatic Mutation Widget

--- src/sections/evidence/SomaticMutation/Body.js
+++ src/sections/evidence/SomaticMutation/Body.js
@@ -8,6 +8,8 @@ import * as DummyData from './DummyData';
 import SnvByGeneTab from './SnvByGeneTab';
 import SnvByVariantTab from './SnvByVariantTab';
 import CnvByGeneTab from './CnvByGeneTab';
+import FusionByGeneTab from './FusionByGeneTab';
+import FusionTab from './FusionTab';
 
 function Body({ definition, id, label }) {
   const defaultTab = "snvByGene";
@@ -20,7 +22,10 @@ function Body({ definition, id, label }) {
         evidences: {
           snvByGeneRows: DummyData.snvByGeneRows,
           snvByVariantRows: DummyData.snvByVariantRows,
-          cnvByGeneRows: DummyData.cnvByGeneRows
+          cnvByGeneRows: DummyData.cnvByGeneRows,
+          fusionByGeneRows: DummyData.fusionByGene,
+          fusionRows: DummyData.fusion
+
         }
       }
     },
@@ -39,22 +44,31 @@ function Body({ definition, id, label }) {
         <Description symbol={label.symbol} name={label.name} />
       )}
       renderBody={({ disease }) => {
-        const { snvByGeneRows, snvByVariantRows, cnvByGeneRows } = disease.evidences;
+        const { snvByGeneRows, snvByVariantRows, cnvByGeneRows, fusionByGeneRows, fusionRows} = disease.evidences;
         return (
           <>
             <Tabs value={tab} onChange={handleChangeTab} style={{ marginBottom: '2rem' }}>
               <Tab value="snvByGene" label="SNV By Gene"></Tab>
               <Tab value="snvByVariant" label="SNV By Variant"></Tab>
               <Tab value="cnvByGene" label="CNV By Gene"></Tab>
+              <Tab value="fusionByGene" label="Fusion By Gene"></Tab>
+              <Tab value="fusion" label="Fusion"></Tab>
             </Tabs>
             {/* table 1: SNV by Gene */}
-            { tab === "snvByGene" && <SnvByGeneTab data={snvByGeneRows} /> }
+            { tab === "snvByGene" && <SnvByGeneTab data={snvByGeneRows} ids={id} labels={label}  /> }
 
             {/* table 2: SNV by Variant */}
-            { tab === "snvByVariant" && <SnvByVariantTab data={snvByVariantRows}/> }
+            { tab === "snvByVariant" && <SnvByVariantTab data={snvByVariantRows} ids={id} labels={label} /> }
 
             {/* table 3: CNV by Gene*/}
-            { tab === "cnvByGene" && <CnvByGeneTab data={cnvByGeneRows} /> }
+            { tab === "cnvByGene" && <CnvByGeneTab data={cnvByGeneRows} ids={id} labels={label}  /> }
+
+            {/* table 4: Fusion by Gene*/}
+            { tab === "fusionByGene" && <FusionByGeneTab data={fusionByGeneRows} ids={id} labels={label}  /> }
+
+            {/* table 5: Fusion */}
+            { tab === "fusion" && <FusionTab data={fusionRows} ids={id} labels={label}  /> }
+
           </>
         );
       }}
