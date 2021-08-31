61348e73ba610968b359f0721e814f228fcf80d4
Luca Fumis <lucaf@ebi.ac.uk>
2021-08-31 12:00:27 +0100

    Add basic lock component to page header

--- src/components/OpenTargetsTitle.js
+++ src/components/OpenTargetsTitle.js
@@ -3,6 +3,11 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
 
+import config from '../config';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import Tooltip from './Tooltip';
+
 const styles = () => ({
   root: {
     display: 'inline',
@@ -23,6 +28,16 @@ const OpenTargetsTitle = ({ classes, className, name }) => {
     <Typography className={titleClasses} variant="h6" color="inherit">
       <span className={classes.fat}>Open Targets </span>
       <span className={classes.thin}>{name}</span>
+      {config.isPartnerPreview ? (
+        <>
+          <span className={classes.thin}> - Partner Preview </span>
+          <Tooltip title="The partner preview Platform contains all data found in the public Platform along with pre-publication data from Open Targets' projects and new features developed by the Platform team. Please do not share data or screenshots outside of the consortium partners. For more information, please email helpdesk@opentargets.org.">
+            <span>
+              <FontAwesomeIcon icon={faLock} />
+            </span>
+          </Tooltip>
+        </>
+      ) : null}
     </Typography>
   );
 };
