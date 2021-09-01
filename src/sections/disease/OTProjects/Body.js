347105a998c1e53b354597226a26118344508fc3
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-01 14:01:43 +0100

    Update otarproject widget and table

--- src/sections/disease/OTProjects/Body.js
+++ src/sections/disease/OTProjects/Body.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { loader } from 'graphql.macro';
 
 import Description from './Description';
 import Link from '../../../components/Link';
@@ -8,39 +6,29 @@ import { DataTable } from '../../../components/Table';
 import SectionItem from '../../../components/Section/SectionItem';
 import { defaultRowsPerPageOptions } from '../../../constants';
 import projects from './projects.json';
+import usePlatformApi from '../../../hooks/usePlatformApi';
 
-const OT_PROJECTS_QUERY = loader('./OTProjectsQuery.gql');
+const columns = [
+  {
+    id: 'otarCode',
+    label: 'Project Code',
+  },
+  {
+    id: 'projectName',
+    label: 'Project name',
+  },
+  { id: 'status', label: 'Status' },
+  {
+    id: 'reference',
+    label: 'Open Targest Intranet Link',
+    renderCell: ({ otarCode }) => {
+      return <Link to="/">{otarCode} project page</Link>;
+    },
+  },
+];
 
-function getColumns(diseaseName) {
-  return [
-    {
-      id: 'otarCode',
-      label: 'Project Code',
-    },
-    {
-      id: 'projectName',
-      label: 'Project name',
-    },
-    { id: 'name', label: 'Disease', renderCell: () => diseaseName },
-    { id: 'status', label: 'Status' },
-    {
-      id: 'reference',
-      label: 'Open Targest Intranet Link',
-      renderCell: ({ otarCode }) => {
-        return <Link to="/">{otarCode} project page</Link>;
-      },
-    },
-  ];
-}
-
-function Body({ definition, label, id }) {
-  const request = useQuery(OT_PROJECTS_QUERY, {
-    variables: {
-      efoId: id,
-    },
-  });
-
-  const columns = getColumns(label);
+function Body({ definition, label }) {
+  const request = usePlatformApi();
 
   return (
     <SectionItem
@@ -55,6 +43,7 @@ function Body({ definition, label, id }) {
             columns={columns}
             rows={projects}
             rowsPerPageOptions={defaultRowsPerPageOptions}
+            sortBy="status"
           />
         );
       }}
