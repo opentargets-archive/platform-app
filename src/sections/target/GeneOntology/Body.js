e144151318de3e1125af4e3562db62b9e5f349a3
carcruz <me@carcruzcast.com>
2021-09-01 12:56:06 -0600

    feat: columns content and filtering

--- src/sections/target/GeneOntology/Body.js
+++ src/sections/target/GeneOntology/Body.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import usePlatformApi from '../../../hooks/usePlatformApi';
 import Description from './Description';
-import DataTable from '../../../components/Table/DataTable';
-import Link from '../../../components/Link';
-import SectionItem from '../../../components/Section/SectionItem';
-import { epmcUrl } from '../../../utils/urls';
-import { PublicationsDrawer } from '../../../components/PublicationsDrawer';
 import Summary from './Summary';
 import Tooltip from '../../../components/Tooltip';
+import { PublicationsDrawer } from '../../../components/PublicationsDrawer';
+import SectionItem from '../../../components/Section/SectionItem';
+import DataTable from '../../../components/Table/DataTable';
+import Link from '../../../components/Link';
+import usePlatformApi from '../../../hooks/usePlatformApi';
+import { epmcUrl } from '../../../utils/urls';
+import { defaultRowsPerPageOptions } from '../../../constants';
+import { sortBy, filter } from 'lodash';
+import GeneOntologyEvidenceCodeMap from './GeneOntologyEvidenceCodeMappings.json';
 
 const CATEGORY_BY_PREFIX = {
   F: { code: 'MOLECULAR_FUNCTION', label: 'Molecular Function' },
@@ -32,7 +35,7 @@ const sourceMapContent = source => {
   if (sourceName !== 'PMID')
     return (
       <Link external to={sourceURLS[sourceName](sourceId)}>
-        {source}
+        {sourceName}
       </Link>
     );
 
@@ -50,70 +53,75 @@ const sourceMapContent = source => {
     );
 };
 
-const EvidenceTooltip = () => (
-  <div>
-    GO annotations include an evidence code that indicates how the annotation to
-    a particular term is supported. Evidence codes fall into six general
-    categories:
-    <ul>
-      <li>
-        Experimental evidence (EXP, IDA, IPI, IMP, IGI, IEP, HTP, HDA, HMP, HGI,
-        HEP)
-      </li>
-      <li>Phylogenetic evidence (IBA, IBD, IKR, IRD)</li>
-      <li>Computational evidence (ISS, ISO, ISA, ISM, IGC, RCA)</li>
-      <li>Author statements (TAS, NAS)</li>
-      <li>Curatorial statements (IC, ND)</li>
-      <li> Curatorial statements (IC, ND)</li>
-      <li>Automatically generated annotations (IEA)</li>
-    </ul>
-    For more information, please visit
-    http://geneontology.org/docs/guide-go-evidence-codes/˝
-  </div>
-);
+const EvidenceTooltip = ({ evidence }) => {
+  const code = filter(GeneOntologyEvidenceCodeMap, {
+    evidenceCode: evidence,
+  })[0];
+  return (
+    <div>
+      <p>
+        <b>Label: </b>
+        <span>{code.evidenceLabel}</span>
+      </p>
+      <p>
+        <b>Category: </b>
+        {code.evidenceCategory}
+      </p>
+      <p>
+        <b>Source: </b>
+        <Link external to={code.evidenceSourceUrl}>
+          Gene Ontology wiki
+        </Link>
+      </p>
+    </div>
+  );
+};
 
 const columns = [
   {
     id: 'category',
     label: 'Category',
-    renderCell: row => row.category.label,
+    renderCell: ({ category }) => category.label,
+    filterValue: ({ category }) => category.label,
     exportLabel: 'Category',
+    propertyPath: 'category.label',
   },
   {
     id: 'goTerm',
     label: 'GO term',
-    renderCell: row => (
-      <Link external to={`https://identifiers.org/${row.term.id}`}>
-        {row.term.name}
+    renderCell: ({ term }) => (
+      <Link external to={`https://identifiers.org/${term.id}`}>
+        {term.name}
       </Link>
     ),
     exportLabel: 'GO term',
-    exportValue: row => row.term.name,
+    exportValue: ({ term }) => term.name,
+    filterValue: ({ term }) => term.name,
   },
   {
     id: 'geneProduct',
     label: 'Gene product',
-    renderCell: row => (
+    renderCell: ({ geneProduct, term }) => (
       <Link
         external
-        to={`https://www.ebi.ac.uk/QuickGO/annotations?geneProductId=${
-          row.geneProduct
-        }&goId=${row.term.id}`}
+        to={`https://www.ebi.ac.uk/QuickGO/annotations?geneProductId=${geneProduct}&goId=${
+          term.id
+        }`}
       >
-        {row.geneProduct}
+        {geneProduct}
       </Link>
     ),
     exportLabel: 'GO term',
-    exportValue: row => row.term.name,
+    exportValue: ({ term }) => term.name,
   },
   {
-    id: 'evidenceCode',
+    id: 'evidence',
     label: 'Evidence code',
     exportLabel: 'Evidence code',
     exportValue: row => row.evidence,
-    renderCell: row => (
-      <Tooltip title={<EvidenceTooltip />} showHelpIcon>
-        {row.evidence}
+    renderCell: ({ evidence }) => (
+      <Tooltip title={<EvidenceTooltip evidence={evidence} />} showHelpIcon>
+        {evidence}
       </Tooltip>
     ),
   },
@@ -136,15 +144,18 @@ function Section({ definition, label: symbol }) {
       definition={definition}
       request={request}
       renderDescription={() => <Description symbol={symbol} />}
-      renderBody={data => {
-        const rows = data.geneOntology.map(extractCategory);
+      renderBody={({ geneOntology }) => {
+        const rows = sortBy(
+          geneOntology.map(extractCategory),
+          'category.label'
+        );
         return (
           <DataTable
             showGlobalFilter
-            columns={columns}
             dataDownloader
+            columns={columns}
             rows={rows}
-            noWrap={false}
+            rowsPerPageOptions={defaultRowsPerPageOptions}
           />
         );
       }}
