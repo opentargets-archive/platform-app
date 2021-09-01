3c1b4802928f2ee28865f52ae698bea19f439dc6
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-01 18:11:15 +0100

    Update description

--- src/sections/disease/OTProjects/Description.js
+++ src/sections/disease/OTProjects/Description.js
@@ -6,7 +6,7 @@ function Description({ name }) {
   return (
     <React.Fragment>
       Active and closed projects for <strong>{name}</strong>. Source:{' '}
-      <Link external to="https://platform-docs.opentargets.org/">
+      <Link external to="http://home.opentargets.org/">
         Open Targets
       </Link>
       .
