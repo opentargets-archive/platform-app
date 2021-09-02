076cfd80d069c2032944cdc73d47e398d75c4a3c
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-02 15:23:49 +0100

    Add config for partner sections on disease page

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ const config = {
     window.configUrlApi ??
     process.env.REACT_APP_configUrlApi ??
     'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
-  urlApiBeta:
-    window.configUrlApiBeta ??
-    process.env.REACT_APP_configUrlApiBeta ??
-    'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
+  // urlApiBeta:
+  //   window.configUrlApiBeta ??
+  //   process.env.REACT_APP_configUrlApiBeta ??
+  //   'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
   googleTagManagerID: window.configGoogleTagManagerID ?? null,
   efoURL:
     window.configEFOURL ??
@@ -16,14 +16,20 @@ const config = {
     window.configPrimaryColor ??
     process.env.REACT_APP_configPrimaryColor ??
     '#3489ca',
-  flagShowOTARProjects:
-    window.configFlagShowOTARProjects ??
-    process.env.REACT_APP_configFlagShowOTARProjects ??
-    false,
+  // flagShowOTARProjects:
+  //   window.configFlagShowOTARProjects ??
+  //   process.env.REACT_APP_configFlagShowOTARProjects ??
+  //   false,
 
   // partner preview options
   isPartnerPreview:
     window.isPartnerPreview ?? process.env.REACT_APP_isPartnerPreview ?? false,
+
+  // partner (private) widgets on disease page, identified by widget id (as defined in widget definition)
+  partnerDiseaseSectionIds:
+    window.partnerDiseaseSectionIds ??
+    process.env.REACT_APP_partnerDiseaseSectionIds ??
+    '',
 };
 
 export default config;
