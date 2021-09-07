5e79031407e208d3124ea8811a61e3339f503ff2
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-07 19:11:01 +0100

    Add isPrivate param to widget definition

--- src/sections/disease/OTProjects/index.js
+++ src/sections/disease/OTProjects/index.js
@@ -1,8 +1,12 @@
+import { isPrivateDiseaseSection } from '../../../utils/partnerPreviewUtils';
+
+const id = 'otProjects';
 export const definition = {
   id: 'otarProjects',
   name: 'Open Targets Projects',
   shortName: 'OP',
   hasData: data => data.otarProjects?.length > 0,
+  isPrivate: isPrivateDiseaseSection(id),
 };
 
 export { default as Summary } from './Summary';
