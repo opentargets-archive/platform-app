fc8540e1af69ffe12d7c49d92b834a5c631fbd71
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-07 23:18:20 +0100

    Fix widget id

--- src/sections/disease/OTProjects/index.js
+++ src/sections/disease/OTProjects/index.js
@@ -2,7 +2,7 @@ import { isPrivateDiseaseSection } from '../../../utils/partnerPreviewUtils';
 
 const id = 'otProjects';
 export const definition = {
-  id: 'otarProjects',
+  id: id,
   name: 'Open Targets Projects',
   shortName: 'OP',
   hasData: data => data.otarProjects?.length > 0,
