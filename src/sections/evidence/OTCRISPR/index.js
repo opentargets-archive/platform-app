380d03ae47484ea3607ad1e342f157ba799fa86c
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-07 19:00:21 +0100

    Add isPrivate param to widget definition

--- src/sections/evidence/OTCRISPR/index.js
+++ src/sections/evidence/OTCRISPR/index.js
@@ -1,8 +1,12 @@
+import { isPrivateEvidenceSection } from '../../../utils/partnerPreviewUtils';
+
+const id = 'otCrispr';
 export const definition = {
-  id: 'ot_crispr',
+  id: id,
   name: 'Open Targets CRISPR',
   shortName: 'OT',
   hasData: ({ OtCrisprSummary }) => OtCrisprSummary.count > 0,
+  isPrivate: isPrivateEvidenceSection(id),
 };
 
 export { default as Summary } from './Summary';
