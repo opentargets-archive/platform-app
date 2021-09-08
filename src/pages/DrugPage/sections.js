1365c4668b0cb78f71c8c33208fbed48f6da1bbb
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-08 12:33:17 +0100

    Add logic for sections on/off on drug page

--- src/pages/DrugPage/sections.js
+++ src/pages/DrugPage/sections.js
@@ -6,6 +6,8 @@ import * as Indications from '../../sections/drug/Indications';
 import * as MechanismsOfAction from '../../sections/drug/MechanismsOfAction';
 import * as Pharmacovigilance from '../../sections/drug/AdverseEvents';
 
+import config from '../../config';
+
 const sections = [
   MechanismsOfAction,
   Indications,
@@ -13,5 +15,17 @@ const sections = [
   DrugWarnings,
   Pharmacovigilance,
   Bibliography,
-];
+].filter(
+  // select sections to show based on:
+  // 1. there is no specific selection for this page (length==0)
+  //    OR there is a specific list which includes this section
+  // AND
+  // 2. only include public section (i.e. not partner sections),
+  //    OR also private sections if it's a partner preview
+  section =>
+    (config.drugSectionIds.length == 0 ||
+      config.drugSectionIds.split(',').includes(section.definition.id)) &&
+    (!section.definition.isPrivate ||
+      (section.definition.isPrivate && config.isPartnerPreview))
+);
 export default sections;
