3202bfebea77014af828cae46bfcd36d5f878ada
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2021-09-10 13:15:43 -0400

    integrate CHOP's column configuration settings

--- src/sections/evidence/SomaticMutation/SnvByGeneTab.js
+++ src/sections/evidence/SomaticMutation/SnvByGeneTab.js
@@ -8,9 +8,6 @@ import RelevantIcon from '../../../components/RMTL/RelevantIcon';
 
 // Configuration for how the tables will display the data
 const columns = [
-  { id: 'Disease', label: 'Disease', sortable: true,
-    renderCell: ({ EFO, Disease }) => 
-      <Link to={`/disease/${EFO}`}>{Disease}</Link>},
   {
     id: 'Gene_symbol', label: 'Targets', sortable: true,
     renderCell: ({ Gene_symbol, Gene_Ensembl_ID }) => 
@@ -18,12 +15,15 @@ const columns = [
   },
   { id: 'PMTL', label: 'PMTL', sortable: true, renderCell: () => <RelevantIcon/>},
   { id: 'Dataset', label: 'Dataset', sortable: true },
-  // { id: 'EFO', label: 'EFO'},
-  // { id: 'MONDO',label: 'MONDO'},
+  
+  { id: 'Disease', label: 'Disease', sortable: true,
+    renderCell: ({ EFO, Disease }) => 
+      <Link to={`/disease/${EFO}`}>{Disease}</Link>},
   { id: 'Gene_full_name', label: 'Gene full name', sortable: true },
   { id: 'Gene_type', label: 'Gene type', sortable: true },
   { id: 'Protein_RefSeq_ID', label: 'Protein RefSeq ID', sortable: true },
-  // { id: 'Gene_Ensembl_ID',label: 'Gene Ensembl ID'},
+  { id: 'Gene_Ensembl_ID', label: 'Gene Ensembl ID' },
+  { id: 'Protein_Ensembl_ID', label: 'Protein Ensembl ID' },
   { id: 'Total_mutations_Over_Patients_in_dataset', label: 'Total mutations Over Patients in dataset', sortable: true },
   { id: 'Frequency_in_overall_dataset', label: 'Frequency in overall dataset', sortable: true },
   { id: 'Total_primary_tumors_mutated_Over_Primary_tumors_in_dataset', label: 'Total primary tumors mutated Over Primary tumors in dataset', sortable: true },
@@ -32,19 +32,22 @@ const columns = [
   { id: 'OncoKB_oncogene_T', label: 'OncoKB oncogene T', sortable: true},
   { id: 'PedcBioPortal', label: 'PedcBioPortal', renderCell: ({PedcBioPortal}) => <Link external to={'https://pedcbioportal.kidsfirstdrc.org/results/oncoprint?cancer_study_list=ped_opentargets_2021&case_set_id=ped_opentargets_2021_pbta_ependymoma&Action=Submit&gene_list=CXCL8'}>{PedcBioPortal}</Link>},
   { id: 'PedcBioPed', label: 'PedcBioPed', renderCell: ({PedcBioPed}) => <Link external to={'https://pedcbioportal.kidsfirstdrc.org/results/mutations?cancer_study_list=ped_opentargets_2021&case_set_id=ped_opentargets_2021_pbta_ependymoma&Action=Submit&gene_list=CXCL8'}>{PedcBioPed}</Link>},
+  // { id: 'EFO', label: 'EFO'},
+  // { id: 'MONDO',label: 'MONDO'},
 ]
 
 const dataDownloaderColumns = [
-  { id: 'Disease' },
   { id: 'Gene_symbol' },
   { id: 'PMTL' },
   { id: 'Dataset' },
+  { id: 'Disease' },
   { id: 'EFO' },
   { id: 'MONDO' },
   { id: 'Gene_full_name' },
   { id: 'Gene_type' },
   { id: 'Protein_RefSeq_ID' },
   { id: 'Gene_Ensembl_ID' },
+  { id: 'Protein_Ensembl_ID' },
   { id: 'Total_mutations_Over_Patients_in_dataset' },
   { id: 'Frequency_in_overall_dataset' },
   { id: 'Total_primary_tumors_mutated_Over_Primary_tumors_in_dataset' },
