9329f57f1174a04d316a4171f410af8d9a902985
Chen <jonkiky@gmail.com>
2021-09-14 09:23:10 -0400

    change the api url from qa to de

--- src/config/Config.js
+++ src/config/Config.js
@@ -2,9 +2,9 @@
 export const config = {
   urlApi:
     window.injectedEnv.configUrlApi ??
-    'https://ppdc-otp-qa.bento-tools.org/api/v4/graphql',
+    'https://ppdc-otp-dev.bento-tools.org/api/v4/graphql',
   urlApiBeta:
     window.injectedEnv.configUrlApiBeta ??
-    'https://ppdc-otp-qa.bento-tools.org/api/v4/graphql',
+    'https://ppdc-otp-dev.bento-tools.org/api/v4/graphql',
   googleTagManagerID: window.injectedEnv.configGoogleTagManagerID ?? null,
 };
