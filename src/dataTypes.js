a31aec79ad351ada2083dd9c72c9092207a30c39
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-17 18:06:41 +0100

    Add private option to datatypes based on config

--- src/dataTypes.js
+++ src/dataTypes.js
@@ -1,12 +1,46 @@
+import { isPrivateDataType } from './utils/partnerPreviewUtils';
+
 const dataTypes = [
-  { id: 'genetic_association', label: 'Genetic associations' },
-  { id: 'somatic_mutation', label: 'Somatic mutations' },
-  { id: 'known_drug', label: 'Drugs' },
-  { id: 'affected_pathway', label: 'Pathways & systems biology' },
-  { id: 'literature', label: 'Text mining' },
-  { id: 'rna_expression', label: 'RNA expression' },
-  { id: 'animal_model', label: 'Animal models' },
-  { id: 'ot_partner', label: 'OTAR Projects', isPrivate: true },
+  {
+    id: 'genetic_association',
+    label: 'Genetic associations',
+    isPrivate: isPrivateDataType('genetic_association'),
+  },
+  {
+    id: 'somatic_mutation',
+    label: 'Somatic mutations',
+    isPrivate: isPrivateDataType('somatic_mutation'),
+  },
+  {
+    id: 'known_drug',
+    label: 'Drugs',
+    isPrivate: isPrivateDataType('known_drug'),
+  },
+  {
+    id: 'affected_pathway',
+    label: 'Pathways & systems biology',
+    isPrivate: isPrivateDataType('affected_pathway'),
+  },
+  {
+    id: 'literature',
+    label: 'Text mining',
+    isPrivate: isPrivateDataType('literature'),
+  },
+  {
+    id: 'rna_expression',
+    label: 'RNA expression',
+    isPrivate: isPrivateDataType('rna_expression'),
+  },
+  {
+    id: 'animal_model',
+    label: 'Animal models',
+    isPrivate: isPrivateDataType('animal_model'),
+  },
+  {
+    id: 'ot_partner',
+    label: 'OTAR Projects',
+    isPrivate: isPrivateDataType('ot_partner'),
+  },
 ];
 
 const dataTypesMap = dataTypes.reduce((acc, dataType) => {
