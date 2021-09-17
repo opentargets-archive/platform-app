f7167a44ac9cdc605cd15b85d0523fa53f6bf87f
Luca Fumis <lucaf@ebi.ac.uk>
2021-09-17 18:08:56 +0100

    Update datatype facets visibility

--- src/components/Facets/utils.js
+++ src/components/Facets/utils.js
@@ -1,5 +1,6 @@
 import dataTypes from '../../dataTypes';
 import dataSources from '../../dataSources';
+import config from '../../config';
 
 const facetData = [...dataTypes, ...dataSources];
 
@@ -22,6 +23,19 @@ export const fixLabel = id => {
 const fixIsPrivate = id =>
   facetData.find(item => item.id === id)?.isPrivate || false;
 
+/**
+ * Filter a level in the datatypes facets based on config where
+ * the datatype can be explicitly hidden, or be private and as such
+ * not shown on public instance.
+ */
+const filterLevel = agg => {
+  return (
+    (config.hideDataTypes.length === 0 ||
+      !config.hideDataTypes.split(',').includes(agg.nodeId)) &&
+    (!agg.isPrivate || (agg.isPrivate && config.isPartnerPreview))
+  );
+};
+
 const sortLevel = (a, b) => {
   const aIndex = facetData.findIndex(item => item.id === a.nodeId);
   const bIndex = facetData.findIndex(item => item.id === b.nodeId);
@@ -31,8 +45,8 @@ const sortLevel = (a, b) => {
     : a.nodeId.localeCompare(b.nodeId);
 };
 
-const extractLevel = level =>
-  level
+const extractLevel = level => {
+  return level
     ?.map(agg => ({
       nodeId: agg.key || agg.name,
       label: fixLabel(agg.key || agg.name),
@@ -42,7 +56,9 @@ const extractLevel = level =>
       root: !!agg.name,
       isPrivate: fixIsPrivate(agg.key || agg.name),
     }))
+    .filter(filterLevel)
     .sort(sortLevel);
+};
 
 export const prepareFacetData = data => extractLevel(data) || [];
 
