25f2427b5a5e88172415ecd598574585ac4cee0d
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2021-09-20 10:42:16 -0400

    update Summary widget to include query from SomaticMutationQuery

--- src/sections/evidence/SomaticMutation/Summary.js
+++ src/sections/evidence/SomaticMutation/Summary.js
@@ -1,24 +1,38 @@
 import React from 'react';
+import { loader } from 'graphql.macro';
+
 import SummaryItem from '../../../components/Summary/SummaryItem';
 import { dataTypesMap } from '../../../dataTypes';
+import usePlatformApi from '../../../hooks/usePlatformApi';
+
+const SOMATIC_MUTATION_SUMMARY = loader('./SomaticMutationSummary.gql')
 
 function Summary({ definition }) {
   // Made up request/response until Graph Query Api is ready
-  const request = {
+  let request = {
     data: { somaticMutationSummary: { count: 10} }, loading: false, error: false,
   }
+
+  request = usePlatformApi(SOMATIC_MUTATION_SUMMARY);
 
   return (
     <SummaryItem
       definition={definition}
       request={request}
       renderSummary={({ somaticMutationSummary }) => {
-        const { count } = somaticMutationSummary;
-        return `${count} ${count === 1 ? 'entry' : 'entries'}`;
+        console.log(somaticMutationSummary);
+        const {count} = somaticMutationSummary;
+        console.log("Count for somaticMutationSummary: ", count)
+        return  'entries';
       }}
       subText={dataTypesMap.somatic_mutation}
     />
   );
 }
+
+Summary.fragments = {
+  somaticMutationSummary: SOMATIC_MUTATION_SUMMARY,
+};
+
 
 export default Summary;
