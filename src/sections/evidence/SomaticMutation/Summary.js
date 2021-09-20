27079198e0822ab6ba935c6e16bffb935a3759ff
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2021-09-20 18:25:15 -0400

    update hasData and create text when data is available in Summary widget

--- src/sections/evidence/SomaticMutation/Summary.js
+++ src/sections/evidence/SomaticMutation/Summary.js
@@ -5,6 +5,7 @@ import SummaryItem from '../../../components/Summary/SummaryItem';
 import { dataTypesMap } from '../../../dataTypes';
 import usePlatformApi from '../../../hooks/usePlatformApi';
 
+
 const SOMATIC_MUTATION_SUMMARY = loader('./SomaticMutationSummary.gql')
 
 function Summary({ definition }) {
@@ -19,11 +20,9 @@ function Summary({ definition }) {
     <SummaryItem
       definition={definition}
       request={request}
-      renderSummary={({ somaticMutationSummary }) => {
-        console.log(somaticMutationSummary);
-        const {count} = somaticMutationSummary;
-        console.log("Count for somaticMutationSummary: ", count)
-        return  'entries';
+      renderSummary={(data) => {
+        const hasData = definition.hasData(data);
+        return  hasData ? 'Available' : 'no data';
       }}
       subText={dataTypesMap.somatic_mutation}
     />
--- src/sections/evidence/SomaticMutation/index.js
+++ src/sections/evidence/SomaticMutation/index.js
@@ -2,8 +2,14 @@ export const definition = {
   id: 'somaticMutation',
   name: 'Somatic Mutation',
   shortName: 'SM',
-  hasData: ( somaticMutationSummary ) => somaticMutationSummary.count > 0,
-};
+  hasData: ( somaticMutationSummary ) => {
+    const {SnvByGene, SnvByVariant, CnvByGene, FusionByGene, Fusion} = somaticMutationSummary;
+    const hasCount = SnvByGene.count > 0 || SnvByVariant.count > 0 || CnvByGene.count > 0 || FusionByGene.count > 0 || Fusion.count > 0
+
+    return hasCount
+  
+  },
+}
 
 export { default as Summary } from './Summary';
 export { default as Body } from './Body';
