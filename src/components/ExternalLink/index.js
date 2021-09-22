ed7c94115f9f3f86b453e477effdd9f60c372820
Alfredo Miranda <alfredo@miranda.io>
2021-09-22 13:35:08 +0100

    Disease cross references (#451)

--- src/components/ExternalLink/index.js
+++ src/components/ExternalLink/index.js
@@ -1,3 +1,4 @@
 export { default as CrisprDepmapLink } from './CrisprDepmapLink';
 export { default as ExternalLink } from './ExternalLink';
 export { default as TepLink } from './TepLink';
+export { default as XRefLinks } from './XRefLinks';
