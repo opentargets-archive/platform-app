e223713dcc074531556b934389e1d237691f0ac2
carcruz <me@carcruzcast.com>
2021-10-04 23:09:48 +0100

    feat: lazy import of pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import { RecoilRoot } from 'recoil';
@@ -7,15 +7,18 @@ import OtUiThemeProvider from './components/OtUiThemeProvider';
 import client from './client';
 import initLocalStorage from './utils/initLocalStorage';
 import theme from './theme';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-import DiseasePage from './pages/DiseasePage';
-import DownloadsPage from './pages/DownloadsPage';
-import DrugPage from './pages/DrugPage';
-import TargetPage from './pages/TargetPage';
-import EvidencePage from './pages/EvidencePage';
-import VariantsPage from './pages/VariantsPage';
-import NotFoundPage from './pages/NotFoundPage';
+
+import LoadingBackdrop from './components/LoadingBackdrop';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const SearchPage = lazy(() => import('./pages/SearchPage'));
+const DiseasePage = lazy(() => import('./pages/DiseasePage'));
+const DownloadsPage = lazy(() => import('./pages/DownloadsPage'));
+const DrugPage = lazy(() => import('./pages/DrugPage'));
+const TargetPage = lazy(() => import('./pages/TargetPage'));
+const EvidencePage = lazy(() => import('./pages/EvidencePage'));
+const VariantsPage = lazy(() => import('./pages/VariantsPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
 class App extends Component {
   componentDidMount() {
@@ -28,20 +31,22 @@ class App extends Component {
         <ApolloProvider client={client}>
           <OtUiThemeProvider theme={theme}>
             <Router>
-              <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/search" component={SearchPage} />
-                <Route path="/downloads" component={DownloadsPage} />
-                <Route path="/disease/:efoId" component={DiseasePage} />
-                <Route path="/target/:ensgId" component={TargetPage} />
-                <Route path="/drug/:chemblId" component={DrugPage} />
-                <Route
-                  path="/evidence/:ensgId/:efoId"
-                  component={EvidencePage}
-                />
-                <Route path="/variants" component={VariantsPage} />
-                <Route component={NotFoundPage} />
-              </Switch>
+              <Suspense fallback={<LoadingBackdrop />}>
+                <Switch>
+                  <Route exact path="/" component={HomePage} />
+                  <Route path="/search" component={SearchPage} />
+                  <Route path="/downloads" component={DownloadsPage} />
+                  <Route path="/disease/:efoId" component={DiseasePage} />
+                  <Route path="/target/:ensgId" component={TargetPage} />
+                  <Route path="/drug/:chemblId" component={DrugPage} />
+                  <Route
+                    path="/evidence/:ensgId/:efoId"
+                    component={EvidencePage}
+                  />
+                  <Route path="/variants" component={VariantsPage} />
+                  <Route component={NotFoundPage} />
+                </Switch>
+              </Suspense>
             </Router>
           </OtUiThemeProvider>
         </ApolloProvider>
