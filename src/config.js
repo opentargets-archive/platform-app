1fdc5b8d2b6887a6959cff47b6dff49d22a24553
Luca Fumis <lucaf@ebi.ac.uk>
2021-10-21 17:26:34 +0100

    Remove netlify config options

--- src/config.js
+++ src/config.js
@@ -2,25 +2,18 @@
 const config = {
   urlApi:
     window.configUrlApi ??
-    process.env.REACT_APP_configUrlApi ??
     'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
   googleTagManagerID: window.configGoogleTagManagerID ?? null,
   efoURL:
     window.configEFOURL ??
     'https://storage.googleapis.com/open-targets-data-releases/alpha-rewrite/static/ontology/diseases_efo.jsonl',
-  primaryColor:
-    window.configPrimaryColor ??
-    process.env.REACT_APP_configPrimaryColor ??
-    '#3489ca',
+  primaryColor: window.configPrimaryColor ?? '#3489ca',
   helpdeskEmail: window.configHelpdeskEmail ?? 'helpdesk@opentargets.org',
 
   // Partner preview config options
 
   // main flag to toggle partner preview on/off
-  isPartnerPreview:
-    window.configIsPartnerPreview ??
-    process.env.REACT_APP_configIsPartnerPreview ??
-    false,
+  isPartnerPreview: window.configIsPartnerPreview ?? false,
 
   // Page specific sections:
   // hide[Page]SectionsIds: hide the specified sections (comma separated ids, no spaecs, e.g. 'bibliography,otProjects')
@@ -29,75 +22,39 @@ const config = {
   // partner[Page]SectionIds: specify the private widget on this page
 
   // disease page
-  hideDiseaseSectionIds:
-    window.configHideDiseaseSectionIds ??
-    process.env.REACT_APP_configHideDiseaseSectionIds ??
-    '',
+  hideDiseaseSectionIds: window.configHideDiseaseSectionIds ?? '',
 
-  partnerDiseaseSectionIds:
-    window.configPartnerDiseaseSectionIds ??
-    process.env.REACT_APP_configPartnerDiseaseSectionIds ??
-    '',
+  partnerDiseaseSectionIds: window.configPartnerDiseaseSectionIds ?? '',
 
   // target page
-  hideTargetSectionIds:
-    window.configHideTargetSectionIds ??
-    process.env.REACT_APP_configHideTargetSectionIds ??
-    '',
+  hideTargetSectionIds: window.configHideTargetSectionIds ?? '',
 
-  partnerTargetSectionIds:
-    window.configPartnerTargetSectionIds ??
-    process.env.REACT_APP_configPartnerTargetSectionIds ??
-    '',
+  partnerTargetSectionIds: window.configPartnerTargetSectionIds ?? '',
 
   // drug page
-  hideDrugSectionIds:
-    window.configHideDrugSectionIds ??
-    process.env.REACT_APP_configHideDrugSectionIds ??
-    '',
+  hideDrugSectionIds: window.configHideDrugSectionIds ?? '',
 
-  partnerDrugSectionIds:
-    window.configPartnerDrugSectionIds ??
-    process.env.REACT_APP_configPartnerDrugSectionIds ??
-    '',
+  partnerDrugSectionIds: window.configPartnerDrugSectionIds ?? '',
 
   // evidence page
-  hideEvidenceSectionIds:
-    window.configHideEvidenceSectionIds ??
-    process.env.REACT_APP_configHideEvidenceSectionIds ??
-    '',
+  hideEvidenceSectionIds: window.configHideEvidenceSectionIds ?? '',
 
-  partnerEvidenceSectionIds:
-    window.configPartnerEvidenceSectionIds ??
-    process.env.REACT_APP_configPartnerEvidenceSectionIds ??
-    '',
+  partnerEvidenceSectionIds: window.configPartnerEvidenceSectionIds ?? '',
 
   // partner colour scale
   // default as empty string so it falls back to value in constants.js
-  partnerColorRange:
-    window.configPartnerColorRange ??
-    process.env.REACT_APP_configPartnerColorRange ??
-    '',
+  partnerColorRange: window.configPartnerColorRange ?? '',
 
   // associations - heatmaps and facets
   // these work in the same way as the page specific sections.
   // hideDataTypes: list the id of any datatype to be hidden
   // partnerDataTypes: list any private datatypes (shown with lock in facets)
-  hideDataTypes:
-    window.configHideDataTypes ??
-    process.env.REACT_APP_configHideDataTypes ??
-    '',
-  partnerDataTypes:
-    window.configPartnerDataTypes ??
-    process.env.REACT_APP_configPartnerDataTypes ??
-    '',
+  hideDataTypes: window.configHideDataTypes ?? '',
+  partnerDataTypes: window.configPartnerDataTypes ?? '',
 
   // for datasources we only set those that are private (partner)
   // partnerDataSources: list any private datasource (shown with lock in facets)
-  partnerDataSources:
-    window.configPartnerDataSources ??
-    process.env.REACT_APP_configPartnerDataSources ??
-    '',
+  partnerDataSources: window.configPartnerDataSources ?? '',
 };
 
 export default config;
