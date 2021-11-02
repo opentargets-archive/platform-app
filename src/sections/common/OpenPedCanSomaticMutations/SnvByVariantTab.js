739612428f93922f59bc275d79065d25ddb36820
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2021-11-02 19:41:23 -0400

    rename to Total primary tumors mutated / Primary tumors in dataset

--- src/sections/common/OpenPedCanSomaticMutations/SnvByVariantTab.js
+++ src/sections/common/OpenPedCanSomaticMutations/SnvByVariantTab.js
@@ -35,7 +35,7 @@ const columns = [
     comparator: (row1, row2) => genericComparator(row1, row2, 'frequencyInOverallDataset', true) },
   { id: 'frequencyInOverallDataset', label: 'Frequency in overall dataset', sortable: true,
     comparator: (row1, row2) => genericComparator(row1, row2, 'frequencyInOverallDataset', true) },
-  { id: 'totalPrimaryTumorsMutatedOverPrimaryTumorsInDataset', label: 'Total primary tumors mutated Over Primary tumors in dataset', sortable: true,
+  { id: 'totalPrimaryTumorsMutatedOverPrimaryTumorsInDataset', label: 'Total primary tumors mutated / Primary tumors in dataset', sortable: true,
     comparator: (row1, row2) => genericComparator(row1, row2, 'frequencyInPrimaryTumors', true) },
   { id: 'frequencyInPrimaryTumors', label: 'Frequency in primary tumors', sortable: true,
     comparator: (row1, row2) => genericComparator(row1, row2, 'frequencyInPrimaryTumors', true) },
