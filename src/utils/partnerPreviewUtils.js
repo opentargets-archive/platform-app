cfa045119e23d69d8384d0177c435071493c40fe
Luca Fumis <lucaf@ebi.ac.uk>
2021-11-16 01:15:04 +0000

    Fix partners preview page utils

--- src/utils/partnerPreviewUtils.js
+++ src/utils/partnerPreviewUtils.js
@@ -2,16 +2,16 @@ import config from '../config';
 
 // page sections
 export const isPrivateTargetSection = id =>
-  config.profile.partnerTargetSectionIds.split(',').includes(id);
+  config.profile.partnerTargetSectionIds.includes(id);
 export const isPrivateDiseaseSection = id =>
   config.profile.partnerDiseaseSectionIds.includes(id);
 export const isPrivateDrugSection = id =>
-  config.profile.partnerDrugSectionIds.split(',').includes(id);
+  config.profile.partnerDrugSectionIds.includes(id);
 export const isPrivateEvidenceSection = id =>
-  config.profile.partnerEvidenceSectionIds.split(',').includes(id);
+  config.profile.partnerEvidenceSectionIds.includes(id);
 
 // associations
 export const isPrivateDataType = id =>
-  config.profile.partnerDataTypes.split(',').includes(id);
+  config.profile.partnerDataTypes.includes(id);
 export const isPrivateDataSource = id =>
-  config.profile.partnerDataSources.split(',').includes(id);
+  config.profile.partnerDataSources.includes(id);
