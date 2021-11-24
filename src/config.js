76b1c794c8e372ed9cabbf78a995ff8adaf1d2df
Luca Fumis <lucaf@ebi.ac.uk>
2021-11-24 01:15:25 +0000

    Remove tagmanager from original config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ const config = {
   urlApi:
     window.configUrlApi ??
     'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
-  googleTagManagerID: window.configGoogleTagManagerID ?? null,
   efoURL:
     window.configEFOURL ??
     'https://storage.googleapis.com/open-targets-data-releases/alpha-rewrite/static/ontology/diseases_efo.jsonl',
