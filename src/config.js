10997a2a6a4a3168ecba29623510963a01dc66a9
Luca Fumis <lucaf@ebi.ac.uk>
2021-11-24 01:29:01 +0000

    Remove efourl from config

--- src/config.js
+++ src/config.js
@@ -3,9 +3,6 @@ const config = {
   urlApi:
     window.configUrlApi ??
     'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
-  efoURL:
-    window.configEFOURL ??
-    'https://storage.googleapis.com/open-targets-data-releases/alpha-rewrite/static/ontology/diseases_efo.jsonl',
 
   profile: window.configProfile ?? {},
 };
