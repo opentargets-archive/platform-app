068fe4d947a1991495742cd1f5ec6e4dfa75bd2a
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2021-12-10 11:20:11 -0500

    retrieve summary request under target page

--- src/sections/target/OpenPedCanSomaticAlterations/Body.js
+++ src/sections/target/OpenPedCanSomaticAlterations/Body.js
@@ -3,10 +3,14 @@ import { loader } from 'graphql.macro';
 
 import { Body as OpenPedCanSomaticAlterationsBody} from '../../common/OpenPedCanSomaticAlterations';
 import Description from './Description';
+import usePlatformApi from '../../../hooks/usePlatformApi';
+import Summary from './Summary';
 
 const SOMATIC_ALTERATIONS_QUERY = loader('./SomaticAlterationsQuery.gql');
 
 function Body({ definition, id, label: symbol}) {
+  const summaryRequest = usePlatformApi(Summary.fragments.targetSomaticAlterationsSummary)
+  
   const variables = { ensemblId: id };
   const dataDownloaderFileStem = `OpenPedCanSomaticAlterations-${id}`
   return (
@@ -17,6 +21,7 @@ function Body({ definition, id, label: symbol}) {
       entity="target"
       variables={variables}
       BODY_QUERY={SOMATIC_ALTERATIONS_QUERY}
+      summaryRequest={summaryRequest}
       Description={Description}
       dataDownloaderFileStem={dataDownloaderFileStem}
 
