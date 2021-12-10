05fe0fcb394ed98c7d7644e6d9e17d43bf437062
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2021-12-10 09:28:13 -0500

    add helper function to get SA's default tab

--- src/sections/common/OpenPedCanSomaticAlterations/utils.js
+++ src/sections/common/OpenPedCanSomaticAlterations/utils.js
@@ -21,3 +21,20 @@ export const renderPMTLCell = (pmtlText) => {
   
 }
 
+// return a default Tab that has available data
+export const getSADefaultTab = (data) => {
+  let defaultTab = "snvByGene"
+  if (data) {
+    const {snvByGene, snvByVariant, cnvByGene, fusionByGene} = data;
+    if (snvByGene && snvByGene.count === 0 && snvByVariant.count !== 0) {
+      defaultTab = "snvByVariant"
+    } else if (snvByVariant && snvByVariant.count === 0 && cnvByGene.count !== 0) {
+      defaultTab = "cnvByGene"
+    } else if (cnvByGene && cnvByGene.count === 0 && fusionByGene.count !== 0) {
+      defaultTab = "fusionByGene"
+    } else if (fusionByGene && fusionByGene.count === 0) {
+      defaultTab = "fusion"
+    }
+  }
+  return defaultTab;
+}
