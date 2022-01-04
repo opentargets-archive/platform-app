40b0042e2e95a52378ff9fb8a862041a52f53cb9
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2022-01-04 11:37:08 -0500

    initial implementation of Pediatric Cancer Data Navigation

---
+++ src/components/Search/PedSearch.js
@@ -0,0 +1,208 @@
+import React, { useEffect, useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { loader } from 'graphql.macro';
+import {
+  Box,
+  CircularProgress,
+  Input,
+  InputBase,
+  makeStyles,
+} from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import { Search as SearchIcon, ArrowDropDown } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+
+import useDebounce from '../../hooks/useDebounce';
+import Option from './Option';
+import Group from './Group';
+
+const SEARCH_QUERY = loader('./SearchQuery.gql');
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    width: '100%',
+  },
+  containerEmbedded: {
+    minWidth: '447px',
+    [theme.breakpoints.only('sm')]: { minWidth: '48%' },
+    [theme.breakpoints.only('xs')]: { display: 'none' },
+  },
+  input: {
+    width: '100%',
+    paddingRight: '60px',
+  },
+  inputBase: {
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.text.secondary,
+    width: '100%',
+  },
+  inputBaseInput: { padding: '.25rem0- .5rem' },
+  listbox: { maxHeight: 'fit-content', color: theme.palette.text.primary },
+  option: { display: 'block', padding: '0 .5rem' },
+  root: {
+    width: '100%',
+  },
+}));
+
+function PedSearch({ autoFocus = false, embedded = false, inputValue='', setInputValue  }) {
+  const [open, setOpen] = React.useState(false);
+  const debouncedInputValue = useDebounce(inputValue, 300);
+  const [getData, { loading, data }] = useLazyQuery(SEARCH_QUERY, {
+    variables: { queryString: debouncedInputValue },
+    onCompleted: () => {},
+  });
+  const [searchResults, setSearchResults] = useState([]);
+  console.log("searchResults: ", searchResults)
+  let history = useHistory();
+
+  const handleChangeInputValue = e => {
+    if (!e.target.value) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+    setInputValue(e.target.value || '');
+  };
+
+  const handleSelectOption = (e, option, r) => {
+    handleChangeInputValue(e);
+
+
+    if (!option) return;
+
+    if (option.type === 'search') {
+      history.push(`/search?q=${option.name}&page=1`);
+      console.log("Input More character than ( ", option.name, " )")
+    } else {
+      console.log("")
+      history.push(
+        `/${option.entity}/${option.id}${
+          option.entity !== 'drug' ? '/associations' : ''
+        }`
+      );
+    }
+  };
+
+  useEffect(
+    () => {
+      if (debouncedInputValue) {
+        getData({ variables: { queryString: debouncedInputValue } });
+      } else {
+        setSearchResults([]);
+      }
+    },
+    [debouncedInputValue, getData]
+  );
+
+  useEffect(
+    () => {
+      const res = [];
+
+      if (inputValue) {
+        res.push({
+          type: 'search',
+          entity: 'any',
+          id: inputValue,
+          name: inputValue,
+        });
+      }
+
+      if (data) {
+        Object.keys(data).forEach(key =>
+          data[key].hits.map(i =>
+            res.push({
+              type: key === 'topHit' ? 'topHit' : 'normal',
+              entity: i.entity,
+              ...i.object,
+            })
+          )
+        );
+      }
+      setSearchResults(res);
+    },
+    [data, inputValue]
+  );
+
+  const classes = useStyles();
+
+  return (
+    <Box className={!embedded ? classes.container : classes.containerEmbedded}>
+      <Autocomplete
+        autoHighlight
+        freeSolo
+        forcePopupIcon
+        disablePortal
+        clearOnEscape={false}
+        classes={{
+          listbox: classes.listbox,
+          option: classes.option,
+          root: classes.root,
+        }}
+        filterOptions={(o, s) => searchResults}
+        getOptionLabel={option => (option.id ? option.id : option)}
+        getOptionSelected={(option, value) => option.id === value}
+        groupBy={option =>
+          option.type === 'topHit' ? 'topHit' : option.entity
+        }
+        loading={loading}
+        noOptionsText="No results"
+        options={searchResults}
+        onChange={handleSelectOption}
+        onOpen={() => {
+          if (inputValue) setOpen(true);
+        }}
+        onClose={() => {
+          setOpen(false);
+        }}
+        open={open}
+        popupIcon={open ? <ArrowDropDown /> : <SearchIcon />}
+        renderOption={option => <Option data={option} />}
+        renderGroup={group => (
+          <Group key={group.key} name={group.group} children={group.children} />
+        )}
+        renderInput={params =>
+          !embedded ? (
+            <Input
+              className={classes.input}
+              inputProps={params.inputProps}
+              ref={params.InputProps.ref}
+              endAdornment={
+                loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : (
+                  params.InputProps.endAdornment
+                )
+              }
+              placeholder=" Search ..."
+              onChange={handleChangeInputValue}
+              value={inputValue}
+            />
+          ) : (
+            <InputBase
+              autoFocus={autoFocus}
+              classes={{
+                root: classes.inputBase,
+                input: classes.inputBaseInput,
+              }}
+              inputProps={params.inputProps}
+              ref={params.InputProps.ref}
+              endAdornment={
+                loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : (
+                  params.InputProps.endAdornment
+                )
+              }
+              placeholder="Search..."
+              onChange={handleChangeInputValue}
+              value={inputValue}
+            />
+          )
+        }
+        value={inputValue}
+      />
+    </Box>)
+
+}
+
+export default PedSearch;
--- src/pages/PedCancerDataNavPage/CHOPPage.js
+++ src/pages/PedCancerDataNavPage/CHOPPage.js
@@ -1,13 +1,294 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import View from './View';
+import { initial } from 'lodash-es';
+import CHOPTable from '../../components/RMTLTable';
+
+import { Grid, Paper, Box, Typography, Button, TextField } from '@material-ui/core';
+import BasePage from '../../components/BasePage';
+import Link from '../../components/Link';
+import PedSearch from '../../components/Search/PedSearch';
+
+import {Check as CheckIcon, CropLandscapeOutlined} from '@material-ui/icons';
 
 const CHoP_CONTENT_URL = "https://gl.githack.com/yizhenchen/dummy-data/-/raw/main/chopDataNavigationTable_SA_GX.json";
+// const {Renderr, debouncedInputValue} = PedSearch;
+// console.log("debouncedInputValue: ", debouncedInputValue)
+const intitalData = [{
+  "targetFromSourceId": "ENSG00000000003",
+  "diseaseFromSourceMappedId": "EFO_0000174",
+  "Gene_symbol": "TSPAN6",
+  "Disease": "Ewing sarcoma",
+  "SNV": false,
+  "CNV": true,
+  "Fusion": false,
+  "GeneExpression": true
+},
+{
+  "targetFromSourceId": "ENSG00000000003",
+  "diseaseFromSourceMappedId": "EFO_0000220",
+  "Gene_symbol": "TSPAN6",
+  "Disease": "Acute Lymphoblastic Leukemia",
+  "SNV": true,
+  "CNV": true,
+  "Fusion": false,
+  "GeneExpression": true
+},
+{
+  "targetFromSourceId": "ENSG00000000003",
+  "diseaseFromSourceMappedId": "EFO_0000222",
+  "Gene_symbol": "TSPAN6",
+  "Disease": "Acute Myeloid Leukemia",
+  "SNV": false,
+  "CNV": true,
+  "Fusion": false,
+  "GeneExpression": true
+},
+{
+  "targetFromSourceId": "ENSG00000000003",
+  "diseaseFromSourceMappedId": "EFO_0000232",
+  "Gene_symbol": "TSPAN6",
+  "Disease": "Adenoma",
+  "SNV": false,
+  "CNV": false,
+  "Fusion": false,
+  "GeneExpression": true
+},
+{
+  "targetFromSourceId": "ENSG00000000003",
+  "diseaseFromSourceMappedId": "EFO_0000350",
+  "Gene_symbol": "TSPAN6",
+  "Disease": "Clear cell sarcoma of the kidney",
+  "SNV": false,
+  "CNV": false,
+  "Fusion": false,
+  "GeneExpression": true
+},
+{
+  "targetFromSourceId": "ENSG00000000003",
+  "diseaseFromSourceMappedId": "EFO_0000514",
+  "Gene_symbol": "TSPAN6",
+  "Disease": "Germinoma",
+  "SNV": false,
+  "CNV": false,
+  "Fusion": false,
+  "GeneExpression": true
+},
+{
+  "targetFromSourceId": "ENSG00000000003",
+  "diseaseFromSourceMappedId": "EFO_0000621",
+  "Gene_symbol": "TSPAN6",
+  "Disease": "Neuroblastoma",
+  "SNV": true,
+  "CNV": true,
+  "Fusion": false,
+  "GeneExpression": true
+}]
+
+/*
+ * genericComparator: comparing row1 and row2 using the input keyName.
+ * return: -1 if first string is lexicographically less than second property
+ *          1 if first string is lexicographically greater than second property
+ *          0 if both property are equal
+ */
+function genericComparator(row1, row2, keyName) {
+  const a =
+    typeof row1[keyName] === 'string'
+      ? row1[keyName].toLowerCase()
+      : row1[keyName];
+  const b =
+    typeof row2[keyName] === 'string'
+      ? row2[keyName].toLowerCase()
+      : row2[keyName];
+
+  return a < b ? -1 : a > b ? 1 : 0;
+}
+
+const columns = [
+  {
+    id: 'geneSymbol',
+    label: 'Gene symbol',
+    renderCell: row => {
+      const ensemblID = row.geneEnsemblId;
+      const url = '/target/' + ensemblID;
+      return ensemblID !== "Symbol_Not_Found" ? 
+      ( <Link to={url}>{row.geneSymbol}</Link>):
+       (<p> {row.geneSymbol} </p>)
+    },
+    
+    comparator: (a, b) => genericComparator(a, b, 'geneSymbol'),
+  },
+  {
+    id: 'Disease',
+    label: 'Disease',
+    renderCell: ({ EFO, Disease }) => 
+      <Link to={`/disease/${EFO}`}>{Disease}</Link>,
+    comparator: (a, b) => genericComparator(a, b, 'Disease'),
+  },
+  {
+    id: 'geneEnsemblId',
+    label: 'Evidence',
+    renderCell: ({ geneEnsemblId, EFO }) => 
+      <Link to={`/evidence/${geneEnsemblId}/${EFO}`} external>Evidence Page</Link>,
+  },
+  {
+    id: 'SNV',
+    label: 'SNV',
+    renderCell: ({SNV}) => SNV === "true" ? <CheckIcon style={{color:"Green"}}/>: '',
+    comparator: (a, b) => genericComparator(a, b, 'SNV'),
+  },
+  {
+    id: 'CNV',
+    label: 'CNV',
+    renderCell: ({CNV}) => CNV === "true" ? <CheckIcon style={{color:"Green"}}/>: '',
+    comparator: (a, b) => genericComparator(a, b, 'CNV'),
+  },
+  {
+    id: 'Fusion',
+    label: 'Fusion',
+    renderCell: ({Fusion}) => Fusion === "true" ? <CheckIcon style={{color:"Green"}}/>: '',
+    comparator: (a, b) => genericComparator(a, b, 'Fusion'),
+  },
+  {
+    id: 'GeneExpression',
+    label: 'Gene Expression',
+    renderCell: ({GeneExpression}) => GeneExpression === "true" ? <CheckIcon style={{color:"Green"}}/>: '',
+    comparator: (a, b) => genericComparator(a, b, 'GeneExpression'),
+  },
+];
+
+function handleRowsPerPageChange(newPageSize){
+  this.setState({ pageSize: newPageSize });
+};
+
+function getRows(downloadData) {
+  const rows = [];
+  downloadData.forEach(mapping => {
+    rows.push({
+      geneEnsemblId: mapping.targetFromSourceId,
+      EFO: mapping.diseaseFromSourceMappedId,
+      geneSymbol: mapping.Gene_symbol,
+      Disease: mapping.Disease,
+      SNV: mapping.SNV + "",
+      CNV: mapping.CNV + "",
+      Fusion: mapping.Fusion + "",
+      GeneExpression: mapping.GeneExpression + "",
+    });
+  });
+  return rows;
+}
+
+const pageSize = 25;
+const rowsPerPageOptions = [10, 25, 50];
+
 
 function CHoPPage() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(getRows(intitalData));
+  const [targetInputValue, setTargetInputValue] = useState('');
+  const [diseaseInputValue, setDiseaseInputValue] = useState('');
+
+  const [searchButton, setSearchButton] = useState(false)
+
+
+  console.log("targetInputValue: ", targetInputValue)
+  console.log("diseaseInputValue", diseaseInputValue)
+
+  const handleOnClick = e => {
+    console.log("I am hereeeeeee")
+    if (targetInputValue.length > 0 || diseaseInputValue.length > 0) {
+      setSearchButton(true)
+    }
+  }
+
+  return (
+    <BasePage title="Pediatric Cancer Data Navigation" >
+      <Typography component="p" paragraph>
+        (This page is under active development)
+      </Typography>
+
+      <Typography variant="h5" component="h1" paragraph>
+        Pediatric Cancer Data Navigation
+      </Typography>
+    
+
+      <br />
+      <Typography component="p" paragraph>
+        Search for a <b>Target</b>, <b>Disease</b>, or <b>both</b> to find detailed evidence pages with pediatric cancer data within the Molecular Targets Platform.
+      </Typography>
+
+      <Grid container direction="row">
+        <Grid container item direction="row" justifyContent="center" alignItems="center" spacing={0} style={{maxWidth: '447px', borderRadius: '10px', backgroundColor: '#3489ca', padding: '5px', border: 'solid 1px black'}}>
+        
+          <Grid item style={{marginRight: '15px', color: 'white'}}>
+            Target:  
+          </Grid>
+          <Grid item xs={10} style={{backgroundColor: 'white'}}>
+            <PedSearch inputValue={targetInputValue} setInputValue={setTargetInputValue}
+              />
+          </Grid>
+        </Grid>
+
+        <Grid item xs={1} style={{minWidth: '50px', minHeight: '50px'}}></Grid>
+
+        <Grid container item direction="row" justifyContent="center" alignItems="center" spacing={0} style={{maxWidth: '447px', borderRadius: '10px', backgroundColor: '#3489ca', padding: '5px', border: 'solid 1px black'}}>
+        
+          <Grid item style={{marginRight: '15px', color: 'white'}}>
+            Disease:  
+          </Grid>
+          <Grid item xs={10} style={{backgroundColor: 'white'}}>
+            <PedSearch inputValue={diseaseInputValue} setInputValue={setDiseaseInputValue}
+             />
+          </Grid>
+        </Grid>
+      </Grid> <br/><br/>
+      <Grid container  direction="row"
+      justifyContent="center"
+      alignItems="center">
+        <Grid item >
+          <Button onClick={handleOnClick} variant="contained" style={{}} disabled={targetInputValue.length===0 && diseaseInputValue.length===0} >Search</Button>
+        </Grid>
+      </Grid>
+
+      <Grid container>
+        <Grid item >
+          <Typography variant='p'>
+          Found 37 Diseases with ALKpediatric cancer evidence data. Note that  the existence of data does not necessarily indicate significance.
+          </Typography>
+        </Grid>
+      </Grid>
+     
+        
+      <hr />
+      <br />
+      {searchButton ?
+      <Paper variant="outlined" elevation={0}>
+        <Box m={2}>
+          {(
+            <>
+              <CHOPTable
+                columns={columns}
+                data={data}
+                pageSize={pageSize}
+                onRowsPerPageChange={handleRowsPerPageChange}
+                rowsPerPageOptions={rowsPerPageOptions}
+              />
+            </>
+          )}
+        </Box>
+      </Paper>
+        : <></>}
+    </BasePage>
+    
+  )
+}
+export default CHoPPage;
+
+
+
+/* function CHoPPage() {
+  const [data, setData] = useState(intitalData);
  
-  useEffect(() => {
+   useEffect(() => {
     const fetchData = async () => {
       let resultData = [];
       let result = [];
@@ -24,6 +305,4 @@ function CHoPPage() {
     fetchData();
   },[data.length]);
 
-  return <View data={data} /> ;
-}
-export default CHoPPage;
+  return <View data={data} /> };*/
