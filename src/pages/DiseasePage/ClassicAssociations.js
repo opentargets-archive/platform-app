ebec1e67eb819a652424321f968eab9358f1c1cf
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2022-01-25 16:25:26 -0500

    Add URL to "Pediatric Cancer Data Navigation" page under Disease page

--- src/pages/DiseasePage/ClassicAssociations.js
+++ src/pages/DiseasePage/ClassicAssociations.js
@@ -5,6 +5,7 @@ import { loader } from 'graphql.macro';
 
 import ClassicAssociationsTable from './ClassicAssociationsTable';
 import { Facets } from '../../components/Facets';
+import Link from '../../components/Link';
 
 const DISEASE_FACETS_QUERY = loader('./DiseaseFacets.gql');
 
@@ -22,7 +23,7 @@ function ClassicAssociations({ efoId, name }) {
 
   return (
     <Grid style={{ marginTop: '8px' }} container spacing={2}>
-      <Grid item xs={12}>
+      <Grid item xs={12} md={4}>
         <Typography variant="h6">
           {data ? (
             <>
@@ -34,6 +35,28 @@ function ClassicAssociations({ efoId, name }) {
           )}
         </Typography>
       </Grid>
+      <Grid item xs={12} md={8}>
+        <Typography variant="h6">
+          {data ? (
+            <>
+            <span style={{fontSize: '15px'}}>Pediatric Cancer Data is not yet part of these results. Please use </span> {' '} 
+              <Link to={{
+                pathname: "/pediatric-cancer-data-navigation",
+                state: {
+                  entity: 'disease',
+                  'disease': name
+                }
+              }}>
+                <span style={{fontSize: '16px'}}>Pediatric Cancer Data Navigation</span>
+              </Link> {' '}
+              <span style={{fontSize: '15px'}}>to search for pediatric cancer data.</span>
+            </>
+          ) : (
+            <></>
+          )}
+        </Typography>
+      </Grid>{' '}
+
       <Grid item xs={12} lg={3}>
         <Card elevation={0}>
           <CardContent>
