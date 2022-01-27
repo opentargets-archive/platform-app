5a49fe916df22cbd0426fc44090b945f41f8fabe
Luca Fumis <lucaf@ebi.ac.uk>
2022-01-27 00:55:11 +0000

    Update mock data

--- src/sections/evidence/OTValidation/data/sample.js
+++ src/sections/evidence/OTValidation/data/sample.js
@@ -1,21 +1,5 @@
 const sample = {
   rows: [
-    // {
-    //   disease: {
-    //     id: '123',
-    //     name: 'colorectal carcinoma',
-    //   },
-    //   project: 'CRISPR Cas9 Target ID OTAR015',
-    //   contrast: 'loss of cell viability vs. control?',
-    //   cellLine: 'ABC1',
-    //   biomarkers: ['APC', 'CRIS', 'MSI', 'TP53', 'KRAS'],
-    //   size: 65.12,
-    //   hit: true,
-    //   projectHit: true,
-    //   observation: true,
-    //   hypothesis: 'MSI',
-    // },
-
     {
       datasourceId: 'ot_crispr_validation',
       dataTypeId: 'ot_validation_lab',
