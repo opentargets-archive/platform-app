e09c79372a88813d4bf66212e92b8f9556582b56
Luca Fumis <lucaf@ebi.ac.uk>
2022-02-21 23:45:52 +0000

    Update validation lab summary

--- src/sections/evidence/OTValidation/Summary.js
+++ src/sections/evidence/OTValidation/Summary.js
@@ -3,32 +3,19 @@ import { loader } from 'graphql.macro';
 import usePlatformApi from '../../../hooks/usePlatformApi';
 import SummaryItem from '../../../components/Summary/SummaryItem';
 import { dataTypesMap } from '../../../dataTypes';
-// TODO: remove mock data
-import sample from './data/sample';
 
 const OT_VALIDATION_SUMMARY = loader('./OTValidationSummary.gql');
 
 function Summary({ definition }) {
   const request = usePlatformApi(OT_VALIDATION_SUMMARY);
-  // TODO: remove mock data
-  const tmpCount = sample.rows.length;
-  // return (
-  //   <SummaryItem
-  //     definition={definition}
-  //     request={request}
-  //     renderSummary={({ otValidationSummary }) => {
-  //       const { count } = otValidationSummary;
-  //       return `${tmpCount} ${tmpCount === 1 ? 'entry' : 'entries'}`;
-  //     }}
-  //     subText={dataTypesMap.ot_validation_lab}
-  //   />
-  // );
+
   return (
     <SummaryItem
       definition={definition}
       request={request}
-      renderSummary={() => {
-        return `${tmpCount} ${tmpCount === 1 ? 'entry' : 'entries'}`;
+      renderSummary={({ otValidationSummary }) => {
+        const { count } = otValidationSummary;
+        return `${count} ${count === 1 ? 'entry' : 'entries'}`;
       }}
       subText={dataTypesMap.ot_validation_lab}
     />
