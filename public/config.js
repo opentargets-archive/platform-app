457226f98d131d364d4dcfaa28d0de9a5b484a49
Luca Fumis <lucaf@ebi.ac.uk>
2022-02-22 17:17:15 +0000

    Update config for testing only

--- public/config.js
+++ public/config.js
@@ -1,1 +1,4 @@
 // Default configuration is defined within the application bundle
+// TODO: remove this after preview
+var configUrlApi =
+  'https://api.platform.dp22023.opentargets.xyz/api/v4/graphql';
