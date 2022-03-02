020ef9b8b14147a06550dad094d2db78d589bb3d
Luca Fumis <lucaf@ebi.ac.uk>
2022-03-02 17:03:03 +0000

    Fix hasData function

--- src/sections/evidence/OTValidation/index.js
+++ src/sections/evidence/OTValidation/index.js
@@ -5,7 +5,7 @@ export const definition = {
   id: id,
   name: 'Open Targets Validation CRISPR',
   shortName: 'VL',
-  hasData: () => ({ otValidationSummary }) => otValidationSummary.count > 0,
+  hasData: ({ otValidationSummary }) => otValidationSummary.count > 0,
   isPrivate: isPrivateEvidenceSection(id),
 };
 
