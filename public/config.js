00f647393365afa6017c42bb1f75171d1ce8b9e5
Luca Fumis <lucaf@ebi.ac.uk>
2022-03-03 17:20:51 +0000

    Revert config to defaults

--- public/config.js
+++ public/config.js
@@ -1,4 +1,1 @@
 // Default configuration is defined within the application bundle
-// TODO: remove this after preview
-var configUrlApi =
-  'https://api.platform.dp22023.opentargets.xyz/api/v4/graphql';
