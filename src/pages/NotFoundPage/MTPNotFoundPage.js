ea183575be69893544d677fa2a3bc90b9df908cf
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2022-03-11 10:20:23 -0500

    implement MTP Not Found Page

--- src/pages/NotFoundPage/MTPNotFoundPage.js
+++ src/pages/NotFoundPage/MTPNotFoundPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { makeStyles, Grid } from '@material-ui/core';
+import { makeStyles, Grid, Typography } from '@material-ui/core';
 
 import NCIHeader from '../../components/NCIHeader';
 import NCIFooter from '../../components/NCIFooter'
 import { appTitle, appDescription, appCanonicalUrl } from '../../constants';
+import cn from '../../components/helpers/classNameConcat';
+
+const blurRadius = 5
+const shadowColor = '#035BA2'
 
 const styles = makeStyles(theme => ({
   page: {
-    backgroundColor: theme.palette.grey['50'],
+    // Fall Back background color
+    backgroundColor: '#6bb1e7',
+    // Background color
+    background: 'linear-gradient(to bottom right,#6bb1e7,#67cbda)',
     minHeight: '100vh',
     display: 'flex',
     flexDirection: 'column',
@@ -16,11 +23,68 @@ const styles = makeStyles(theme => ({
     width: '100%',
   },
   gridContainer: {
-    margin: '260px 0 0 0',
+    margin: '240px 0 0 0',
     padding: '24px',
     width: '100%',
     flex: '1 0 auto',
   },
+  four0four: {
+    color: '#E0F4FC',
+    fontFamily: 'Inter',
+    fontSize: '400px',
+    fontWeight: '600',
+    lineHeight: '400px',
+    textShadow: `1px 1px ${blurRadius}px ${shadowColor},
+                -1px -1px ${blurRadius}px ${shadowColor},
+                1px -1px ${blurRadius}px ${shadowColor}, 
+                -1px 1px ${blurRadius}px ${shadowColor},
+                0px 0px ${blurRadius}px ${shadowColor}`
+  },
+  fFour: {
+    color: '#E0F4FC',
+  },
+  zero: {
+    color: '#A3E0FF',
+  },
+  lFour: {
+    color: '#6EBEFF',
+  },
+  pageNotFound: {
+    fontSize: '35px',
+    fontWeight: '600',
+    lineHeight: '45px',
+    color: '#00344F',
+    marginTop: '8px'
+  },
+  pageNotFoundInfo: {
+    fontSize: '16px',
+    lineHeight: '25px',
+    color: '#00344F',
+    marginTop: '25px',
+    marginBottom: '186px'
+  },
+  "@media (max-width: 900px)": {
+    four0four: {
+      fontSize: '250px',
+      lineHeight: '250px',
+    },
+    pageNotFound: {
+      fontSize: '28px',
+      fontWeight: '600',
+      lineHeight: '45px',
+    },
+  },
+  "@media (max-width: 650px)": {
+    four0four: {
+      fontSize: '150px',
+      lineHeight: '150px',
+    },
+    pageNotFound: {
+      fontSize: '20px',
+      fontWeight: '600',
+      lineHeight: '45px',
+    },
+  }
 }));
 const MTPNotFoundPage = ({location}) => {
   const classes = styles()
@@ -41,7 +105,20 @@ const MTPNotFoundPage = ({location}) => {
               href={appCanonicalUrl + (location || '')}
             />
           </Helmet>
-          {/* Body Code here ... */}
+          <Typography align='center'>
+            <span className={cn(classes.four0four, classes.fFour)}>4</span>
+            <span className={cn(classes.four0four, classes.zero)}>0</span>
+            <span className={cn(classes.four0four, classes.lFour)}>4</span>
+        
+            <p className={classes.pageNotFound}>
+              Page not found.
+            </p>
+
+            <p className={classes.pageNotFoundInfo}>
+              The page you are looking for does not exist or another error has occurred. <br/>
+              Go back or head <b>home</b> to choose another direction.
+            </p>
+          </Typography>
         </Grid>
       </Grid>
       <NCIFooter/>
