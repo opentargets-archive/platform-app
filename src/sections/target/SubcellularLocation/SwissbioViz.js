a4ee5c590d8dced439d9cdef2cf5b7ae06598b7d
Luca Fumis <lucaf@ebi.ac.uk>
2022-04-03 10:30:57 +0100

    Initial wrapper for the swissbiopic widget

---
+++ src/sections/target/SubcellularLocation/SwissbioViz.js
@@ -0,0 +1,78 @@
+import React, { useRef, useEffect } from 'react';
+// import { useQuery } from '@apollo/client';
+// import { loader } from 'graphql.macro';
+// import { Typography, List, ListItem, Box } from '@material-ui/core';
+
+// import Description from './Description';
+// import SectionItem from '../../../components/Section/SectionItem';
+// import Link from '../../../components/Link';
+// import { identifiersOrgLink, getUniprotIds } from '../../../utils/global';
+
+import '@swissprot/swissbiopics-visualizer';
+
+const canonicalName = 'sib-swissbiopics-sl';
+const CanonicalDefinition = customElements.get(canonicalName);
+
+/**
+ * Visualization for the SwissBioPic widget.
+ * This is based on Uniprot's approach
+ */
+function SwissbioViz({ uniProtLocations, goLocations, taxonId, children }) {
+  /*
+
+  <template id="sibSwissBioPicsSlLiItem">
+            <li className="subcellular_location">
+                <a className="subcell_name"></a>
+                <span className="subcell_description"></span>
+            </li>
+          </template>
+
+          <template id="sibSwissBioPicsStyle">
+      <style>
+          ul > li > a {
+              font-style:oblique;
+          }
+          ul.notpresent li > .subcell_description {
+              display:none;
+          }
+      </style>
+    </template>
+    
+          <sib-swissbiopics-sl taxid="9606" sls="SL0073"></sib-swissbiopics-sl>
+
+
+        
+        <template id="sibSwissBioPicsStyle" />
+        <div id="fakeContent" />
+        <Instance taxid={taxonId} contentid="fakeContent" {...locationIds}>
+          {children}
+        </Instance>
+
+   */
+  const instanceName = useRef(canonicalName + '-go');
+  const uniProtLocationIds = uniProtLocations?.map(({ id }) => id).join(',');
+  const goLocationIds = goLocations; //goLocations?.map(({ id }) => id).join(',');
+
+  useEffect(
+    () => {
+      class InstanceClass extends CanonicalDefinition {
+        constructor() {
+          super();
+        }
+      }
+      customElements.define(instanceName.current, InstanceClass);
+    },
+    [uniProtLocationIds, uniProtLocations, goLocationIds, goLocations]
+  );
+  const Instance = props => <instanceName.current {...props} />;
+  return (
+    <>
+      <template id="sibSwissBioPicsStyle" />
+      <div id="fakeContent" />
+
+      <Instance taxid={taxonId} contentid="fakeContent" gos={goLocationIds} />
+    </>
+  );
+}
+
+export default SwissbioViz;
