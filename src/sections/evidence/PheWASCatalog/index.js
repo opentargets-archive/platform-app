5e303b6243f041886b626940dc51f6d9feaccf74
carcruz <me@carcruzcast.com>
2022-04-13 00:04:27 +0100

    delete: remove PheWASCatalog section files

--- src/sections/evidence/PheWASCatalog/index.js
+++
@@ -1,9 +0,0 @@
-export const definition = {
-  id: 'phewasCatalog',
-  name: 'PheWAS Catalog',
-  shortName: 'PC',
-  hasData: data => data.phewasCatalogSummary.count > 0,
-};
-
-export { default as Summary } from './Summary';
-export { default as Body } from './Body';
