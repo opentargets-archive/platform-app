3640c6641e5dc60af19566469cdb072d6d57e898
Ricardo Martinez <remo87@gmail.com>
2022-04-14 09:40:14 -0500

    Split variantRsId column Add variantHgvsId column

--- src/sections/evidence/EVASomatic/Body.js
+++ src/sections/evidence/EVASomatic/Body.js
@@ -72,34 +72,46 @@ const columns = [
     },
   },
   {
-    id: 'variantRsId',
-    label: 'Variant ID (RSID)',
-    renderCell: ({ variantId, variantRsId }) => {
+    id: 'variantId',
+    label: 'Variant ID',
+    renderCell: ({ variantId }) => {
       return variantId ? (
         <>
           {variantId.substring(0, 20)}
           {variantId.length > 20 ? '\u2026' : ''}
-          {variantRsId ? (
-            <>
-              {' '}
-              (
-              <Link
-                external
-                to={`http://www.ensembl.org/Homo_sapiens/Variation/Explore?v=${variantRsId}`}
-              >
-                {variantRsId}
-              </Link>
-              )
-            </>
-          ) : (
-            ''
-          )}
         </>
       ) : (
         naLabel
       );
     },
-    filterValue: ({ variantId, variantRsId }) => `${variantId} ${variantRsId}`,
+    filterValue: ({ variantId }) => `${variantId}`,
+  },
+  {
+    id: 'variantRsId',
+    label: 'rsID',
+    renderCell: ({ variantRsId }) => {
+      return variantRsId ? (
+        <>
+          <Link
+            external
+            to={`http://www.ensembl.org/Homo_sapiens/Variation/Explore?v=${variantRsId}`}
+          >
+            {variantRsId}
+          </Link>
+        </>
+      ) : (
+        naLabel
+      );
+    },
+    filterValue: ({ variantRsId }) => `${variantRsId}`,
+  },
+  {
+    id: 'variantHgvsId',
+    label: 'HGVS ID',
+    renderCell: ({ variantHgvsId }) => {
+      return variantHgvsId ? variantHgvsId : naLabel;
+    },
+    filterValue: ({ variantHgvsId }) => `${variantHgvsId}`,
   },
   {
     id: 'studyId',
