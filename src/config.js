c0968e40e81b1e317583f52faa8873f969cecbe8
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2022-04-21 15:14:54 -0400

    update config file

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@
 const config = {
   urlApi:
     window.injectedEnv.configUrlApi ??
-    'https://ppdc-otp-dev.bento-tools.org/api/v4/graphql',
+    'https://api.platform.opentargets.org/api/v4/graphql',
   urlApiBeta:
     window.injectedEnv.configUrlApiBeta ??
     'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql',
   googleTagManagerID: window.injectedEnv.configGoogleTagManagerID ?? null,
   efoURL:
     window.injectedEnv.configEFOURL ??
-    'https://storage.googleapis.com/open-targets-data-releases/alpha-rewrite/static/ontology/diseases_efo.jsonl',
+    'https://platform.opentargets.org/data/ontology/efo_json/diseases_efo.jsonl',
   primaryColor: window.injectedEnv.configPrimaryColor ?? '#3489ca',
   chopRServer: window.injectedEnv.chopRServer ?? 'https://openpedcan-api.d3b.io',
   flagShowOTARProjects: window.injectedEnv.configFlagShowOTARProjects ?? false,
