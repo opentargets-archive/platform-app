4986f19d047cc40a8c3b01c5c9d8226716088cec
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2022-04-21 14:15:09 -0400

    remove import alias

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link as ReactRouterLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
