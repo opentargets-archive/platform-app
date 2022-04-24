51b6056534f9f2e79d23318a437f64bd5c0aaffe
Luca Fumis <lucaf@ebi.ac.uk>
2022-04-24 23:42:33 +0100

    Update description links

--- src/sections/evidence/GeneBurden/Description.js
+++ src/sections/evidence/GeneBurden/Description.js
@@ -2,16 +2,15 @@ import React from 'react';
 import Link from '../../../components/Link';
 
 function Description({ symbol, diseaseName }) {
-  // TODO: update sources links
   return (
     <>
       Gene burden analysis prioritising <strong>{symbol}</strong> as likely
       causal gene for <strong>{diseaseName}</strong>. Source:{' '}
-      <Link external to="http://home.opentargets.org/">
+      <Link external to="https://www.ebi.ac.uk/gwas/">
         GWAS Catalog
       </Link>
       ,{' '}
-      <Link external to="http://home.opentargets.org/">
+      <Link external to="https://azphewas.com">
         AstraZeneca PheWAS Portal
       </Link>
       .
