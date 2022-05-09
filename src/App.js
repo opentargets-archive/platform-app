be90a88418d83f47b804599bd68e56736acd529c
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2022-05-09 10:28:41 -0400

    add 'PlatformApiProvider' to provide context to all component (from PPDC-563)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import OtUiThemeProvider from './components/OtUiThemeProvider';
+import PlatformApiProvider from './contexts/PlatformApiProvider';
 import client from './client';
 import initLocalStorage from './utils/initLocalStorage';
 import theme from './theme';
+import { globalQuery } from './GlobalQuery';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import DiseasePage from './pages/DiseasePage';
@@ -29,24 +31,26 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <OtUiThemeProvider theme={theme}>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route path="/search" component={SearchPage} />
-              <Route path="/downloads" component={DownloadsPage} />
-              <Route path="/disease/:efoId" component={DiseasePage} />
-              <Route path="/target/:ensgId" component={TargetPage} />
-              <Route path="/drug/:chemblId" component={DrugPage} />
-              <Route path="/evidence/:ensgId/:efoId" component={EvidencePage} />
-              <Route path="/variants" component={VariantsPage} />
-              <Route path="/api" component={APIPage} />
-              <Route path="/About" component={AboutPage} />
-              <Route path="/fda-pmtl" component={PMTLPage} />
-              <Route path="/fda-pmtl-docs" component={PMTLDocPage} />
-              <Route path="/pediatric-cancer-data-navigation" component={PedCancerDataNavPage} />
-              <Route component={NotFoundPage} />
-            </Switch>
-          </Router>
+          <PlatformApiProvider query={globalQuery}>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/search" component={SearchPage} />
+                <Route path="/downloads" component={DownloadsPage} />
+                <Route path="/disease/:efoId" component={DiseasePage} />
+                <Route path="/target/:ensgId" component={TargetPage} />
+                <Route path="/drug/:chemblId" component={DrugPage} />
+                <Route path="/evidence/:ensgId/:efoId" component={EvidencePage} />
+                <Route path="/variants" component={VariantsPage} />
+                <Route path="/api" component={APIPage} />
+                <Route path="/About" component={AboutPage} />
+                <Route path="/fda-pmtl" component={PMTLPage} />
+                <Route path="/fda-pmtl-docs" component={PMTLDocPage} />
+                <Route path="/pediatric-cancer-data-navigation" component={PedCancerDataNavPage} />
+                <Route component={NotFoundPage} />
+              </Switch>
+            </Router>
+          </PlatformApiProvider>
         </OtUiThemeProvider>
       </ApolloProvider>
     );
