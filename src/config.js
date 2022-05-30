644700a5e80e8d0a24c1ba0699986d6729b997e1
carcruz <me@carcruzcast.com>
2022-05-30 12:01:19 +0100

    fix: efo static asset URL

--- src/config.js
+++ src/config.js
@@ -4,9 +4,7 @@ const config = {
     window.configUrlApi ??
     'https://api.platform.dev.opentargets.xyz/api/v4/graphql',
   googleTagManagerID: window.configGoogleTagManagerID ?? null,
-  efoURL:
-    window.configEFOURL ??
-    'https://platform.dev.opentargets.xyz/data/ontology/efo_json/diseases_efo.jsonl',
+  efoURL: window.configEFOURL ?? '/data/ontology/efo_json/diseases_efo.jsonl',
 
   profile: window.configProfile ?? {},
   downloadsURL: window.configDownloadsURL ?? '/data/downloads.json',
