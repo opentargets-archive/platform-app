4a651b15318acd956aefa9c5d619398cedb98989
Luca Fumis <lucaf@ebi.ac.uk>
2022-06-07 18:32:20 +0100

    Clean up body component

--- src/sections/target/SubcellularLocation/Body.js
+++ src/sections/target/SubcellularLocation/Body.js
@@ -1,77 +1,12 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/client';
 import { loader } from 'graphql.macro';
-import { Typography, List, ListItem, Box, makeStyles } from '@material-ui/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 import Description from './Description';
 import SectionItem from '../../../components/Section/SectionItem';
-import Link from '../../../components/Link';
-import { identifiersOrgLink, getUniprotIds } from '../../../utils/global';
+import SubcellularViz from './SubcellularViz';
 
 const SUBCELLULAR_LOCATION_QUERY = loader('./SubcellularLocation.gql');
-const SwissbioViz = lazy(() => import('./SwissbioViz'));
-
-const useStyles = makeStyles(theme => ({
-  locationIcon: {
-    paddingRight: '0.5em',
-  },
-  locationsList: {
-    cursor: 'pointer',
-    fontWeight: 'bold',
-    color: theme.palette.primary.main,
-    '& .inpicture.lookedAt': {
-      color: theme.palette.primary.dark,
-    },
-  },
-}));
-
-const sources = [
-  {
-    id: 'HPA_main',
-    label: 'HPA main location',
-  },
-  {
-    id: 'HPA_additional',
-    label: 'HPA additional location',
-  },
-  {
-    id: 'HPA_extracellular_location',
-    label: 'HPA extracellular location',
-  },
-  {
-    id: 'uniprot',
-    label: 'UniProt',
-  },
-];
-
-// Remove the 'SL-' from a location termSL (e.g. "SL-0097")
-// The sib-swissbiopics component (different from what is documented)
-// actually doesn't accept the "SL-" part of the term
-const parseLocationTerm = term => term?.substring(3);
-
-// Parse termSL to specific id format used by the text for rollovers
-const parseTermToTextId = term => (term ? `${term.replace('-', '')}term` : '');
-
-const LocationsList = ({ title, sls }) => {
-  const classes = useStyles();
-  return (
-    <>
-      {title ? <Typography>{title}</Typography> : null}
-      <List className={classes.locationsList}>
-        {sls.map(({ location, termSL }) => (
-          <ListItem key={location} id={parseTermToTextId(termSL)}>
-            <span className={classes.locationIcon}>
-              <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" />
-            </span>
-            {location}
-          </ListItem>
-        ))}
-      </List>
-    </>
-  );
-};
 
 function Body({ definition, id: ensemblId, label: symbol }) {
   const request = useQuery(SUBCELLULAR_LOCATION_QUERY, {
@@ -84,69 +19,9 @@ function Body({ definition, id: ensemblId, label: symbol }) {
       request={request}
       renderDescription={() => <Description symbol={symbol} />}
       renderBody={({ target }) => {
-        const uniprotId = getUniprotIds(target.proteinIds)[0];
-
-        // split API response locations based on sources:
-        // {
-        //     HPA_main: []
-        // }
-        {
-          /* const sourcesLocations = sources.reduce((srcLocs, src) => {
-          srcLocs[src.id] = target.subcellularLocations.filter(
-            sl => sl.source === src.id
-          );
-          return srcLocs;
-        }, {}); */
-        }
-
-        // split API response locations based on sources
-        const sourcesLocations = {};
-        target.subcellularLocations.forEach(sl => {
-          if (sourcesLocations[sl.source] === undefined) {
-            sourcesLocations[sl.source] = [];
-          }
-          sourcesLocations[sl.source].push(sl);
-        });
-        console.log(sourcesLocations);
-
         return (
           <>
-            <SwissbioViz
-              taxonId="9606"
-              locationIds={target.subcellularLocations
-                .map(s => parseLocationTerm(s.termSL))
-                .join()}
-            >
-              <>
-                {sources.map(s => {
-                  {
-                    /* TODO: this will go in dropdown */
-                  }
-                  return sourcesLocations[s.id] ? (
-                    <Box ml={4} key={s.id}>
-                      <Typography variant="h6">{s.label}</Typography>
-                      Location for{' '}
-                      {s.id === 'uniprot' ? (
-                        <Link
-                          external
-                          to={identifiersOrgLink('uniprot', uniprotId)}
-                        >
-                          {uniprotId}
-                        </Link>
-                      ) : (
-                        <Link
-                          external
-                          to={identifiersOrgLink('hpa', ensemblId)}
-                        >
-                          {ensemblId}
-                        </Link>
-                      )}
-                      <LocationsList sls={sourcesLocations[s.id]} />
-                    </Box>
-                  ) : null;
-                })}
-              </>
-            </SwissbioViz>
+            <SubcellularViz data={target} />
           </>
         );
       }}
